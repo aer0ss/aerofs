7eba9351014cf14630345cfc4bbe9eeb3a167815
Adam Lesinski <adam@aerofs.com>
2012-03-29 00:08:35 -0700

    Wrote test and modified ZephyrUnicastConnectionService to allow successfull sending of a Unicast Packet
    - Requires delay between starting the transport and sending a message
    
    Change-Id: I98aa36247e7832399832ee71692ba361f25ae9f5

--- python/aerofs.tapclient/test.py
+++ python/aerofs.tapclient/test.py
@@ -3,17 +3,29 @@ import time
 import sys
 
 from tap import transport
+from tap import tap_pb2
 
 if __name__ == '__main__':
-	didB = "18d601269b8e4f26baaf9443b72eeddc".decode("hex")
-	sid = uuid.uuid5(uuid.NAMESPACE_DNS, "g.arrowfs.org")
+    didB = uuid.UUID("18d601269b8e4f26baaf9443b72eeddc")
+    sid = uuid.uuid5(uuid.NAMESPACE_DNS, "g.arrowfs.org")
 
-	peerA = transport.connect(('localhost', int(3001)), 'zephyr')
-	peerB = transport.connect(('localhost', int(3002)), 'zephyr')
+    peerA = transport.connect(('localhost', int(3001)), 'zephyr')
+    peerB = transport.connect(('localhost', int(3002)), 'zephyr')
 
-	peerA.updateLocalStoreInterest([sid], [])
-	peerB.updateLocalStoreInterest([sid], [])
+    time.sleep(5)
 
-	peerA.sendPacket(didB, sid, 'Is there anyone home?')
+    peerA.updateLocalStoreInterest([sid], [])
+    peerB.updateLocalStoreInterest([sid], [])
 
-	print peerB.awaitEvent()
+    peerA.sendPacket(didB, sid, 'Is there anyone home?')
+
+    while True:
+        event = peerB.awaitEvent()
+        if event.type == tap_pb2.TransportEvent.UNICAST_PACKET_RECEIVED:
+            print "Unicast packet received {"
+            print "\tfrom DID:     %s" % uuid.UUID(bytes=event.did)
+            print "\ton store SID: %s" % uuid.UUID(bytes=event.sid)
+            print "\tis client:    %s" % event.client
+            print "\tpayload:      %s" % event.payload
+            print "}"
+            break
