b0a7a016cd65463eb50395073769d9d6d191888b
Allen George <allen@aerofs.com>
2012-06-15 15:07:15 -0700

    Update protobuf-obj brew formula so that it actually works
    Change-Id: I3a1862fc416393755c5abdb8e8299cf50cbcf49c

--- tools/protobuf-objc.rb
+++ tools/protobuf-objc.rb
@@ -1,33 +1,19 @@
-# Usage:
-# brew install --HEAD tools/protobuf-objc.rb
-
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-
 class ProtobufObjc < Formula
-  # homepage 'http://github.com/booyah/protobuf-objc'
-  # head 'git://github.com/booyah/protobuf-objc.git'
-  homepage 'http://github.com/aerofs/protobuf-objc'
-  head 'git://github.com/aerofs/protobuf-objc.git'
-  md5 ''
-
   depends_on 'protobuf'
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
+  depends_on 'automake'
+
+  head 'https://github.com/aerofs/protobuf-objc.git'
+  homepage 'https://github.com/aerofs/protobuf-objc.git'
 
   def install
     system "./autogen.sh"
-    system "./configure",
-      "--disable-dependency-tracking",
-      "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
 
   def test
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test protobuf`.
-    system "false"
+    system "which protoc-gen-objc"
   end
 end
