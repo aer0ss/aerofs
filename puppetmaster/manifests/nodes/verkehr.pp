50c5a248793f07e34f012e47f2549d119ae06a1c
Peter Hamilton <Peter@aerofs.com>
2012-07-31 14:58:50 -0700

    Added logrotate to verkehr (puppet) and updated logrotate template
    Change-Id: I8c6ef586c7abe7f16b041526cd47600036559321

--- puppetmaster/manifests/nodes/verkehr.pp
+++ puppetmaster/manifests/nodes/verkehr.pp
@@ -5,4 +5,12 @@ node /^verkehr\.aerofs\.com$/ inherits default {
     users::add_user {
         [ hiera('dev_users') ]:
     }
+
+    logrotate::log{"verkehr":
+        filename => "/var/log/verkehr/verkehr.log",
+        quantity => 14,
+        frequency => "daily",
+        compress => true,
+        require => Class["verkehr"]
+    }
 }
