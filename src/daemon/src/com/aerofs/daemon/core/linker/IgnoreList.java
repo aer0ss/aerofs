cd98ed0f6f126bbe604f53c1bdc6d72686e2b9fd
Hugues Bruant <hugues@aerofs.com>
2012-09-12 13:14:51 -0700

    Ignore temporary files generated by GEdit and Kate
    Reduce DB pollution
    Avoid generating inordinate amount of uninteresting activity log entries
    Avoid monopolizing network bandwidth to sync inherently local files
    
    Change-Id: If60e26324c519aaa854b2e426f07f00624d9edde

--- src/daemon/src/com/aerofs/daemon/core/linker/IgnoreList.java
+++ src/daemon/src/com/aerofs/daemon/core/linker/IgnoreList.java
@@ -41,6 +41,26 @@ public class IgnoreList
         // Addresses aerofs-590.
         if (name.indexOf('\ufffd') != -1) return true;
 
+        /**
+         * Patterns of temporary files ignored because they interfere badly with AeroFS core logic
+         *
+         * NOTE: long term we probably need to use a more generic regexp based solution (probably
+         * not Pattern though because it's a slow backtracking algorithm instead of a fast DFA) and
+         * load the list of excluded pattern from a text file so that power users can tweak the
+         * ignore list
+         */
+
+        // GEdit creates temporary .goutputstream-XXXXXX and copies them over the target file when
+        // saving which pollutes the DB, causes useless network overhead and creates linker conflict
+        // so we simply ignore these files
+        if (name.startsWith(".goutputstream-")) return true;
+
+        // Kate creates temporary .${filename}.kate-swp files to hold the temporary state of a file
+        // being edited and writes it to disk on pretty much every keystroke which pollutes the DB,
+        // creates an inordinate amount of uninteresting activity log entries and significant
+        // network overhead so we ignore these files
+        if (name.charAt(0) == '.' && name.endsWith(".kate-swp")) return true;
+
         return _set.contains(name);
     }
 }
