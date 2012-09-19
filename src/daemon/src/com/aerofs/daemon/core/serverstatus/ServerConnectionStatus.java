1152670e7723749bc8c6235b81aba853c968af62
Hugues Bruant <hugues@aerofs.com>
2012-09-19 12:30:56 -0700

    Server Connection Status
    Introduce ServerConnectionStatus
    Hook VerkherNotificationSubscriber to ServerConnectioinStatus
    Introduce SyncStatusConnection to centralize sync stat RPC calls
    Hook SyncStatusConnection to ServerConnectionStatus
    
    Change-Id: Ibda931762cc51d22c655c2b1071889b7b73d7426

---
+++ src/daemon/src/com/aerofs/daemon/core/serverstatus/ServerConnectionStatus.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) Air Computing Inc., 2012.
+ */
+
+package com.aerofs.daemon.core.serverstatus;
+
+import com.aerofs.daemon.core.serverstatus.IConnectionStatusNotifier.IListener;
+import com.aerofs.daemon.core.syncstatus.SyncStatusConnection;
+import com.aerofs.daemon.core.verkehr.VerkehrNotificationSubscriber;
+import com.aerofs.lib.Util;
+import com.google.common.collect.Maps;
+import com.google.inject.Inject;
+import org.apache.log4j.Logger;
+
+import java.util.Map;
+
+/**
+ * This class keeps tracks of connection status to the different servers the daemon is talking to
+ *
+ * The main purpose is to centralize the knowledge of which server are up and which are experiencing
+ * problems so we can warn the users that some features are temporarily not working
+ */
+public class ServerConnectionStatus
+{
+    public static enum Server
+    {
+        VERKEHR, SYNCSTAT
+    }
+
+    private static final Logger l = Util.l(ServerConnectionStatus.class);
+    private final Map<Server, Boolean> _status = Maps.newEnumMap(Server.class);
+
+    @Inject
+    ServerConnectionStatus(VerkehrNotificationSubscriber vks, SyncStatusConnection ssc)
+    {
+        addListener_(vks, Server.VERKEHR);
+        addListener_(ssc, Server.SYNCSTAT);
+    }
+
+    /**
+     * Helper method to add a listener to a connection
+     */
+    private void addListener_(IConnectionStatusNotifier csn, final Server server)
+    {
+        csn.addListener_(new IListener()
+        {
+            @Override
+            public void onConnected()
+            {
+                synchronized (ServerConnectionStatus.this) {
+                    l.info("connected " + server);
+                    _status.put(server, true);
+                }
+            }
+
+            @Override
+            public void onDisconnected()
+            {
+                synchronized (ServerConnectionStatus.this) {
+                    l.info("disconnected " + server);
+                    _status.put(server, false);
+                }
+            }
+        });
+    }
+
+    /**
+     * @return connection status of a given server
+     */
+    public synchronized boolean isConnected(Server server)
+    {
+        Boolean r = _status.get(server);
+        return r != null ? r : false;
+    }
+
+    /**
+     * @return connection status of all servers AND'ed together
+     */
+    public synchronized boolean isConnected()
+    {
+        boolean r = true;
+        for (Server server : Server.values()) {
+            r &= isConnected(server);
+        }
+        return r;
+    }
+}
---
+++ src/daemon/src/com/aerofs/daemon/core/syncstatus/SyncStatusConnection.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) Air Computing Inc., 2012.
+ */
+
+package com.aerofs.daemon.core.syncstatus;
+
+import com.aerofs.daemon.core.serverstatus.AbstractConnectionStatusNotifier;
+import com.aerofs.daemon.core.tc.Cat;
+import com.aerofs.daemon.core.tc.TC;
+import com.aerofs.daemon.core.tc.TC.TCB;
+import com.aerofs.daemon.core.tc.Token;
+import com.aerofs.lib.Param.SyncStat;
+import com.aerofs.lib.cfg.CfgLocalUser;
+import com.aerofs.lib.id.OID;
+import com.aerofs.lib.id.SID;
+import com.aerofs.lib.syncstat.SyncStatBlockingClient;
+import com.aerofs.proto.Syncstat.GetSyncStatusReply;
+import com.google.inject.Inject;
+import com.google.protobuf.ByteString;
+
+/**
+ * Single persistent connection to sync status server
+ *
+ * The purpose of this class is to ensure each client only has one connection to the sync status
+ * server, that the status of this connection is available through the IConnectionStatusNotifier
+ * interface and that access to the underlying RPC client is properly synchronized.
+ *
+ * Another benefit is that it abstracts away reconnection handling and the sign-in step.
+ */
+public class SyncStatusConnection extends AbstractConnectionStatusNotifier
+{
+    private final TC _tc;
+    private final CfgLocalUser _user;
+    private final SyncStatBlockingClient.Factory _ssf;
+
+    private SyncStatBlockingClient _client;
+
+    @Inject
+    SyncStatusConnection(CfgLocalUser user, TC tc, SyncStatBlockingClient.Factory ssf)
+    {
+        _tc = tc;
+        _ssf = ssf;
+        _user = user;
+        _client = null;
+    }
+
+    /**
+     * Try connecting if needed and notify listeners in case of successful connection
+     *
+     * NOTE: should only be called with the object lock held (i.e synchronized method or similar)
+     */
+    private void ensureConnected_() throws Exception
+    {
+        if (_client == null) {
+            try {
+                _client = _ssf.create(SyncStat.URL, _user.get());
+                _client.signInRemote();
+                notifyConnected_();
+            } catch (Exception e) {
+                _client = null;
+                throw e;
+            }
+        }
+    }
+
+    /**
+     * Discard the current client (to enforce reconnect on next RPC) and notify listeners of
+     * the disconnection.
+     *
+     * NOTE: should only be called with the object lock held (i.e synchronized method or similar)
+     */
+    private void reset_()
+    {
+        _client = null;
+        notifyDisconnected_();
+    }
+
+    /**
+     * Releases the core lock around the setVersionHash RPC call
+     */
+    public void setVersionHash_(OID oid, SID sid, byte[] vh) throws Exception
+    {
+        Token tk = _tc.acquireThrows_(Cat.UNLIMITED, "syncstatpush");
+        TCB tcb = null;
+        try {
+            tcb = tk.pseudoPause_("syncstatpush");
+            setVersionHash(oid, sid, vh);
+        } finally {
+            if (tcb != null) tcb.pseudoResumed_();
+            tk.reclaim_();
+        }
+    }
+
+    /**
+     * Synchronized wrapper of setVersionHash RPC call
+     *
+     * NOTE: should not be called with the core lock held
+     */
+    public synchronized void setVersionHash(OID oid, SID sid, byte[] vh) throws Exception
+    {
+        ensureConnected_();
+        try {
+            _client.setVersionHash(oid.toPB(), sid.toPB(), ByteString.copyFrom(vh));
+        } catch (Exception e) {
+            reset_();
+            throw e;
+        }
+    }
+
+    /**
+     * Releases the core lock around the getSyncStatus RPC call
+     */
+    public GetSyncStatusReply getSyncStatus_(long ssEpoch) throws Exception
+    {
+        Token tk = _tc.acquireThrows_(Cat.UNLIMITED, "syncstatpull");
+        TCB tcb = null;
+        try {
+            tcb = tk.pseudoPause_("syncstatpull");
+            return getSyncStatus(ssEpoch);
+        } finally {
+            if (tcb != null) tcb.pseudoResumed_();
+            tk.reclaim_();
+        }
+    }
+
+    /**
+     * Synchronized wrapper of getSyncStatus RPC call
+     *
+     * NOTE: should not be called with the core lock held
+     */
+    public synchronized GetSyncStatusReply getSyncStatus(long ssEpoch) throws Exception
+    {
+        ensureConnected_();
+        try {
+            return _client.getSyncStatus(ssEpoch);
+        } catch (Exception e) {
+            reset_();
+            throw e;
+        }
+    }
+}
