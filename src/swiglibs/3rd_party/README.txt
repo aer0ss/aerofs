1680f4eab20748bed9b1c6f5c88b55e62cb1a38e
Drew Fisher <drew@aerofs.com>
2012-09-26 18:23:37 -0700

    Update to OpenSSL 1.0.1c.
    There was a security vulnerability in OpenSSL's DTLS implementation that
    could lead to plaintext recovery:
    <http://www.isg.rhul.ac.uk/~kp/dtls.pdf>
    
    Since libaerofsd links to OpenSSL, we must also rebuild libaerofsd for
    all platforms.
    
    Rather than continue to ship a copy of libcrypto.so.1.0.0 for the python
    libraries, we shall instead make aerofs server debian packages depend on
    libssl1.0.0 and let the OS provide it for us (we're using the stock one
    anyway).  Interestingly, the library in the admin panel appears unused
    in prod anyway.
    
    This changeset also gets rid of all duplicate copies of libcrypto and
    libssl.  Everything is now a symbolic link to the official copy in
    resource/client.
    
    Change-Id: I63ec73031c73b9b439d6f7a85467bd41813316d8

---
+++ src/swiglibs/3rd_party/README.txt
@@ -0,0 +1,95 @@
+We use OpenSSL.  However, it can be a pain to build.
+
+Steps to using a new openssl:
+
+----------------------------------
+1) Build OpenSSL for each platform.
+
+  -------------
+  OSX build:
+
+    Update the openssl_aerofs.rb brew script in this folder to use the latest
+    url and shasum, then:
+
+    # brew install ./openssl_aerofs.rb
+    # cp $(brew --prefix)/Cellar/openssl_aerofs/1.0.1c/*.dylib ~/repos/aerofs/resource/client/osx/
+
+    Why use a brew script?  We have to patch upstream openssl, since it
+    specifies an absolute RPATH at build time (but we need a relative path).
+    This seemed easiest.
+
+    Alternatively, you can just build manually as described by the docs and use
+    install_name_tool(1) to adjust the linker paths and library names, but
+    that's more manual effort.
+
+  -------------
+  Linux build:
+
+    You'll have to do this twice; once for 32-bit and once for 64-bit.
+
+    1) Get the source:
+
+    # cd $HOME
+    # mkdir scratch
+    # cd scratch
+    # # Obviously, you should pick the latest link.
+    # wget http://www.openssl.org/source/openssl-1.0.1c.tar.gz
+    # tar xzvf openssl*.tar.gz
+    # cd openssl-1.0.1c/
+
+    2) Configure:
+
+    # # For linux32, make the last argument "linux-elf"
+    # perl ./Configure --prefix=/usr/local shared linux-x86_64
+
+    3) Build:
+
+    # make
+    # make test
+
+    4) Copy libssl and libcrypto to repos/aerofs/resource/client/linux(32|64)/ .
+
+  -------------
+  Windows build:
+
+    1) Install on a Windows machine:
+       Visual Studio 2010
+       Activestate Perl: <http://www.activestate.com/activeperl/downloads>
+       Netwide Assembler (nasm): <http://www.nasm.us/pub/nasm/releasebuilds/>
+
+    2) Download the OpenSSL source as above
+
+    3) Open a Visual Studio Command Prompt (x86, not x64)
+
+      > cd path\to\openssl-1.0.1c
+      > "C:\Program Files (x86)\nasm\nasmpath.bat"
+      > perl Configure VC-WIN32
+      > ms\do_nasm.bat
+      > nmake -f ms\ntdll.mak
+
+    4) Copy the binaries from out32dll:
+       * libeay32.dll and ssleay32.dll should be copied to resource/client/win/
+       * libeay32.lib and ssleay32.lib should be copied to src/swiglibs/3rd_party/lib/win32/
+
+----------------------------------
+2) copy the headers into the folder containing this README:
+
+  # Wipe out the old headers:
+    cd $HOME/repos/aerofs/src/swiglibs/3rd_party/include
+    mv openssl openssl_old
+  # Copy in the new headers
+    cd $HOME
+    rm -rf scratch
+    mkdir scratch
+    cd scratch
+        # Obviously, you should pick the latest link.
+    wget http://www.openssl.org/source/openssl-1.0.1c.tar.gz
+    tar xzvf openssl*.tar.gz
+    cd openssl-*
+    cd include
+    # Mind the slashes
+    rsync -avL openssl $HOME/repos/aerofs/src/swiglibs/3rd_party/include/
+
+----------------------------------
+3) rebuild aerofsd.so for all platforms (beyond the scope of this README)
+
---
+++ src/swiglibs/3rd_party/openssl_aerofs.rb
@@ -0,0 +1,57 @@
+require 'formula'
+
+class OpensslAerofs < Formula
+  homepage 'http://openssl.org'
+  url 'http://openssl.org/source/openssl-1.0.1c.tar.gz'
+  sha256 '2a9eb3cd4e8b114eb9179c0d3884d61658e7d8e8bf4984798a5f5bd48e325ebe'
+
+  keg_only :provided_by_osx,
+    "The OpenSSL provided by OS X is too old for some software."
+
+  def patches
+    # Makes libcrypto and libssl not specify absolute RPATHs
+    DATA
+  end
+
+  def install
+    args = %W[./Configure
+               --prefix=#{prefix}
+               --openssldir=#{etc}/openssl
+               shared
+             ]
+
+    args << (MacOS.prefer_64_bit? ? "darwin64-x86_64-cc" : "darwin-i386-cc")
+
+    system "perl", *args
+
+    ENV.deparallelize # Parallel compilation fails
+    system "make"
+    system "make", "test"
+    prefix.install 'libcrypto.1.0.0.dylib', 'libssl.1.0.0.dylib'
+    #system "make", "install", "MANDIR=#{man}", "MANSUFFIX=ssl"
+  end
+end
+
+__END__
+--- a/Makefile.shared	2012-09-24 18:56:25.000000000 -0700
++++ b/Makefile.shared	2010-08-21 04:37:17.000000000 -0700
+@@ -250,7 +250,7 @@
+ 	if [ -n "$$SHLIB_SOVER_NODOT" ]; then \
+ 		SHAREDFLAGS="$$SHAREDFLAGS -compatibility_version $$SHLIB_SOVER_NODOT"; \
+ 	fi; \
+-	SHAREDFLAGS="$$SHAREDFLAGS -install_name $(INSTALLTOP)/$(LIBDIR)/$$SHLIB$(SHLIB_EXT)"; \
++	SHAREDFLAGS="$$SHAREDFLAGS -install_name $$SHLIB$(SHLIB_EXT)"; \
+ 	$(LINK_SO_A)
+ link_app.darwin:	# is there run-path on darwin?
+ 	$(LINK_APP)
+--- a/Configure   2012-10-03 14:35:24.000000000 -0700
++++ b/Configure   2012-03-14 15:20:40.000000000 -0700
+@@ -579,7 +579,7 @@
+ "darwin64-ppc-cc","cc:-arch ppc64 -O3 -DB_ENDIAN::-D_REENTRANT:MACOSX:-Wl,-search_paths_first%:SIXTY_FOUR_BIT_LONG RC4_CHAR RC4_CHUNK DES_UNROLL BF_PTR:${ppc64_asm}:osx64:dlfcn:darwin-shared:-fPIC -fno-common:-arch ppc64 -dynamiclib:.\$(SHLIB_MAJOR).\$(SHLIB_MINOR).dylib",
+ "darwin-i386-cc","cc:-arch i386 -O3 -fomit-frame-pointer -DL_ENDIAN::-D_REENTRANT:MACOSX:-Wl,-search_paths_first%:BN_LLONG RC4_INT RC4_CHUNK DES_UNROLL BF_PTR:".eval{my $asm=$x86_asm;$asm=~s/cast\-586\.o//;$asm}.":macosx:dlfcn:darwin-shared:-fPIC -fno-common:-arch i386 -dynamiclib:.\$(SHLIB_MAJOR).\$(SHLIB_MINOR).dylib",
+ "debug-darwin-i386-cc","cc:-arch i386 -g3 -DL_ENDIAN::-D_REENTRANT:MACOSX:-Wl,-search_paths_first%:BN_LLONG RC4_INT RC4_CHUNK DES_UNROLL BF_PTR:${x86_asm}:macosx:dlfcn:darwin-shared:-fPIC -fno-common:-arch i386 -dynamiclib:.\$(SHLIB_MAJOR).\$(SHLIB_MINOR).dylib",
+-"darwin64-x86_64-cc","cc:-arch x86_64 -O3 -DL_ENDIAN -Wall::-D_REENTRANT:MACOSX:-Wl,-search_paths_first%:SIXTY_FOUR_BIT_LONG RC4_CHAR RC4_CHUNK DES_INT DES_UNROLL:".eval{my $asm=$x86_64_asm;$asm=~s/rc4\-[^:]+//;$asm}.":macosx:dlfcn:darwin-shared:-fPIC -fno-common:-arch x86_64 -dynamiclib:.\$(SHLIB_MAJOR).\$(SHLIB_MINOR).dylib",
++"darwin64-x86_64-cc","cc:-arch x86_64 -O3 -mmacosx-version-min=10.5 -DL_ENDIAN -Wall::-D_REENTRANT:MACOSX:-Wl,-search_paths_first%:SIXTY_FOUR_BIT_LONG RC4_CHAR RC4_CHUNK DES_INT DES_UNROLL:".eval{my $asm=$x86_64_asm;$asm=~s/rc4\-[^:]+//;$asm}.":macosx:dlfcn:darwin-shared:-fPIC -fno-common:-arch x86_64 -dynamiclib:.\$(SHLIB_MAJOR).\$(SHLIB_MINOR).dylib",
+ "debug-darwin-ppc-cc","cc:-DBN_DEBUG -DREF_CHECK -DCONF_DEBUG -DCRYPTO_MDEBUG -DB_ENDIAN -g -Wall -O::-D_REENTRANT:MACOSX::BN_LLONG RC4_CHAR RC4_CHUNK DES_UNROLL BF_PTR:${ppc32_asm}:osx32:dlfcn:darwin-shared:-fPIC:-dynamiclib:.\$(SHLIB_MAJOR).\$(SHLIB_MINOR).dylib",
+ # iPhoneOS/iOS
+ "iphoneos-cross","llvm-gcc:-O3 -isysroot \$(CROSS_TOP)/SDKs/\$(CROSS_SDK) -fomit-frame-pointer -fno-common::-D_REENTRANT:iOS:-Wl,-search_paths_first%:BN_LLONG RC4_CHAR RC4_CHUNK DES_UNROLL BF_PTR:${no_asm}:dlfcn:darwin-shared:-fPIC -fno-common:-dynamiclib:.\$(SHLIB_MAJOR).\$(SHLIB_MINOR).dylib",
