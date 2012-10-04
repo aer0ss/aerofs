aer0ss/aerofs

commit f586f60c13b30be7ccc4ca4d054dfbacf0c6af5d

/bin/ls may be a symlink to /usr/bin/ls
See:

<http://www.freedesktop.org/wiki/Software/systemd/TheCaseForTheUsrMerge>
<https://wiki.ubuntu.com/FoundationsTeam/Specs/Quantal/UsrMerge>

This breaks our 32/64-bitness detection logic.  So we should dereference
the symlink and test that file instead.

Change-Id: I3685f24de84bbc50983e650732c4f1507c81b406

resource/client/linux.inst/aerofs
@@ -12,7 +12,7 @@ if [ $PROG == gui ]; then
     fi
 fi
 
-if [ x"`file /bin/ls | grep 64`" != x ]; then
+if [ x"$(file $(readlink -f /bin/ls) | grep 64)" != x ]; then
     ARCH=64
 else
     ARCH=32

src/lib/src/com/aerofs/lib/os/OSUtil.java
@@ -36,8 +36,13 @@ public class OSUtil
             OSArch arch;
             try {
                 OutArg<String> output = new OutArg<String>();
+                OutArg<String> realpath = new OutArg<String>();
                 // can't use Log4J as it may not be initialized yet
-                Util.execForegroundNoLogging(output, "file", "/bin/ls");
+                // We avoid uname because we care about the userspace bitness, not the kernel
+                // bitness.  /bin/ls may be a symlink to /usr/bin/ls, see
+                // http://www.freedesktop.org/wiki/Software/systemd/TheCaseForTheUsrMerge
+                Util.execForegroundNoLogging(realpath, "readlink", "-f", "/bin/ls");
+                Util.execForegroundNoLogging(output, "file", realpath.get());
                 if (output.get().contains("64")) arch = OSArch.X86_64;
                 else arch = OSArch.X86;
             } catch (IOException e) {

src/lib/src/com/aerofs/lib/spsv/SVClient.java
@@ -539,7 +539,15 @@ public class SVClient
             OutArg<String> fileRes = new OutArg<String>();
             fileRes.set("n/a");
             try {
-                Util.execForeground(fileRes,"file", "/bin/ls");
+                if (OSUtil.isLinux()) {
+                    // /bin/ls may be a symlink on Linux.
+                    // On OSX, readlink doesn't have the -f option.
+                    OutArg<String> readlinkRes = new OutArg<String>();
+                    Util.execForeground(readlinkRes,"readlink", "-f", "/bin/ls");
+                    Util.execForeground(fileRes,"file", readlinkRes.get());
+                } else {
+                    Util.execForeground(fileRes,"file", "/bin/ls");
+                }
             } catch (Exception e2) {
                 //ignored
             }

tools/populate/run
@@ -25,7 +25,7 @@ if [ $OS == osx ]; then
 elif [ x`echo $OS | grep linux` != x ]; then
     # We need to set the LD_LIBRARY_PATH to include the JRE directories so that
     # aerofsd will successfully launch
-    if [ x"`file /bin/ls | grep 64`" != x ]; then
+    if [ x"$(file $(readlink -f /bin/ls) | grep 64)" != x ]; then
         ARCH=64
         JRE_LIB_ARCH=amd64
     else

commit a7f4cbf31760840d49406a8c6b1fc27f05df0ff9

Linux launcher: redownload on 32/64bit mismatch
f586f60c13b30be7ccc4ca4d054dfbacf0c6af5d introduced a regression in the
Java updater code which would cause all Linux clients to think they were
running on 32-bit systems, regardless of actual architecture.

Although d21c2bfe2bc9a4e6f55a8492bd0cc2ed5fc4ffed fixed the error in the
Java code, we now have clients in the wild with broken updaters, and all
Linux clients will download the 32-bit package for 0.4.85.

This changeset adds functionality to the aerofs{-sh,-cli,-gui} launcher
scripts to check if /bin/ls has the same bitness as aerofsd.  If not, it
will attempt to redownload the package and remove the broken approot.

To perform this download without assuming anything about the user's
system, I have added com.aerofs.downloader to aerofs.jar.  Since the
script is run in a headless manner, I added a headless download option
to the downloader.

Change-Id: Ib88b68829119aa444c3190f6f0f7ee8728952302

resource/client/linux.inst/aerofs
@@ -12,11 +12,17 @@ if [ $PROG == gui ]; then
     fi
 fi
 
-if [ x"$(file $(readlink -f /bin/ls) | grep 64)" != x ]; then
+if [ x"$(file -L /bin/ls | grep 64 )" != x ]; then
     ARCH=64
+    PACKAGE_FILENAME=aerofs-x86_64.tgz
+    JRE_LIB_ARCH=amd64
 else
     ARCH=32
+    PACKAGE_FILENAME=aerofs-x86.tgz
+    JRE_LIB_ARCH=i386
 fi
+
+URL=https://cache.clientDOT_STAGING__TO_BE_REPLACED_BY_SED.aerofs.com/$PACKAGE_FILENAME
 
 DIST="$HOME/.aerofs-binDOT_STAGING__TO_BE_REPLACED_BY_SED"
 
@@ -38,13 +44,6 @@ if [ ! -f "$DIST/aerofs.jar" ]; then
         exit 1
     fi
     uudecode -o /dev/stdout $0 | tar xzf - -C $TMP_DIR 2>/dev/null
-
-    if [ $ARCH == 64 ]; then
-        URL=aerofs-x86_64.tgz
-    else
-        URL=aerofs-x86.tgz
-    fi
-    URL=https://cache.clientDOT_STAGING__TO_BE_REPLACED_BY_SED.aerofs.com/$URL
 
     which java > /dev/null
     if [ $? != 0 ]; then
@@ -77,12 +76,24 @@ if [ ! -f "$DIST/aerofs.jar" ]; then
     mkdir -p "$HOME/.config/autostart/"
     rm -f "$HOME/.config/autostart/aerofs.desktop"
     ln -s "$DIST/aerofs.desktop" "$HOME/.config/autostart/"
-fi
-
-if [ $ARCH == 64 ]; then
-    JRE_LIB_ARCH=amd64
 else
-    JRE_LIB_ARCH=i386
+    # Sanity check - ensure that aerofs libs match 32/64 bitness with userspace
+    if [ x"$( file -L "$DIST/aerofsd" | grep 64 )" != x ]; then
+        AEROFS_ARCH=64
+    else
+        AEROFS_ARCH=32
+    fi
+    if [ x"$AEROFS_ARCH" != x"$ARCH" ]; then
+        # Somehow we have the wrong binaries.  Redownload and reinstall.
+        TMP_DIR=$( mktemp -d )
+        TARGET="$TMP_DIR/retry"
+        java -cp $DIST/aerofs.jar com.aerofs.downloader.Main headless $URL "$TARGET"
+        rm -rf "$DIST"
+        tar zxf "$TARGET" -C "$TMP_DIR" 2>/dev/null
+        mv "$TMP_DIR/aerofs" "$DIST"
+        cd "$DIST" # Java doesn't like being launched from a deleted folder
+        rm -rf "$TMP_DIR"
+    fi
 fi
 
 JRE_BASE="$(dirname $(dirname $(readlink -f $(which java))))"

src/gui/src/com/aerofs/downloader/CLIDownloader.java
@@ -42,7 +42,7 @@ class CLIDownloader implements IProgressIndicator {
     @Override
     public void error(Throwable e)
     {
-        System.out.println(Main.getErrorMessage(e));
+        System.err.println(Main.getErrorMessage(e));
         System.exit(1);
     }
 }

src/gui/src/com/aerofs/downloader/Main.java
@@ -18,20 +18,24 @@ public class Main {
     static final String PRODUCT_NAME = "AeroFS";
     static final String TITLE = "Downloading " + PRODUCT_NAME + " for initial setup...";
 
+    private static IProgressIndicator getDownloader(String indicatorType)
+    {
+        if (indicatorType.equals("gui")) return new GUIDownloader();
+        if (indicatorType.equals("headless")) return new SilentDownloader();
+        return new CLIDownloader();
+    }
+
     /**
-     * parameters:  ["gui"|"cli"] [url] [dest_path]
+     * parameters:  ["gui"|"cli"|"headless"] [url] [dest_path]
      * exit code: 0 if download succeeds, non-0 otherwise
      */
     public static void main(String[] args) throws Exception
     {
         if (args.length != 3) throw new Exception("bad paramaters");
 
-        boolean gui = args[0].equals("gui");
+        final IProgressIndicator pi = getDownloader(args[0]);
         final String url = args[1];
         final String dest = args[2];
-
-        final IProgressIndicator pi = gui ? new GUIDownloader() :
-            new CLIDownloader();
 
         pi.run(new Runnable() {
             @Override

src/gui/src/com/aerofs/downloader/SilentDownloader.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (c) Air Computing Inc., 2012.
+ */
+
+package com.aerofs.downloader;
+
+class SilentDownloader implements IProgressIndicator
+{
+
+    @Override
+    public void setProgress(int count)
+    {
+        // noop
+    }
+
+    @Override
+    public void setTotal(int len)
+    {
+        // noop
+    }
+
+    @Override
+    public void complete()
+    {
+        System.exit(0);
+    }
+
+    @Override
+    public void run(Runnable runnable)
+    {
+        runnable.run();
+    }
+
+    @Override
+    public void error(Throwable e)
+    {
+        System.err.println(Main.getErrorMessage(e));
+        System.exit(1);
+    }
+}

src/lib/src/com/aerofs/lib/os/OSUtil.java
@@ -35,27 +35,19 @@ public class OSUtil
             _os = new OSUtilLinux(factFile);
             OSArch arch;
             try {
-                OutArg<String> output = new OutArg<String>();
-                OutArg<String> realpath = new OutArg<String>();
+                OutArg<String> commandOutput = new OutArg<String>();
                 // can't use Log4J as it may not be initialized yet
                 // We avoid uname because we care about the userspace bitness, not the kernel
                 // bitness.  /bin/ls may be a symlink to /usr/bin/ls, see
                 // http://www.freedesktop.org/wiki/Software/systemd/TheCaseForTheUsrMerge
-                int result = Util.execForegroundNoLogging(realpath, "readlink", "-f", "/bin/ls");
-                if (result != 0) {
-                    Util.fatal("arch detect, error code " + result + ": could not read /bin/ls");
-                }
-
-                // Make sure we trim the newline at the end of the previous call's output,
-                // or the 'file' command won't be able to find the returned path
-                String path = realpath.get().trim();
-                result = Util.execForegroundNoLogging(output, "file", path);
+                String path = "/bin/ls";
+                int result = Util.execForegroundNoLogging(commandOutput, "file", "-L", path);
                 if (result != 0) {
                     Util.fatal("arch detect, error code " + result
                             + ": could not read '" + path + "'");
                 }
 
-                if (output.get().contains("64")) arch = OSArch.X86_64;
+                if (commandOutput.get().contains("64")) arch = OSArch.X86_64;
                 else arch = OSArch.X86;
             } catch (IOException e) {
                 arch = null;

src/lib/src/com/aerofs/lib/spsv/SVClient.java
@@ -539,15 +539,7 @@ public class SVClient
             OutArg<String> fileRes = new OutArg<String>();
             fileRes.set("n/a");
             try {
-                if (OSUtil.isLinux()) {
-                    // /bin/ls may be a symlink on Linux.
-                    // On OSX, readlink doesn't have the -f option.
-                    OutArg<String> readlinkRes = new OutArg<String>();
-                    Util.execForeground(readlinkRes,"readlink", "-f", "/bin/ls");
-                    Util.execForeground(fileRes,"file", readlinkRes.get());
-                } else {
-                    Util.execForeground(fileRes,"file", "/bin/ls");
-                }
+                Util.execForeground(fileRes, "file", "-L", "/bin/ls");
             } catch (Exception e2) {
                 //ignored
             }

tools/build/make_release_resources
@@ -135,7 +135,9 @@ function create_aerofs_installer() {
     local local_BIN="${AEROFS_RELEASE_DIR}/bin"
     local local_AEROFS_DLR_PKG=com/aerofs/downloader
     mkdir -p "${local_DOWNLOADER_TEMP}/${local_AEROFS_DLR_PKG}"
-    mv "${AEROFS_RELEASE_DIR}/bin/${local_AEROFS_DLR_PKG}"/* "${local_DOWNLOADER_TEMP}/${local_AEROFS_DLR_PKG}"
+    # N.B. we use the downloader in both the main aerofs.jar (for recovery
+    # purposes) and the installer .jar (for initial setup).
+    cp "${AEROFS_RELEASE_DIR}/bin/${local_AEROFS_DLR_PKG}"/* "${local_DOWNLOADER_TEMP}/${local_AEROFS_DLR_PKG}"
     cp "${CLIENT_RESOURCES_DIR}/linux32/lib/swt-"*.jar "${local_DOWNLOADER_TEMP}/s32.jar"
     cp "${CLIENT_RESOURCES_DIR}/linux64/lib/swt-"*.jar "${local_DOWNLOADER_TEMP}/s64.jar"
     tar zcf - -C "$local_DOWNLOADER_TEMP" . | uuencode bin >> "${AEROFS_RELEASE_INST_DIR}/aerofs"