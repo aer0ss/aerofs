277440acbb7afd713c3e109c2566b2b4ddb77813
Gregory Schlomoff <greg@aerofs.com>
2012-10-11 10:00:27 -0700

    On Windows, display a dialog box if we fail to launch because MSVC redist is missing
    Without this, AeroFS fails to launch silently and we get SV defects about
    aerofs.dll failing to load because of missing dependent library.
    
    This message should allow the user to f.ix the problem
    
    Change-Id: I2b361a171a016237f0f930da64d8e623f33b3a30

--- src/gui/src/com/aerofs/gui/GUIProgram.java
+++ src/gui/src/com/aerofs/gui/GUIProgram.java
@@ -7,10 +7,14 @@ package com.aerofs.gui;
 import com.aerofs.controller.ControllerBadCredentialListener;
 import com.aerofs.controller.ControllerService;
 import com.aerofs.lib.IProgram;
+import com.aerofs.lib.S;
 import com.aerofs.lib.Util;
+import com.aerofs.lib.os.OSUtil;
 import com.aerofs.lib.spsv.SPBlockingClient;
 import com.aerofs.ui.UI;
 import org.apache.log4j.Logger;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
 
 public class GUIProgram implements IProgram
 {
@@ -19,8 +23,27 @@ public class GUIProgram implements IProgram
     @Override
     public void launch_(String rtRoot, String prog, String[] args) throws Exception
     {
-        // "dc" stands for GUI native library in C
-        Util.initDriver("gc");
+        try {
+            // "gc" is the name of the log file for the aerofsd library loaded by the GUI
+            Util.initDriver("gc");
+        } catch (UnsatisfiedLinkError linkError) {
+            // On Windows, a common cause of failure is that the MSVC 2010 redistributables aren't
+            // installed. Display a message box to the user so that he can fix the problem
+            if (OSUtil.isWindows()) {
+                try {
+                    System.loadLibrary("msvcp100");
+                } catch (UnsatisfiedLinkError e1) {
+                    MessageBox msgBox = new MessageBox(new Shell());
+                    msgBox.setMessage(S.PRODUCT + " cannot launch because the Microsoft Visual " +
+                            "C++ 2010 redistributable package is not installed. Please go to the " +
+                            "following URL to download and install it:\n\nhttp://ae.ro/msvc2010");
+                    msgBox.open();
+                    linkError = new UnsatisfiedLinkError("aerofsd.dll could not load because " +
+                            "MSVC 2010 redistributables are missing");
+                }
+            }
+            throw linkError;
+        }
         ControllerService.init(rtRoot, UI.notifier());
         SPBlockingClient.setListener(new ControllerBadCredentialListener());
         UI.set(new GUI(rtRoot));
