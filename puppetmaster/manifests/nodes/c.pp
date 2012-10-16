e5c6e4c853f5039360557af67c0e2937321f5551
Matt Pillar <matt@aerofs.com>
2012-10-16 12:44:08 -0700

    Remove references to mysql_syncstat.
    Change-Id: I94adda878c5c68b45161e84c79a747ef4cf2c7e2

--- puppetmaster/manifests/nodes/c.pp
+++ puppetmaster/manifests/nodes/c.pp
@@ -15,14 +15,12 @@ node "c.aerofs.com" inherits default {
     }
 
     # Fetch variables from hiera
-    $mysql_syncstat = hiera("mysql_syncstat")
     $mysql_sp = hiera("mysql_sp")
     $mysql_endpoint = hiera("mysql_endpoint")
 
     # install syncstat servlet
     class{"servlet::syncstat":
         mysql_sp_password       => $mysql_sp["password"],
-        mysql_syncstat_password => $mysql_syncstat["password"],
         mysql_endpoint          => $mysql_endpoint,
         verkehr_host            => "staging.aerofs.com",
         cacert_location         => $cacert_location
--- puppetmaster/manifests/nodes/sss.pp
+++ puppetmaster/manifests/nodes/sss.pp
@@ -7,14 +7,12 @@ node "sss.aerofs.com" inherits default {
     include redis
 
     # Fetch variables from hiera
-    $mysql_syncstat = hiera("mysql_syncstat")
     $mysql_sp = hiera("mysql_sp")
     $mysql_endpoint = hiera("mysql_endpoint")
 
     # install syncstat servlet
     class{"servlet::syncstat":
         mysql_sp_password       => $mysql_sp["password"],
-        mysql_syncstat_password => $mysql_syncstat["password"],
         mysql_endpoint          => $mysql_endpoint,
         verkehr_host            => "verkehr.aerofs.com",
         cacert_location         => "/etc/ssl/certs/AeroFS_CA.pem"
