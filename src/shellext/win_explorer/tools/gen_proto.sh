7206be28b469ea9a3dfb4be4ffc43be9b0fc7254
Hugues Bruant <hugues@aerofs.com>
2012-10-18 18:03:48 -0700

    Remove some logging in shellext
    Change-Id: I0bb90e1d89f2dbd99d663cee2aa51ed475b4bcf4

--- src/shellext/win_explorer/tools/gen_proto.sh
+++ src/shellext/win_explorer/tools/gen_proto.sh
@@ -4,8 +4,9 @@ GEN_ROOT=../gen
 SRC_ROOT=../src
 GUI_PROTO=../../../../src/gui/src/proto
 LIB_PROTO=../../../../src/lib/src/proto
+PROTOC=../3rdparty/bin/protoc.exe
 
 mkdir -p $GEN_ROOT
 
-protoc -I=$LIB_PROTO --cpp_out=$GEN_ROOT $LIB_PROTO/path_status.proto
-protoc -I=$LIB_PROTO -I=$GUI_PROTO --cpp_out=$GEN_ROOT $GUI_PROTO/shellext.proto
+$PROTOC -I=$LIB_PROTO --cpp_out=$GEN_ROOT $LIB_PROTO/path_status.proto
+$PROTOC -I=$LIB_PROTO -I=$GUI_PROTO --cpp_out=$GEN_ROOT $GUI_PROTO/shellext.proto
