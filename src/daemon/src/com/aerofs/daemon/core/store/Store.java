b541a189fa0efbf96fd5bdd4b86c151598ec4473
Mark Jeffrey <mark@aerofs.com>
2012-11-02 14:21:47 -0700

    Remove dependency of collector on Store
    o removes Store's dependency on DirectoryService
    
    Change-Id: I3d2d8add4b39ff553a6f8429a78a7fc33e462364

--- src/daemon/src/com/aerofs/daemon/core/store/Store.java
+++ src/daemon/src/com/aerofs/daemon/core/store/Store.java
@@ -9,29 +9,21 @@ import javax.annotation.Nullable;
 
 import com.aerofs.daemon.core.device.DevicePresence;
 import com.google.inject.Inject;
-import org.apache.log4j.Logger;
 
 import com.aerofs.daemon.core.AntiEntropy;
 import com.aerofs.daemon.core.collector.Collector;
 import com.aerofs.daemon.core.collector.SenderFilters;
 import com.aerofs.daemon.core.device.Device;
 import com.aerofs.daemon.core.device.OPMStore;
-import com.aerofs.daemon.core.ds.CA;
-import com.aerofs.daemon.core.ds.DirectoryService;
-import com.aerofs.daemon.core.ds.OA;
 import com.aerofs.daemon.lib.IDumpStatMisc;
 import com.aerofs.daemon.lib.db.IMetaDatabase;
 import com.aerofs.lib.Util;
 import com.aerofs.lib.cfg.Cfg;
-import com.aerofs.lib.id.CID;
 import com.aerofs.lib.id.DID;
 import com.aerofs.lib.id.SIndex;
-import com.aerofs.lib.id.SOCID;
 
 public class Store implements Comparable<Store>, IDumpStatMisc
 {
-    private static final Logger l = Util.l(Store.class);
-
     private final SIndex _sidx;
 
     // only set for partial replicas
@@ -47,19 +39,17 @@ public class Store implements Comparable<Store>, IDumpStatMisc
         private DevicePresence _dp;
         private IMetaDatabase _mdb;
         private AntiEntropy _ae;
-        private DirectoryService _ds;
         private Collector.Factory _factCollector;
         private SenderFilters.Factory _factSF;
 
         @Inject
         public void inject_(SenderFilters.Factory factSF,
-            Collector.Factory factCollector, DirectoryService ds, AntiEntropy ae,
+            Collector.Factory factCollector, AntiEntropy ae,
             IMetaDatabase mdb, DevicePresence dp)
         {
 
             _factSF = factSF;
             _factCollector = factCollector;
-            _ds = ds;
             _ae = ae;
             _mdb = mdb;
             _dp = dp;
@@ -119,8 +109,6 @@ public class Store implements Comparable<Store>, IDumpStatMisc
     @Override
     public int hashCode()
     {
-        // TODO change back to _sidx.hashCode when Stores are guaranteed to be re-admitted with
-        // the same SIndex as they had before expulsion
         return _sidx.hashCode();
     }
 
@@ -148,40 +136,6 @@ public class Store implements Comparable<Store>, IDumpStatMisc
         _opms = opms;
     }
 
-    private long _lenSinceLastCompute;
-
-    /**
-     * Notify this Store that its child object {@code socid} was downloaded
-     * TODO (MJ) Store could implement IDownloadCompletionListener instead of using this method
-     * FIXME: DOCUMENT THIS
-     * If this device is not a Full Replica (which means it is SP?),
-     * then verify the socid which was downloaded is meta-data, or that it has no OA. Otherwise
-     * crash. Does this mean that SP should verify that it is only downloading meta-data,
-     * or it should have null OA's for content SOCIDs? This would be useful to document
-     */
-    public void downloaded_(SOCID socid)
-    {
-        if (Cfg.isFullReplica()) return;
-
-        try {
-            assert socid.sidx().equals(_sidx);
-            if (socid.cid().equals(CID.META)) return;
-            OA oa = _f._ds.getOANullable_(socid.soid());
-            if (oa == null) return;
-            for (CA ca : oa.cas().values()) _lenSinceLastCompute += ca.length();
-
-            // recompute the size if the total downloads since the last computation
-            // exceeds a half of the remaining space
-            assert false : "uncomment the code below";
-//            if (_lenSinceLastCompute >= (_quota - _used) / 2) {
-//                computeUsedSpace_();
-//                _lenSinceLastCompute = 0;
-//            }
-        } catch (Exception e) {
-            l.warn("downloaded: " + Util.e(e));
-        }
-    }
-
     private void computeUsedSpace_() throws SQLException
     {
         assert !Cfg.isFullReplica();
@@ -198,7 +152,7 @@ public class Store implements Comparable<Store>, IDumpStatMisc
     public void dumpStatMisc(String indent, String indentUnit, PrintStream ps)
     {
         ps.println(indent + _sidx + " used " + Util.format(_used) + "+" +
-                Util.format(_lenSinceLastCompute) + (isOverQuota_(0) ? " (over quota)" : ""));
+                (isOverQuota_(0) ? " (over quota)" : ""));
         _collector.dumpStatMisc(indent + indentUnit, indentUnit, ps);
     }
 
