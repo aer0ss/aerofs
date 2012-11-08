bb662772db5ce9b29302e80938889b8656db678f
Peter Hamilton <Peter@aerofs.com>
2012-11-07 19:22:37 -0800

    Added verification that current commit passed CI before allowing deploy.
    Change-Id: Ifa58daa153aad642cd00397881a10b64b8e1559e

---
+++ tools/build/verify_ci_status.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import requests
+import subprocess
+import sys
+
+BASE_CI_URL = "https://ci.local:8543"
+
+current_sha = subprocess.check_output("git log --pretty='%H' -n 1", shell=True).strip()
+
+print "Current commit: {0}".format(current_sha)
+sys.stdout.flush()
+
+# get a list of last 20 successful builds
+url = BASE_CI_URL + "/httpAuth/app/rest/buildTypes/id:bt4/builds"
+params = {
+    "status":"SUCCESS",
+    "count":"20"
+}
+headers = {
+    'accept': 'application/json'
+}
+# This is a non admin account. If we cared about it we wouldn't set the
+# password to temp123.
+auth = ('aerofsteam','temp123')
+r = requests.get(url, params=params, auth=auth, headers=headers, verify=False)
+builds = r.json["build"]
+
+for build in builds:
+    # for each build we need to make an api call for more details
+    build_url = BASE_CI_URL + build["href"]
+    build_request = requests.get(build_url, auth=('aerofsteam','temp123'), headers=headers, verify=False)
+    build_info = build_request.json
+    # grab the sha of the build and compare with the current sha
+    sha = build_info["revisions"]["revision"][0]["version"]
+    if current_sha == sha:
+        print "Current commit successfully passed CI"
+        exit(0)
+
+print "Current commit has not passed CI"
+exit(1)
