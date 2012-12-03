b8603b338cf88a8fa2330cc5bc9560c9fdd5b4bb
Adam Lesinski <adam@aerofs.com>
2012-12-03 11:20:41 -0800

    Added test to ensure ExceptionReply uses the correct fields in PBException
    Change-Id: I6719d1f5da76944603785f60bc91a45be4ca1bd3

--- src/python-lib/setup.py
+++ src/python-lib/setup.py
@@ -2,11 +2,18 @@ import os
 
 from setuptools import setup, find_packages
 
+requires = [
+    'protobuf',
+    'pycrypto'
+]
+
 # TODO (GS): Find a better name for this package
 setup(name='aerofs-py-lib',
       version='0.1',
       description='AeroFS Python lib',
       packages=find_packages(),
       include_package_data=True,
+      install_requires=requires,
+      test_suite='unittests'
 )
 
---
+++ src/python-lib/unittests/aerofs/test_exception.py
@@ -0,0 +1,25 @@
+import unittest
+
+from aerofs import exception
+from aerofs.gen import common_pb2
+
+class RegularMessagePBExceptionTest(unittest.TestCase):
+    def runTest(self):
+        pbException = common_pb2.PBException()
+        pbException.type = common_pb2.PBException.INTERNAL_ERROR
+        pbException.message = "This is an error"
+
+        reply = exception.ExceptionReply(pbException)
+        self.assertEqual(common_pb2.PBException.INTERNAL_ERROR, reply.get_type())
+        self.assertEqual(u"INTERNAL_ERROR: This is an error", str(reply))
+
+class PlainTextMessagePBExceptionTest(unittest.TestCase):
+    def runTest(self):
+        pbException = common_pb2.PBException()
+        pbException.type = common_pb2.PBException.INTERNAL_ERROR
+        pbException.message = "This is an error"
+        pbException.plain_text_message = "This is a pretty string"
+
+        reply = exception.ExceptionReply(pbException)
+        self.assertEqual(common_pb2.PBException.INTERNAL_ERROR, reply.get_type())
+        self.assertEqual(u"INTERNAL_ERROR: This is a pretty string", str(reply))
