4604e8506d2cb64b070abb4211161ebf2862c1db
Weihan Wang <weihan@aerofs.com>
2013-01-03 22:44:53 -0800

    rename and fix a unit test
    Change-Id: I5c1c8f050676893e0abda272470e8a312b57873f

--- src/spsv/test/com/aerofs/sp/server/integration/TestSP_InviteToSignUp.java
+++ src/spsv/test/com/aerofs/sp/server/integration/TestSP_InviteToSignUp.java
@@ -4,16 +4,16 @@
 
 package com.aerofs.sp.server.integration;
 
-import com.aerofs.lib.ex.ExAlreadyExist;
 import com.aerofs.lib.ex.ExBadArgs;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.Collections;
 
 /**
  * A class to test the get device info SP call.
  */
-public class TestSP_InviteUser extends AbstractSPTest
+public class TestSP_InviteToSignUp extends AbstractSPTest
 {
     @Test(expected = ExBadArgs.class)
     public void inviteUser_shouldThrowOnEmptyInviteeList()
@@ -24,12 +24,14 @@ public class TestSP_InviteUser extends AbstractSPTest
         service.inviteToSignUp(Collections.<String>emptyList());
     }
 
-    @Test(expected = ExAlreadyExist.class)
-    public void inviteUser_shouldThrowIfUserAlreadyExists()
+    @Test
+    public void inviteUser_shouldIgnoreIfUserAlreadyExists()
             throws Exception
     {
         setSessionUser(USER_1);
 
         service.inviteToSignUp(Collections.singletonList(USER_2.toString()));
+
+        Mockito.verifyZeroInteractions(factEmailer);
     }
 }
