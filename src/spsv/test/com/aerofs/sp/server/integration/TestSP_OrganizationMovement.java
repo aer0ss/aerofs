e95928e269ce18c2764c8fb780ea7090f37df2a0
Matt Pillar <matt@aerofs.com>
2013-01-07 15:32:06 -0800

    Downgrade a user to non-admin when moving organizations.
    Change-Id: I09afe4239db1ba8b3e4e170ef9eb05b7be614210

--- src/spsv/test/com/aerofs/sp/server/integration/TestSP_OrganizationMovement.java
+++ src/spsv/test/com/aerofs/sp/server/integration/TestSP_OrganizationMovement.java
@@ -8,6 +8,8 @@ import com.aerofs.base.id.UserID;
 import com.aerofs.lib.ex.ExAlreadyExist;
 import com.aerofs.lib.ex.ExAlreadyInvited;
 import com.aerofs.lib.ex.ExNoPerm;
+import com.aerofs.proto.Sp.PBAuthorizationLevel;
+import com.aerofs.proto.Sp.GetAuthorizationLevelReply;
 import com.aerofs.proto.Sp.GetOrganizationInvitationsReply;
 import org.junit.Test;
 
@@ -135,5 +137,27 @@ public class TestSP_OrganizationMovement extends AbstractSPTest
         }
 
         acceptSpecificInvitation(orgID);
+    }
+
+    @Test
+    public void shouldDowngradeToNonAdminWhenMovingOrganization()
+            throws Exception
+    {
+        // Make user 1 and admin of a new organization.
+        setSessionUser(USER_1);
+        addOrganization();
+
+        // User 2 is also an admin, and invites user 1 to thier org.
+        setSessionUser(USER_2);
+        addOrganization();
+        sendInvitation(USER_1);
+
+        // User 1 accept invite to user 2's org.
+        setSessionUser(USER_1);
+        acceptFirstInvitation();
+
+        // Verify user 1 is not an admin in user 2's organization.
+        GetAuthorizationLevelReply reply = service.getAuthorizationLevel().get();
+        assertEquals(PBAuthorizationLevel.USER, reply.getLevel());
     }
 }
