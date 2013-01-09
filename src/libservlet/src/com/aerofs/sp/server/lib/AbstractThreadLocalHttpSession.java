65eabef8da5b914603e4130c70788ed48502bd19
Matt Pillar <matt@aerofs.com>
2013-01-08 16:35:45 -0800

    Update comment in abstract thread local http session.
    Change-Id: I430df56835255fabd467296b1cd92ba0002ab625

--- src/libservlet/src/com/aerofs/sp/server/lib/AbstractThreadLocalHttpSession.java
+++ src/libservlet/src/com/aerofs/sp/server/lib/AbstractThreadLocalHttpSession.java
@@ -6,8 +6,10 @@ package com.aerofs.sp.server.lib;
 
 import javax.servlet.http.HttpSession;
 
-// TODO (WW) FIXME The correlation of sessions and threads are accidental. It is incorrect to use
-// thread-local storage to simulate session-local storage.
+/**
+ * Use a thread local object here to hold session data because each individual tomcat request
+ * is guaranteed by tomcat to be processed in its own thread.
+ */
 public class AbstractThreadLocalHttpSession
 {
     protected static final ThreadLocal<HttpSession> _session = new ThreadLocal<HttpSession>();
