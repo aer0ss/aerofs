c7956c6a69c505596a63b37284afb9a21825eff6
Hugues Bruant <hugues@aerofs.com>
2013-02-27 10:57:02 -0800

    Add -h switch to rm
    Explicit revsion history cleanup ftw:
    
    rm -rh foo bar baz
    will delete all version history under folders foo, bar and baz
    
    rm -h foo revindex
    will delete a specific revision of file foo
    
    Change-Id: Icde14c5462f6d93275cb223e53234cfe0dbea597

--- src/gui/src/com/aerofs/shell/CmdRm.java
+++ src/gui/src/com/aerofs/shell/CmdRm.java
@@ -6,6 +6,7 @@ import com.aerofs.lib.ex.ExNotFile;
 import com.aerofs.proto.Common.PBPath;
 import com.aerofs.proto.Objects.PBObjectAttributes;
 import com.aerofs.proto.Objects.PBObjectAttributes.Type;
+import com.google.protobuf.ByteString;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 
@@ -16,16 +17,30 @@ public class CmdRm implements IShellCommand<ShProgram>
     {
         if (cl.getArgs().length == 0) throw new ExBadArgs();
 
-        for (String arg : cl.getArgs()) {
-            PBPath path = s.d().buildPath_(arg);
+        if (cl.hasOption('h')) {
+            if (cl.hasOption('r')) {
+                for (String arg : cl.getArgs()) {
+                    PBPath path = s.d().buildPath_(arg);
+                    s.d().getRitualClient_().deleteRevision(path, null);
+                }
+            } else {
+                if (cl.getArgs().length != 2) throw new ExBadArgs();
+                PBPath path = s.d().buildPath_(cl.getArgs()[0]);
+                ByteString index = ByteString.copyFromUtf8(cl.getArgs()[1]);
+                s.d().getRitualClient_().deleteRevision(path, index);
+            }
+        } else {
+            for (String arg : cl.getArgs()) {
+                PBPath path = s.d().buildPath_(arg);
 
-            PBObjectAttributes attr = s.d().getRitualClient_()
-                    .getObjectAttributes(Cfg.user().getString(), path)
-                    .getObjectAttributes();
+                PBObjectAttributes attr = s.d().getRitualClient_()
+                        .getObjectAttributes(Cfg.user().getString(), path)
+                        .getObjectAttributes();
 
-            if (!(attr.getType() == Type.FILE || cl.hasOption('r'))) throw new ExNotFile();
+                if (!(attr.getType() == Type.FILE || cl.hasOption('r'))) throw new ExNotFile();
 
-            s.d().getRitualClient_().deleteObject(path);
+                s.d().getRitualClient_().deleteObject(path);
+            }
         }
     }
 
@@ -44,12 +59,14 @@ public class CmdRm implements IShellCommand<ShProgram>
     @Override
     public String getOptsSyntax()
     {
-        return "[PATH]...";
+        return "[[PATH]...|PATH REVINDEX]";
     }
 
     @Override
     public Options getOpts()
     {
-        return new Options().addOption("r", "recursive", false, "recursively delete directories");
+        return new Options()
+                .addOption("r", "recursive", false, "recursively delete directories")
+                .addOption("h", "history", false, "delete old version(s)");
     }
 }
