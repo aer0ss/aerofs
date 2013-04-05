e50f750851386fd37fad4fad21ee3993eea01e68
Peter Hamilton <Peter@aerofs.com>
2013-04-05 21:15:51 +0000

    Revert "Removed staging syncstat from c.aerofs.com"
    This reverts commit af046f694fe101105fb4c9958ea553cc7b784bbf
    
    Change-Id: I114d600f2ec116e6b4795857923a43dd46674c35

--- puppetmaster/manifests/nodes/c.pp
+++ puppetmaster/manifests/nodes/c.pp
@@ -8,5 +8,26 @@ node "c.aerofs.com" inherits default {
     # Include the cmd service
     include cmd
 
+    ### STAGING SYNCSTAT ####
+    # Since we need a staging service, we need the staging cacert
+    $cacert_location = "/etc/ssl/certs/AeroFS_CA_staging.pem"
+    file{$cacert_location:
+        source => "puppet:///aerofs_cacert/cacert-staging.pem",
+        ensure => present,
+    }
+
+    # Fetch variables from hiera
+    $mysql_sp = hiera("mysql_sp")
+    $mysql_endpoint = hiera("mysql_endpoint")
+
+    # install syncstat servlet
+    class{"servlet::syncstat":
+        mysql_sp_password       => $mysql_sp["password"],
+        mysql_endpoint          => $mysql_endpoint,
+        verkehr_host            => "staging.aerofs.com",
+        cacert_location         => $cacert_location
+    }
+    ### END STAGING SYNCSTAT ###
+
     include dbtools
 }
