946f2344ff7be9b1e61deabd62341529fa734d94
Drew Fisher <drew@aerofs.com>
2013-05-01 16:42:01 -0700

    Client: recertify device if cert not signed by CA
    This functionality is needed as we push out a new CA.
    
    This commit only handles the single-user case.  Team Servers will
    require additional user interaction.
    
    Change-Id: Id4078c9f525627b158983c425ed96e93214bd025

---
+++ src/gui/src/com/aerofs/launch/LaunchTask.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Air Computing Inc., 2013.
+ */
+
+package com.aerofs.launch;
+
+import com.aerofs.lib.event.AbstractEBSelfHandling;
+import com.aerofs.lib.sched.ExponentialRetry;
+import com.aerofs.lib.sched.IScheduler;
+
+import java.util.concurrent.Callable;
+
+// This class is almost exactly like RunAtLeastOnce, except that RALO
+// gets to use injection, whereas this is in the GUI and has to have
+// things instantiated manually
+abstract class LaunchTask extends AbstractEBSelfHandling
+{
+    protected final IScheduler _sched;
+    LaunchTask(IScheduler sched)
+    {
+        _sched = sched;
+    }
+    public void schedule()
+    {
+        _sched.schedule(this, 0);
+    }
+
+    @Override
+    public void handle_()
+    {
+        new ExponentialRetry(_sched).retry(getClass().getSimpleName(), new Callable<Void>()
+        {
+            @Override
+            public Void call() throws Exception
+            {
+                run_();
+                return null;
+            }
+        });
+    }
+
+    protected abstract void run_() throws Exception;
+}
