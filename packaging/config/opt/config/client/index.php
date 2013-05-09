142e97b6c6638580e6b99fdea921240375ff6c2e
Matt Pillar <matt@aerofs.com>
2013-05-09 14:23:24 -0700

    Clean up aerofs-config and fix pd-aerofs nginx configuration.
    Change-Id: I4ab546079ac741d75463a09932f9f42b2f7916cd

--- packaging/config/opt/config/client/index.php
+++ packaging/config/opt/config/client/index.php
@@ -1,7 +1,7 @@
 <?php
 
-$common = file_get_contents('/opt/config/common.properties');
-$client = file_get_contents('/opt/config/client.properties');
+$common = file_get_contents('/opt/config/properties/common.properties');
+$client = file_get_contents('/opt/config/properties/client.properties');
 
 echo $common;
 echo $client;
