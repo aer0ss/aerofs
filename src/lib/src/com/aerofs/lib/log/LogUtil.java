9902b418f6fb7aa6020d47d7c0fb64ab4725f925
Jon Pile <jon@aerofs.com>
2013-05-18 14:11:47 -0700

    Fix compilation error
    I told you varargs and arrays-of-things did not play nicely!!
    
    Change-Id: I54458ff8efe62809fc3d2464c13556bae823bc81

--- src/lib/src/com/aerofs/lib/log/LogUtil.java
+++ src/lib/src/com/aerofs/lib/log/LogUtil.java
@@ -99,8 +99,7 @@ public abstract class LogUtil
      * Suppress the stack trace if the throwable is an instance of one of the given
      * exception types.
      */
-    public static <T extends Throwable> T suppress(T throwable,
-            Class<? extends Throwable>... suppressTypes)
+    public static <T extends Throwable> T suppress(T throwable, Class<?>... suppressTypes)
     {
         for (Class<?> clazz : suppressTypes) {
             if (clazz.isInstance(throwable)) {
