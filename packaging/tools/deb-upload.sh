205c616ec25b6abc62916392c492d0c6e54c87cb
Matt Pillar <matt@aerofs.com>
2013-05-17 18:33:14 -0700

    Fix permissions on apt again.
    Change-Id: I678a08b08b00f156286b2399924280449bf6d264

--- packaging/tools/deb-upload.sh
+++ packaging/tools/deb-upload.sh
@@ -55,7 +55,7 @@ ssh $APT_SERVER \
     echo Copy debs to /var/www; \
     cp ~/$DEBS_FOLDER/*.ver /var/www/ubuntu/$TARGET_REPOSITORY_DIRECTORY/versions; \
     sudo chown root:admin /var/www/ubuntu/$TARGET_REPOSITORY_DIRECTORY/versions/*; \
-    sudo chmod 663 /var/www/ubuntu/$TARGET_REPOSITORY_DIRECTORY/versions/*; \
+    sudo chmod 666 /var/www/ubuntu/$TARGET_REPOSITORY_DIRECTORY/versions/*; \
     echo Call reprepro includedeb; \
     sudo reprepro --gnupghome=/root/.gnupg includedeb precise ~/$DEBS_FOLDER/*.deb; \
     echo Remove old debs; \
