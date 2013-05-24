b354a660d791e49f9f21d4fc969655c1116185dc
Jon Pile <jon@aerofs.com>
2013-05-24 17:48:09 +0000

    jP: This breaks ci-vmhost (python 2.6 environment)
    Revert "modify systest setup script to clear s3 bucket"
    
    This reverts commit 9eaf185f0fba31e37b663b47f298a22a0f66f064
    
    Change-Id: Ibacb485acaf9b579be26b799b7b9d82ba8dc5d0c

--- tools/systest-setup.py
+++ tools/systest-setup.py
@@ -13,7 +13,6 @@ import sys
 import requests
 import json
 import getpass
-from boto.s3.connection import S3Connection
 
 file_root = os.path.dirname(__file__)
 python_aerofs_lib = os.path.join(file_root,"../src/python-lib")
@@ -146,13 +145,6 @@ def generate_yaml(args, username):
     fs.close()
 
 
-def clear_s3_bucket(access_key, secret_key, bucket_id):
-    s3conn = S3Connection(access_key, secret_key)
-    bucket = s3conn.get_bucket(bucket_id)
-    bucket.delete_keys(bucket.list())
-
-
-
 #######################
 ##   MAIN FUNCTION   ##
 #######################
@@ -206,12 +198,6 @@ def main():
         username = [create_user(args.userid, args.password) for i in xrange(total_users)]
     else:
         username = create_user(args.userid, args.password)
-
-    # Clear S3 bucket if necessary
-    if args.include_s3_ts:
-        clear_s3_bucket(S3_DETAILS['s3_access_key'],
-                        S3_DETAILS['s3_secret_key'],
-                        S3_DETAILS['s3_bucket_id'])
 
     # Generate YAML file
     generate_yaml(args, username)
