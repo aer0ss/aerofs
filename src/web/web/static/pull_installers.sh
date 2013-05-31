10eb2baa0f76f1fa2976c7df918ee4995c8bebf3
Matt Pillar <matt@aerofs.com>
2013-05-31 13:25:35 -0700

    Fix pull installers script.
    Change-Id: I6639d8595d339864e3b46de3c5c894105e0a4646

--- src/web/web/static/pull_installers.sh
+++ src/web/web/static/pull_installers.sh
@@ -1,18 +1,39 @@
 #!/bin/bash -e
 cd $(dirname $0)
+rm -f *.dmg *.deb *.tgz *.exe *.ver *.zip
 
 for file in \
     AeroFSInstall.dmg \
+    aerofs-installer.deb \
+    aerofs-installer.tgz \
     AeroFSInstall.exe \
     AeroFSTeamServerInstall.dmg \
     AeroFSTeamServerInstall.exe \
-    aerofs-installer.deb \
-    aerofs-installer.tgz \
     aerofsts-installer.deb \
     aerofsts-installer.tgz \
+    aerofsts-x86_64.tgz \
+    aerofsts-x86.tgz \
+    aerofs-x86_64.tgz \
+    aerofs-x86.tgz \
     current.ver
 do
-    rm -f $file
+    wget --no-check-certificate https://nocache.client.aerofs.com/$file
+done
+
+version=$(cat current.ver | awk -F'=' '{print $2}')
+
+for file in \
+    aerofs-${version}-x86_64.tgz \
+    aerofs-${version}-x86.tgz \
+    AeroFSInstall-${version}.dmg \
+    AeroFSInstall-${version}.exe \
+    aerofs-osx-${version}.zip \
+    AeroFSTeamServerInstall-${version}.dmg \
+    AeroFSTeamServerInstall-${version}.exe \
+    aerofsts-${version}-x86_64.tgz \
+    aerofsts-${version}-x86.tgz \
+    aerofsts-osx-${version}.zip
+do
     wget --no-check-certificate https://nocache.client.aerofs.com/$file
 done
 
