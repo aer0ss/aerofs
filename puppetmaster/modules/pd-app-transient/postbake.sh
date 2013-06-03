cf725b648944286005d53289fc582492982f1525
Matt Pillar <matt@aerofs.com>
2013-06-03 16:06:21 -0700

    Modify transient postbake task - stop running services.
    Change-Id: Idf3b287e9acd9427c47aa8cf821d176508d57a1d

--- puppetmaster/modules/pd-app-transient/postbake.sh
+++ puppetmaster/modules/pd-app-transient/postbake.sh
@@ -2,3 +2,6 @@
 cd /mnt/image/opt/web/web/static
 ./pull_installers.sh
 echo "Client binaries successfully downloaded."
+
+# Stop pesky services so we can unmount successfully.
+chroot /mnt/image service restund stop || true
