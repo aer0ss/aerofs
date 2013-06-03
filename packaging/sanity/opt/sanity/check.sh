5ccae28f4d70523c489ff79da93cf6a2eca4b16e
Matt Pillar <matt@aerofs.com>
2013-06-03 14:55:47 -0700

    Plumbing for private deployment server sanity checks.
    Change-Id: Iadb8cac67d868aff21f19792131cb9c5c66bf631

---
+++ packaging/sanity/opt/sanity/check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd $(dirname $0)/probes
+
+probes=$(ls *.sh 2>/dev/null)
+
+if [ -z "$probes" ]
+then
+    echo "Error: no probes defined."
+    exit 1
+fi
+
+for probe in $probes
+do
+    ./$probe
+
+    if [ $? -ne 0 ]
+    then
+        echo "Error: probe failed: $(echo $probe | awk -F. '{print $1}')"
+        exit 1
+    fi
+done
