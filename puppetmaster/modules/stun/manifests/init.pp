b19012510df145722e5cdf48973e6f8679f2b117
Matt Pillar <matt@aerofs.com>
2013-06-03 16:56:41 -0700

    Ensure stun service is started via puppet.
    Change-Id: I168747728baa630b7eaf5f59df60b4c7e59fe427

--- puppetmaster/modules/stun/manifests/init.pp
+++ puppetmaster/modules/stun/manifests/init.pp
@@ -7,4 +7,16 @@ class stun {
             Apt::Source["aerofs"]
         ]
     }
+
+    file{"/etc/init.d/restund":
+        ensure => link,
+        target => "/lib/init/upstart-job",
+        require => Package["aerofs-stun"],
+    }
+    
+    service { "restund":
+        ensure => running,
+        provider => upstart,
+        require => File["/etc/init.d/restund"],
+    }
 }
