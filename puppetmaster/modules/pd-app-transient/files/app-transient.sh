20933a67f6e5d2762ba99b592fb630c65c28d51d
Matt Pillar <matt@aerofs.com>
2013-06-05 15:59:27 -0700

    Add bootstrap task to fix restund server config (listen interface).
    Change-Id: I541e9b9a41f236ba561730852603b3bcb51cb3f6

--- puppetmaster/modules/pd-app-transient/files/app-transient.sh
+++ puppetmaster/modules/pd-app-transient/files/app-transient.sh
@@ -3,7 +3,6 @@
 PORT_SCRIPT=/opt/sanity/probes/tools/port.sh
 
 $PORT_SCRIPT localhost 443 "Webadmin and SP"
-$PORT_SCRIPT localhost 3478 "STUN server"
 $PORT_SCRIPT localhost 5222 "Ejabberd"
 $PORT_SCRIPT localhost 8888 "Zephyr relay"
 $PORT_SCRIPT localhost 9293 "Verkehr publish"
