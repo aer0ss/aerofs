c6d48b155a8dd63d55fc9ee32c790f5e135be47e
Matt Pillar <matt@aerofs.com>
2013-06-10 10:20:44 -0700

    Remove downloads page probe.
    Change-Id: I08f48da2a30e2d7bd8523deddc8d2515da4e11a2

--- puppetmaster/manifests/nodes/z.pp
+++ puppetmaster/manifests/nodes/z.pp
@@ -59,7 +59,6 @@ node "z.arrowfs.org" inherits default {
     "verkehr port verkehr.aerofs.com 443",
     "zephyr checkzephyr zephyr.aerofs.com 443",
     "x port x.aerofs.com 443",
-    "download-page url https://www.aerofs.com/download?a=WWtheWise\&b=0",
     # Staging
     "staging port staging.aerofs.com 443",
     "staging port staging.aerofs.com 80",
