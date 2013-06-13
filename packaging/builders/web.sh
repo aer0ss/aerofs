d5dce35441ce59b427f3697669f9790a14ca4076
Matt Pillar <matt@aerofs.com>
2013-06-13 15:51:11 -0700

    Add default version to web package.
    Change-Id: Id1d3388f99ed719c11bf908dd4ddf46af5664aaa

--- packaging/builders/web.sh
+++ packaging/builders/web.sh
@@ -10,7 +10,7 @@ EXTRA=web/opt/web/extra
 
 # Debian-related file copies.
 mkdir -p $DEBIAN
-for f in control postinst prerm postrm
+for f in control postinst prerm postrm conffiles
 do
     cp $RESOURCES/$f $DEBIAN
 done
