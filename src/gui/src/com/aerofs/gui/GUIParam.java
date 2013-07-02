5dfac883332c1b454060e98209e311cff7c7f26b
Hugues Bruant <hugues@aerofs.com>
2013-07-03 00:25:46 +0200

    Attempt to fix bandwidth computation in transfer stats
    Change-Id: Icee4ddfcacfc7a53b05ea440ccccd2b8e20ae258

--- src/gui/src/com/aerofs/gui/GUIParam.java
+++ src/gui/src/com/aerofs/gui/GUIParam.java
@@ -8,6 +8,6 @@ public class GUIParam {
     public static final int MARGIN  = 16;
     // the spacing between major composites
     public static final int MAJOR_SPACING = MARGIN;
-    public static final long STAT_UPDATE_INTERVAL = 5 * C.SEC;
+    public static final long STAT_UPDATE_INTERVAL = 1 * C.SEC;
     public static final int BUTTON_HORIZONTAL_SPACING = OSUtil.isOSX() ? 0 : 5;
 }
--- src/gui/src/com/aerofs/gui/transfers/CompTransferStat.java
+++ src/gui/src/com/aerofs/gui/transfers/CompTransferStat.java
@@ -4,12 +4,14 @@ import com.aerofs.base.Loggers;
 import com.aerofs.gui.GUI;
 import com.aerofs.gui.GUIParam;
 import com.aerofs.gui.Images;
-import com.aerofs.lib.ThreadUtil;
 import com.aerofs.lib.Util;
 import com.aerofs.lib.log.LogUtil;
 import com.aerofs.proto.Files.PBDumpStat;
 import com.aerofs.proto.Files.PBDumpStat.PBTransport;
+import com.aerofs.proto.Ritual.DumpStatsReply;
 import com.aerofs.ui.UIGlobals;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.events.PaintEvent;
@@ -28,7 +30,7 @@ public class CompTransferStat extends Composite
     private final Label _lblIn;
     private final Label _lblOut;
 
-    private long _lastT;
+    private long _lastUp;
     private long _lastIn;
     private long _lastOut;
 
@@ -45,27 +47,8 @@ public class CompTransferStat extends Composite
         gridLayout.marginHeight = 0;
         setLayout(gridLayout);
 
-        CLabel label_1 = new CLabel(this, SWT.NONE);
-        label_1.setImage(Images.get(Images.ICON_ARROW_DOWN));
-        _lblIn = new Label(this, SWT.NONE);
-        // N.B. the label is initialized to a really long text so the initial layout
-        //   allocates sufficient space for the values we are going to display.
-        // The advantage of this approach is that we don't need to redo layout every
-        //   time the text changes, which can get expensive since we do this often.
-        // The disadvantage is that if we ever change the range of text we are going
-        //   to display, this will need to change as well.
-        _lblIn.setText("99999.99 bytes/s");
-
-        CLabel label = new CLabel(this, SWT.NONE);
-        label.setImage(Images.get(Images.ICON_ARROW_UP));
-        _lblOut = new Label(this, SWT.NONE);
-        // N.B. the label is initialized to a really long text so the initial layout
-        //   allocates sufficient space for the values we are going to display.
-        // The advantage of this approach is that we don't need to redo layout every
-        //   time the text changes, which can get expensive since we do this often.
-        // The disadvantage is that if we ever change the range of text we are going
-        //   to display, this will need to change as well.
-        _lblOut.setText("99999.99 bytes/s");
+        _lblIn = addBandwidthLabel_(this, Images.ICON_ARROW_DOWN);
+        _lblOut = addBandwidthLabel_(this, Images.ICON_ARROW_UP);
 
         addPaintListener(new PaintListener() {
             @Override
@@ -80,13 +63,22 @@ public class CompTransferStat extends Composite
             }
         });
 
-        new Thread(new Runnable() {
-            @Override
-            public void run()
-            {
-                thdRefresh();
-            }
-        }, "refresh-stat").start();
+        GUI.get().timerExec(0, _scheduleRefresh);
+    }
+
+    private static Label addBandwidthLabel_(Composite parent, String img)
+    {
+        CLabel icon = new CLabel(parent, SWT.NONE);
+        icon.setImage(Images.get(img));
+        Label lbl = new Label(parent, SWT.NONE);
+        // N.B. the label is initialized to a really long text so the initial layout
+        //   allocates sufficient space for the values we are going to display.
+        // The advantage of this approach is that we don't need to redo layout every
+        //   time the text changes, which can get expensive since we do this often.
+        // The disadvantage is that if we ever change the range of text we are going
+        //   to display, this will need to change as well.
+        lbl.setText("99999.99 bytes/s");
+        return lbl;
     }
 
     private final PBDumpStat TEMPLATE = PBDumpStat.newBuilder()
@@ -94,62 +86,67 @@ public class CompTransferStat extends Composite
         .addTransport(PBTransport.newBuilder().setBytesIn(0).setBytesOut(0))
         .build();
 
-    private void thdRefresh()
-    {
-        while (true) {
-            if (isDisposed()) break;
+    private final Runnable _scheduleRefresh = new Runnable() {
+        @Override
+        public void run()
+        {
+            Futures.addCallback(UIGlobals.ritualNonBlocking().dumpStats(TEMPLATE),
+                    new FutureCallback<DumpStatsReply>() {
+                        @Override
+                        public void onSuccess(final DumpStatsReply reply)
+                        {
+                            GUI.get().safeAsyncExec(CompTransferStat.this, new Runnable() {
+                                @Override
+                                public void run()
+                                {
+                                    refresh(reply.getStats());
+                                }
+                            });
+                        }
 
-            String strIn, strOut;
-
-            try {
-                PBDumpStat data = UIGlobals.ritual().dumpStats(TEMPLATE).getStats();
-                long in = 0, out = 0;
-                for (PBTransport transport : data.getTransportList()) {
-                    in += transport.getBytesIn();
-                    out += transport.getBytesOut();
-                }
-
-                if (_lastT == 0) _lastT = data.getUpTime();
-
-                long now = System.currentTimeMillis();
-                long deltaIn = in - _lastIn;
-                long deltaOut = out - _lastOut;
-                long deltaT = now - _lastT;
-
-                _lastIn = in;
-                _lastOut = out;
-                _lastT = now;
-
-                strIn = Util.formatBandwidth(deltaIn, deltaT);
-                strOut = Util.formatBandwidth(deltaOut, deltaT);
-
-            } catch (Exception e) {
-                l.warn("can't refresh stat: ", LogUtil.suppress(e, ClosedChannelException.class));
-                strIn = Util.formatBandwidth(0, 0);
-                strOut = Util.formatBandwidth(0, 0);
-            }
-
-            final String strInFinal = strIn;
-            final String strOutFinal = strOut;
-
-            if (isDisposed()) break;
-            GUI.get().safeExec(this, new Runnable() {
-                @Override
-                public void run()
-                {
-                    _lblIn.setText(strInFinal);
-                    _lblOut.setText(strOutFinal);
-                    _inited = true;
-                }
-            });
-
-            ThreadUtil.sleepUninterruptable(GUIParam.STAT_UPDATE_INTERVAL);
+                        @Override
+                        public void onFailure(Throwable e)
+                        {
+                            l.warn("can't refresh stat: ",
+                                    LogUtil.suppress(e, ClosedChannelException.class));
+                            GUI.get().safeAsyncExec(CompTransferStat.this, new Runnable() {
+                                @Override
+                                public void run()
+                                {
+                                    refresh(0, 0, 0);
+                                }
+                            });
+                        }
+                    });
         }
+    };
+
+    private void refresh(PBDumpStat data)
+    {
+        long in = 0, out = 0;
+        for (PBTransport transport : data.getTransportList()) {
+            in += transport.getBytesIn();
+            out += transport.getBytesOut();
+        }
+
+        // if the daemon is restarted while transfers were ongoing, the first batch
+        // of values will yield negative deltas, hence the use of Math.max
+        long deltaIn = Math.max(0, in - _lastIn);
+        long deltaOut = Math.max(0, out - _lastOut);
+        long deltaT = Math.max(0, data.getUpTime() - _lastUp);
+
+        _lastIn = in;
+        _lastOut = out;
+        _lastUp = data.getUpTime();
+
+        refresh(deltaIn, deltaOut, deltaT);
     }
 
-    @Override
-    protected void checkSubclass()
+    private void refresh(long deltaIn, long deltaOut, long deltaT)
     {
-        // Disable the check that prevents subclassing of SWT components
+        _lblIn.setText(Util.formatBandwidth(deltaIn, deltaT));
+        _lblOut.setText(Util.formatBandwidth(deltaOut, deltaT));
+        _inited = true;
+        GUI.get().timerExec(GUIParam.STAT_UPDATE_INTERVAL, _scheduleRefresh);
     }
 }
