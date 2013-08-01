63dd4adc96a6003b0ef15dd1a5caa055cfccfea8
Allen George <allen@aerofs.com>
2013-08-01 15:30:00 -0700

    Have lptool check if apt.aerofs.com is reachable
    - Check if apt is reachable before any package step
    - Refactor common code into separate functions
    - Rename deploy -> deploy-all and install -> deploy; rename command in
      lpctrl for consistency
    
    Change-Id: I26a3306604119db7e5acf051e46c376aea8c6b06

--- tools/bashrc/lp-aliases.sh
+++ tools/bashrc/lp-aliases.sh
@@ -5,12 +5,13 @@ if [[ ! -x $LPTOOL ]]; then
     echo "The lptool script (expected at $LPTOOL) is not found."
 fi
 
-alias lp-deploy="$LPTOOL deploy"
 alias lp-create="$LPTOOL create"
 alias lp-delete="$LPTOOL delete"
 alias lp-halt="$LPTOOL halt"
 alias lp-start="$LPTOOL start"
 alias lp-ssh="$LPTOOL ssh"
 alias lp-kick="$LPTOOL kick"
-alias lp-install="$LPTOOL install"
+alias lp-kick-all="$LPTOOL kick-all"
+alias lp-deploy="$LPTOOL deploy"
+alias lp-deploy-all="$LPTOOL deploy-all"
 alias lptool=$LPTOOL
