5d0e5315108cb4a3dd6c2e30802c37c327f76c05
Jon Pile <jon@aerofs.com>
2013-08-02 16:13:40 -0700

    Removed subshells and used Enhanced Cleverness
    This works on Linux.
    
    Change-Id: Ic8f598165394a7f409d89a19ac908ec7fba72c30

--- puppetmaster/modules/transient/files/probes/restund.sh
+++ puppetmaster/modules/transient/files/probes/restund.sh
@@ -3,8 +3,9 @@
 # We're looking for a line in /etc/restund.conf that looks like:
 # tcp_listen           192.168.51.141:3478
 
-addr=$(grep ^tcp_listen /etc/restund.conf | sed 's/\s\s*/ /g' | cut -d ' ' -f 2)
-ip=$(echo $addr | cut -d ':' -f 1)
-port=$(echo $addr | cut -d ':' -f 2)
+typeset line=$(grep ^tcp_listen /etc/restund.conf)
+typeset addr=${line##* }
+ip=${addr%%:*}
+port=${addr##*:}
 
 /opt/sanity/probes/tools/port.sh $ip $port
