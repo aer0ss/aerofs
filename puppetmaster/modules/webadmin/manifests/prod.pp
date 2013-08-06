e9114ebfa61726ed04bfedc471c4da57c47f9d66
Drew Fisher <drew@aerofs.com>
2013-08-05 20:05:35 -0700

    webadmin: Fix syntax error in puppet manifest

--- puppetmaster/modules/webadmin/manifests/prod.pp
+++ puppetmaster/modules/webadmin/manifests/prod.pp
@@ -1,8 +1,8 @@
-class webadmin::prod inherits webadmin(
+class webadmin::prod (
     $stripe_publishable_key,
     $stripe_secret_key,
     $uwsgi_port = 8080
-) {
+) inherits webadmin {
     file {"/opt/web/production.ini":
         source => "puppet:///modules/webadmin/production.ini",
         require => Package["aerofs-web"],
