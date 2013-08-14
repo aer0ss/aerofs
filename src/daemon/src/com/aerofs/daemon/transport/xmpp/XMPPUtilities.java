3f731b086eedc91274fb5532115509efb64a50df
Gregory Schlomoff <greg@aerofs.com>
2013-08-14 16:36:56 -0700

    Fixing imports

--- src/daemon/src/com/aerofs/daemon/transport/xmpp/XMPPUtilities.java
+++ src/daemon/src/com/aerofs/daemon/transport/xmpp/XMPPUtilities.java
@@ -21,8 +21,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
-import java.net.NetworkInterface;
-import java.util.Set;
 
 import static com.aerofs.daemon.lib.DaemonParam.MAX_TRANSPORT_MESSAGE_SIZE;
 
