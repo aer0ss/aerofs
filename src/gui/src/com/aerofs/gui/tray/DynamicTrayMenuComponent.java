aer0ss/aerofs

commit f195d2d141d8d0d979c16398abcf0884f8e6661b

Fix tray menu indexing updates
The spinner timer now causes a menu update
Fix refresh of the file/folder count

Change-Id: Iab1cbcdd94740d6eb13b70265f5346bc12e99a26

src/gui/src/com/aerofs/gui/tray/DynamicTrayMenuComponent.java
@@ -1,0 +1,86 @@
+package com.aerofs.gui.tray;
+
+import com.aerofs.gui.GUI;
+import com.aerofs.ui.UIParam;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import org.eclipse.swt.widgets.UbuntuTrayItem;
+
+import java.util.List;
+
+/**
+ * Abstract base class for all tray menu components that need to be dynamically
+ * refreshed to display new information (e.g. in reaction to a Ritual notification)
+ *
+ * This class makes it easy to rate limit refresh operations, which is particularly
+ * important to properly support systems using libappindicator
+ */
+public abstract class DynamicTrayMenuComponent implements ITrayMenuComponent
+{
+    // We need to refresh slowly on Ubuntu, to avoid making the menu unnavigable
+    private static final long REFRESH_TIME = UIParam.SLOW_REFRESH_DELAY * 2;
+
+    private final long _rate;
+    private long _lastNotify;
+    private boolean _scheduled;
+
+    private final Runnable _notifier;
+
+    private final List<ITrayMenuComponentListener> _listeners = Lists.newArrayList();
+
+    public DynamicTrayMenuComponent()
+    {
+        _rate = REFRESH_TIME;
+        _notifier = new Runnable() {
+            @Override
+            public void run()
+            {
+                refresh();
+            }
+        };
+    }
+
+    /**
+     * Force a refresh of the whole menu
+     *
+     * @pre must be called from the UI thread
+     */
+    protected synchronized void refresh()
+    {
+        Preconditions.checkState(GUI.get().isUIThread());
+
+        _lastNotify = System.currentTimeMillis();
+        _scheduled = false;
+
+        for (ITrayMenuComponentListener listener : _listeners) {
+            listener.onTrayMenuComponentChange();
+        }
+    }
+
+    public synchronized void scheduleUpdate()
+    {
+        if (UbuntuTrayItem.supported()) {
+            // throttled refresh when using libappindicator
+            if (!_scheduled) {
+                long now = System.currentTimeMillis();
+                _scheduled = true;
+                GUI.get().timerExec(Math.max(0, _lastNotify + _rate - now), _notifier);
+            }
+        } else {
+            // non-throttled async update when not using libappindicator
+            GUI.get().asyncExec(new Runnable() {
+                @Override
+                public void run()
+                {
+                    updateInPlace();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void addListener(ITrayMenuComponentListener l)
+    {
+        _listeners.add(l);
+    }
+}

src/gui/src/com/aerofs/gui/Images.java
@@ -62,7 +62,7 @@ public class Images {
     private static final Map<String, Image> s_imgs = new HashMap<String, Image>();
 
     // delay (ms) between each spinner frame
-    private static final int SPINNER_DELAY = 50;
+    private static final int SPINNER_DELAY = 100;
     private static Image[] s_spinner_frames;
 
     private static Cursor s_cursors[];

src/gui/src/com/aerofs/gui/tray/IndexingTrayMenuSection.java
@@ -7,28 +7,23 @@ package com.aerofs.gui.tray;
 import com.aerofs.gui.AbstractSpinAnimator;
 import com.aerofs.gui.GUI;
 import com.aerofs.gui.tray.IndexingPoller.IIndexingCompletionListener;
-import com.aerofs.proto.RitualNotifications.PBIndexingProgress;
 import com.aerofs.proto.RitualNotifications.PBNotification;
 import com.aerofs.proto.RitualNotifications.PBNotification.Type;
 import com.aerofs.ui.RitualNotificationClient.IListener;
 import com.aerofs.ui.UI;
-import com.google.common.base.Preconditions;
-import com.google.common.collect.Lists;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.UbuntuTrayItem;
 
-import java.util.List;
-
-public class IndexingTrayMenuSection
-        implements IListener, IIndexingCompletionListener, ITrayMenuComponent
+public class IndexingTrayMenuSection extends DynamicTrayMenuComponent
+        implements IListener, IIndexingCompletionListener
 {
     private MenuItem _indexingStats1;
     private MenuItem _indexingStats2;
     private Menu _cachedMenu;
 
     private final IndexingPoller _indexingPoller;
-    private final List<ITrayMenuComponentListener> _listeners;
 
     private Image _cachedCurrentImage;
     private String _cachedProgressString;
@@ -38,24 +33,21 @@ public class IndexingTrayMenuSection
 
     public IndexingTrayMenuSection(IndexingPoller indexingPoller)
     {
-        _listeners = Lists.newArrayList();
         _indexingPoller = indexingPoller;
         _indexingPoller.addListener(this);
 
-        _animator = new AbstractSpinAnimator(null) {
-            @Override
-            protected void setImage(Image img)
-            {
-                _cachedCurrentImage = img;
-            }
-        };
+        // no spinner for libappindicator-based menu: refresh rate insufficient
+        if (!UbuntuTrayItem.supported()) {
+            _animator = new AbstractSpinAnimator(null) {
+                @Override
+                protected void setImage(Image img)
+                {
+                    _cachedCurrentImage = img;
+                    updateInPlace();
+                }
+            };
+        }
         UI.rnc().addListener(this);
-    }
-
-    @Override
-    public void addListener(ITrayMenuComponentListener l)
-    {
-        _listeners.add(l);
     }
 
     @Override
@@ -67,7 +59,7 @@ public class IndexingTrayMenuSection
         _indexingStats1.setEnabled(false);
         updateInPlace();
 
-        if (!_animatorStarted) {
+        if (_animator != null && !_animatorStarted) {
             _animatorStarted = true;
             _animator.start();
         }
@@ -84,8 +76,9 @@ public class IndexingTrayMenuSection
                     !_indexingStats1.isDisposed()) {
                 _indexingStats1.setImage(_cachedCurrentImage);
             }
+
             if (_cachedProgressString != null) {
-                if (_indexingStats2 == null) {
+                if (_indexingStats2 == null || _indexingStats2.isDisposed()) {
                     // addMenuItemAfterItem only works on non-null, non-disposed items
                     if (_indexingStats1 != null && !_indexingStats1.isDisposed()) {
                         TrayMenuPopulator p = new TrayMenuPopulator(_cachedMenu);
@@ -93,30 +86,9 @@ public class IndexingTrayMenuSection
                         _indexingStats2.setEnabled(false);
                     }
                 } else {
-                    if (!_indexingStats2.isDisposed()) {
-                        _indexingStats2.setText(_cachedProgressString);
-                    }
+                    _indexingStats2.setText(_cachedProgressString);
                 }
             }
-        }
-    }
-
-    // Called on GUI thread.
-    private void updateProgress(PBIndexingProgress progress)
-    {
-        Preconditions.checkState(GUI.get().isUIThread());
-        _cachedProgressString = progress.getFiles() + " files in " +
-                progress.getFolders() + " folders";
-        updateInPlace();
-        notifyListeners();
-    }
-
-    // N.B. this function must be called from the GUI thread.
-    private void notifyListeners()
-    {
-        Preconditions.checkState(GUI.get().isUIThread());
-        for (ITrayMenuComponentListener listener : _listeners) {
-            listener.onTrayMenuComponentChange();
         }
     }
 
@@ -124,16 +96,11 @@ public class IndexingTrayMenuSection
     public void onNotificationReceived(final PBNotification pb)
     {
         if (pb.getType() == Type.INDEXING_PROGRESS) {
-            GUI.get().asyncExec(new Runnable()
-            {
-                @Override
-                public void run()
-                {
-                    if (_indexingStats1 != null && !_indexingStats1.isDisposed()) {
-                        updateProgress(pb.getIndexingProgress());
-                    }
-                }
-            });
+            _cachedProgressString =
+                    pb.getIndexingProgress().getFiles() + " files in " +
+                    pb.getIndexingProgress().getFolders() + " folders";
+
+            scheduleUpdate();
         }
     }
 
@@ -159,8 +126,9 @@ public class IndexingTrayMenuSection
                 }
                 _indexingStats1 = null;
                 _indexingStats2 = null;
+
                 // Trigger a menu refresh
-                notifyListeners();
+                refresh();
             }
         });
     }

src/gui/src/com/aerofs/gui/tray/TransferTrayMenuSection.java
@@ -14,8 +14,6 @@ import com.aerofs.lib.S;
 import com.aerofs.lib.Util;
 import com.aerofs.lib.os.OSUtil;
 import com.aerofs.proto.RitualNotifications.PBTransferEvent;
-import com.aerofs.ui.UIParam;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
@@ -24,17 +22,12 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
-import org.eclipse.swt.widgets.UbuntuTrayItem;
 
-import java.util.List;
 import java.util.Map;
 
-public class TransferTrayMenuSection implements ITrayMenuComponent
+public class TransferTrayMenuSection extends DynamicTrayMenuComponent
+        implements ITransferStateChangedListener
 {
-    // We need to refresh more slowly on Ubuntu, to avoid making the menu unnavigable
-    private static final long MULTIPLIER = UbuntuTrayItem.supported() ? 2 : 1;
-    private static final long REFRESH_TIME = UIParam.SLOW_REFRESH_DELAY * MULTIPLIER;
-
     private MenuItem _transferStats1;    // menu item used to display information about
                                          // ongoing transfers - line 1
     private MenuItem _transferStats2;    // menu item used to display information about
@@ -67,27 +60,14 @@ public class TransferTrayMenuSection implements ITrayMenuComponent
         }
     };
 
-    private final List<ITrayMenuComponentListener> _trayMenuListeners;
-
     private final Map<Integer, Image> _pieChartCache = Maps.newHashMap();
 
     private final TransferState _ts;
-    private final RateLimitedNotifier _notifier;
 
     public TransferTrayMenuSection(TransferState ts)
     {
         _ts = ts;
-
-        _trayMenuListeners = Lists.newArrayList();
-
-        _notifier = new RateLimitedNotifier(REFRESH_TIME);
-        _ts.addListener(_notifier);
-    }
-
-    @Override
-    public void addListener(ITrayMenuComponentListener l)
-    {
-        _trayMenuListeners.add(l);
+        _ts.addListener(this);
     }
 
     @Override
@@ -219,54 +199,12 @@ public class TransferTrayMenuSection implements ITrayMenuComponent
         }
         _pieChartCache.clear();
 
-        _ts.removeListener(_notifier);
+        _ts.removeListener(this);
     }
 
-    private class RateLimitedNotifier implements ITransferStateChangedListener
+    @Override
+    public void onTransferStateChanged(TransferState state)
     {
-        private long _rate;
-        private long _lastNotify;
-        private boolean _scheduled;
-
-        private Runnable _notifier;
-
-        public RateLimitedNotifier(long rate)
-        {
-            _rate = rate;
-
-            _notifier = new Runnable()
-            {
-                @Override
-                public void run()
-                {
-                    for (ITrayMenuComponentListener listener : _trayMenuListeners) {
-                        listener.onTrayMenuComponentChange();
-                    }
-
-                    _lastNotify = System.currentTimeMillis();
-                    _scheduled = false;
-                }
-            };
-        }
-
-        private synchronized void schedule()
-        {
-            if (!_scheduled) {
-                long now = System.currentTimeMillis();
-
-                if (now >= _lastNotify + _rate) {
-                    _notifier.run();
-                } else {
-                    GUI.get().timerExec(_lastNotify + _rate - now, _notifier);
-                    _scheduled = true;
-                }
-            }
-        }
-
-        @Override
-        public void onTransferStateChanged(TransferState state)
-        {
-            schedule();
-        }
+        scheduleUpdate();
     }
 }

commit 0c84a34d6d2f440ae9d66f6edff73d9859366471

Fixed a GUI threading issue leading to AeroFS GUI crashing.
Trigger:
- An _Ubuntu_ client receiving a ritual notification related
to indexing.

How to Reproduce:
- create/modify files under root anchor while AeroFS is _offline_.
- start AeroFS client on _Ubuntu_.
- this specific code path will be executed and leads to AeroFS
crashing with no error message.
* there are other scenarios, this is an easy way to reproduce the
problem.

Root cause:
- the ritual notification handling code in this specific code path
leads to the client calling timerExec() on a non-UI thread.

Other Note:
- this is a regression introduced in commit
f195d2d141d8d0d979c16398abcf0884f8e6661b

src/gui/src/com/aerofs/gui/tray/DynamicTrayMenuComponent.java
@@ -59,24 +59,28 @@ public abstract class DynamicTrayMenuComponent implements ITrayMenuComponent
         }
     }
 
+    /**
+     * Schedule an update to be run on the UI thread. Can be called from any thread.
+     */
     public synchronized void scheduleUpdate()
     {
-        if (UbuntuTrayItem.supported()) {
-            // throttled refresh when using libappindicator
-            if (!_scheduled) {
-                _scheduled = true;
-                GUI.get().timerExec(Math.max(0, _rate - _timer.elapsed()), _notifier);
-            }
-        } else {
-            // non-throttled async update when not using libappindicator
-            GUI.get().asyncExec(new Runnable() {
-                @Override
-                public void run()
-                {
+        GUI.get().asyncExec(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                if (UbuntuTrayItem.supported()) {
+                    // throttled refresh when using libappindicator
+                    if (!_scheduled) {
+                        _scheduled = true;
+                        GUI.get().timerExec(Math.max(0, _rate - _timer.elapsed()), _notifier);
+                    }
+                } else {
+                    // non-throttled async update when not using libappindicator
                     updateInPlace();
                 }
-            });
-        }
+            }
+        });
     }
 
     @Override