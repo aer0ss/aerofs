fd4e679c4f3757162128dc654311d24fb41e03eb
Drew Fisher <drew@aerofs.com>
2013-09-18 16:01:40 -0700

    pip-prefetch.sh: do stricter check of cache
    Something happened on Yuri's laptop that resulted in him building a
    package with an incomplete sdist.  pip unhelpfully returned 0, so
    packaging proceeded.
    
    This change adds some sanity checking to the pip-prefetch script to
    verify that some file with a probably-matching name exists for each of
    the packages specified in the given requirements.txt to try to tease out
    this sort of failure even earlier.
    
    Change-Id: I6dcf651d985aef7c7fda68ac9b55285e13b26aa0

--- packaging/tools/pip-prefetch.sh
+++ packaging/tools/pip-prefetch.sh
@@ -9,7 +9,7 @@
 set -e -u
 
 function DieUsage() {
-    echo "usage: $0 <package name> <requirements file path>"
+    echo "usage: $0 <requirements file path> <path in which to store sdist packages>"
     exit 1
 }
 
@@ -34,23 +34,69 @@ declare -r requirements_checksum_file="$cache_path/cache-checksum"
 declare -r requirements_checksum_line="$( $checksum_program "$requirements_file" )"
 declare -r requirements_checksum=${requirements_checksum_line%% *} # strip out everything after the 40 chars of sha1sum
 
+PackagesForRequirementsAreAllPresent() {
+    # returns 0 if all the files specified in $2 are cached in $1.
+    # returns 1 otherwise
+    # $1 is cache_path
+    # $2 is requirements file
+    # if $3 is nonempty, then failures will be logged to stdout
+    declare arg_cache_path="$1"
+    declare arg_requirements_file="$2"
+    declare arg_verbose="${3:-}"
+    [[ -d "$arg_cache_path" ]] || return 1
+    [[ -r "$arg_requirements_file" ]] || return 1
+    declare -i failcount=0
+    declare -a failures
+    for line in $( cat "$arg_requirements_file" ) ; do
+        package_underscores="${line/-/_}"
+        package_filename_prefix="${package_underscores/==/-}."
+        # We can't know in advance if a package's archive will be a .zip, a
+        # .tar.gz, or something else.  It's annoying that we have to use shell
+        # globbing here, but I couldn't think of another way to tersely test
+        # "does any file that matches this pattern exist".
+        set +e
+        ls "$arg_cache_path/$package_filename_prefix"* > /dev/null 2>&1
+        if [[ $? -ne 0 ]] ; then
+            failures[$failcount]=$line
+            failcount=$failcount+1
+        fi
+        set -e
+    done
+    # Complain about missing packages if verbose mode
+    if [[ "$arg_verbose" != "" ]] && [[ $failcount -ne 0 ]] ; then
+        echo "Missing $failcount packages:"
+        for failure in ${failures[@]}; do
+            echo "    $failure"
+        done
+        echo "See $arg_cache_path/install.log for details."
+    fi
+    if [[ $failcount -ne 0 ]] ; then
+        return 1
+    else
+        return 0
+    fi
+}
+
 # if $CACHE_PATH/cache-checksum doesn't exist, or has a different sha1sum than
 # the source requirements.txt, fetch all the pip source tarballs.
 # (There's no good way to do incremental updates.)
-if [[ ! -r "$requirements_checksum_file" || "$(cat $requirements_checksum_file)" != "$requirements_checksum" ]] ; then
+if [[ ! -r "$requirements_checksum_file" ]] || \
+   [[ "$(cat $requirements_checksum_file)" != "$requirements_checksum" ]] || \
+   ! PackagesForRequirementsAreAllPresent "$cache_path" "$requirements_file"
+then
     echo "Updating cache for $cache_path"
     rm -rf "$cache_path"
     mkdir -p "$cache_path"
     pip install -vvv --no-install --no-deps --ignore-installed --download=$cache_path/ --requirement=$requirements_file > $cache_path/install.log 2>&1
-    # Sadly, when pip bails because the build dir already existed, it fails to
-    # install the package but still returns 0.  *sigh*
-    if grep --quiet "pip can't proceed with requirement" $cache_path/install.log ; then
-        echo "pip fetch failed; see $cache_path/install.log for details"
-        exit 1
-    else
+    # Sanity check - ensure all packages wound up with a name-matching source archive.
+    if PackagesForRequirementsAreAllPresent "$cache_path" "$requirements_file" verbose ; then
         rm $cache_path/install.log
+        echo $requirements_checksum > "$requirements_checksum_file"
+    else
+        echo "See $cache_path/install.log for details."
+        exit 1
     fi
-    echo $requirements_checksum > "$requirements_checksum_file"
 else
     echo "Checksum $requirements_checksum matched cache for $cache_path"
+    echo "All source packages present and accounted for."
 fi
