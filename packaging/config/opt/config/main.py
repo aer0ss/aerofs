95007498ad0f53609154a2cc8e3d3bd382a2f1ec
Drew Fisher <drew@aerofs.com>
2013-10-15 15:49:25 -0700

    config: internalize base_ca_cert
    While it's currently listed as an external property, it's not really
    user-configurable and should be inferred from the certificate on the
    filesystem, if present.
    
    Change-Id: I138ba73b26a67429b93d05347c9e0b1a8fc3f840

--- packaging/config/opt/config/main.py
+++ packaging/config/opt/config/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from os import rename
+import os
 
 # ----------------------------------------------------------------------
 # Statics
@@ -7,6 +7,7 @@ from os import rename
 
 PROPERTIES_EXTERNAL = "/opt/config/properties/external.properties"
 PROPERTIES_EXTERNAL_TMP = "/opt/config/properties/external.properties.tmp"
+CACERT_PATH = "/etc/ssl/certs/AeroFS_CA.pem"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -27,10 +28,25 @@ def write_dict_to_file(d, tmpfilename, dstfilename):
     with open(tmpfilename, 'w') as f:
         for key in d.keys():
             f.write(str(key) + "=" + str(d[key]) + '\n')
-    rename(tmpfilename, dstfilename)
+    os.rename(tmpfilename, dstfilename)
 
 def get_external_variables():
     return read_dict_from_file(PROPERTIES_EXTERNAL)
+
+def get_ca_cert_as_dict():
+    d = {}
+    if os.path.exists(CACERT_PATH):
+        with open(CACERT_PATH) as f:
+            root_cert = f.read().replace("\n", "\\n")
+            d["base_ca_cert"] = root_cert
+    return d
+
+def get_template_kv_pairs():
+    # Get explicit user-provided properties
+    d = get_external_variables()
+    # Add CA cert, if available
+    d = dict(d, **get_ca_cert_as_dict())
+    return d
 
 # ----------------------------------------------------------------------
 # Routes
@@ -38,11 +54,11 @@ def get_external_variables():
 
 @app.route("/client")
 def client_properties():
-    return render_template("client.tmplt", **get_external_variables())
+    return render_template("client.tmplt", **get_template_kv_pairs())
 
 @app.route("/server")
 def server_properties():
-    return render_template("server.tmplt", **get_external_variables())
+    return render_template("server.tmplt", **get_template_kv_pairs())
 
 @app.route("/set", methods=["POST"])
 def set_external_variable():
