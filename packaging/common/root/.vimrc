66b6620e1c017828ce12f7177a0b34f4b9793a71
Drew Fisher <drew@aerofs.com>
2013-10-17 16:13:08 -0700

    Delete packaging/{admin,common,kvm,vmhost}
    Pretty sure no one's built this in the better part of a year; it's
    likely to bitrot, and the less refactoring I have to do of the packaging
    system the better.
    
    Change-Id: Iea893ccffa6ae05c3901405f8b2776ab50c9d29f

--- packaging/common/root/.vimrc
+++
@@ -1,1 +0,0 @@
-../home/aerofs/.vimrc
--- packaging/vmhost/usr/local/aerofs/tools/clean-system.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Deleting auto mount config and restarting..."
-rm -f /etc/auto.master /etc/auto.aerofs
-service autofs restart
-sleep 5
-
-echo "Deleting all local user data..."
-rm -rf /mnt/share
