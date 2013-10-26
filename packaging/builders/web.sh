e6dc46a80d934c55eb1ec32713aff10be1e88a24
Drew Fisher <drew@aerofs.com>
2013-10-25 22:27:40 -0700

    Use distutils for python-lib, make sdist
    It'd be cleaner if aerofs-py-lib installed the same way as the rest of
    our packages.  This commit makes "make web" produce an sdist package for
    python-lib which gets installed with the rest of the packages in the
    postinst script, rather than copying in the whole source tree for
    python-lib.
    
    Independently-distributable packages are good for the soul.
    
    Change-Id: Ie502b73e0db692204a53019dfe54762071351f97

--- packaging/builders/web.sh
+++ packaging/builders/web.sh
@@ -18,12 +18,6 @@ done
 # The postinst script logs to this folder, so ensure it exists
 mkdir -p $OUTPUT_DIR/var/log/web
 
-# Include the entirety of the src/python-lib repo, since we don't package it
-# TODO: package python-lib as tarball and add to requirements.txt?
-EXTRA=$OPT/extra
-mkdir -p $EXTRA
-cp -a ../src/python-lib $EXTRA
-
 # Copy the actual web module to the output dir
 # cp -r is BAD, prefer cp -a or cp -R for OSX compatibility; man 1 cp
 mkdir -p $OPT
@@ -39,7 +33,10 @@ SDIST=$OPT/sdist
 SDIST_CACHE=$HOME/.aerofs-cache/pip/web
 tools/pip-prefetch.sh "../src/web/requirements.txt" "$SDIST_CACHE"
 
-# Copy dependency packages to /opt/web/sdist, so they'll be available at
+# Copy PyPI dependency packages to /opt/web/sdist, so they'll be available at
 # package install time.
 mkdir -p $SDIST
 cp $SDIST_CACHE/* $SDIST/
+
+# Also include the aerofs-py-lib package.
+tools/python-buildpackage.sh "../src/python-lib" "$SDIST" "$OPT/requirements.txt"
---
+++ packaging/tools/python-buildpackage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eu
+
+# Builds an sdist package of the source in folder $1,
+# places the newly-built package in folder $2, and appends
+# an entry to the package to requirements.txt file $3.
+
+PACKAGE_DIR="$1"
+SDIST="$2"
+REQUIREMENTS_TXT="$3"
+
+# Build sdist package
+pushd "$PACKAGE_DIR"
+rm -rf dist
+PACKAGE_NAME=$(python setup.py --name)
+PACKAGE_VER=$(python setup.py --version)
+python setup.py sdist --formats=gztar
+popd
+
+# Place sdist package in specified output folder
+mv "$PACKAGE_DIR"/dist/* "$SDIST/"
+rmdir "$PACKAGE_DIR/dist"
+
+# Append to requirements.txt
+echo "$PACKAGE_NAME==$PACKAGE_VER" >> "$REQUIREMENTS_TXT"
