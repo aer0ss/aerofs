ecbb4a6003ab3a9802ee20f7440764cb19908a93
Alex Tsay <alex@aerofs.com>
2013-11-05 17:32:00 -0800

    Added pythonlib test coverage for Configuration
    Change-Id: I1505f6a776a37abbe7bcd0ba23867ea200256901

---
+++ src/python-lib/unittests/aerofs/test_configuration.py
@@ -0,0 +1,26 @@
+import unittest
+import requests
+from requests import Response
+from aerofs_common.configuration import Configuration
+
+class TestConfiguration(unittest.TestCase):
+    def test_fetch_should_exclude_browser_key(self):
+        """
+        @note this test covers: 1. server browser key is excluded, and
+        2. unicode parsing is working because if it wasn't, the response
+        won't match our expected output
+        """
+        self._mock_response('server.browser.key=topsecret\n'
+                            'yet.another.property=public')
+        self._verify_fetch_result({'yet.another.property': 'public'})
+
+    def _mock_response(self, content):
+        res = Response()
+        res.status_code = 200
+        res._content = content
+        requests.get = lambda url: res
+
+    def _verify_fetch_result(self, expected):
+        config = {}
+        Configuration().fetch_and_populate(config)
+        self.assertEqual(config, expected)
