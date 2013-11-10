e278bc118997e836e38b5889a83b02a51bbf4660
Jon Pile <jon@aerofs.com>
2013-11-10 10:22:14 -0800

    Fix python unit test
    Looks like a merge error? We have a vestigial testcase for external
    credentials, which no longer exist...
    
    Change-Id: Ia4fae41badf596c7dca3b34821b9ddf932bd7dc4

--- src/web/unittests/login/login_test.py
+++ src/web/unittests/login/login_test.py
@@ -2,7 +2,6 @@ import unittest
 from unittests.test_base import TestBase
 from web.util import is_private_deployment
 from web.login_util import URL_PARAM_NEXT, resolve_next_url
-from web.views.login.login_view import _is_external_cred_enabled, _format_password
 
 
 class LoginTest(TestBase):
@@ -20,18 +19,6 @@ class LoginTest(TestBase):
         request.host_url = "hahahhoho"
 
         self.assertEqual(resolve_next_url(request, 'default'), "hahahhoho/cnn.com")
-
-    def test_internal_should_be_default(self):
-        """
-        This test verifies that if internal_email_pattern is None, we treat all
-        addresses as internal.
-        """
-        settings = {"config.loader.is_private_deployment": "true",
-                    "internal_email_pattern": None,
-                    "lib.authenticator": "external_credential" }
-
-        self.assertTrue(is_private_deployment(settings))
-        self.assertTrue(_is_external_cred_enabled(settings))
 
 def test_suite():
     loader = unittest.TestLoader()
