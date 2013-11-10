845247acb476583ef55422b14a668827f36d956c
Drew Fisher <drew@aerofs.com>
2013-11-10 02:36:18 -0800

    Remove setuptools (again)
    We dropped setuptools for a couple of reasons:
    
    1) the setuptools/distribute split/merge at strange versions causes
       pip-prefetch.sh to fail under certain conditions
    2) current setuptools has some issue with certain versions of virtualenv
       and I'm tired of doing random things until it works on other devs'
       machines
    3) we barely used any setuptools-specific functionality anyway
    
    I appear to have missed a particular use case in my previous commit, but
    I'd really rather not add setuptools back in for the reasons stated
    above.  Thus, this changeset does the following things:
    
    1) Fixes setup.sh to put python-lib and web on sys.path for local
       development.
    2) Removes MANIFEST from .gitignore. It's a build artifact that has the
       potential to affect package contents and thus shouldn't be
       .gitignored unless there's also automation to clean it.
    3) Removes the setuptools imports from assorted setup.py files, as they
       should not longer be needed.
    
    Change-Id: I164185a65b8e856ed4eaf105808ce3f346a38890

--- src/web/development/setup.sh
+++ src/web/development/setup.sh
@@ -5,9 +5,23 @@ cd $(dirname "${BASH_SOURCE[0]}")
 DEV_ROOT=$(pwd)
 SRC_ROOT="$DEV_ROOT"/../..
 
-virtualenv ~/env
-~/env/bin/pip install --requirement ${SRC_ROOT}/web/requirements.txt
-cd "$SRC_ROOT"/python-lib
-~/env/bin/python setup.py develop
-cd "$SRC_ROOT"/web
-~/env/bin/python setup.py develop
+# Create virtualenv
+virtualenv $HOME/env
+
+# Install web's dependencies in virtualenv
+$HOME/env/bin/pip install --requirement ${SRC_ROOT}/web/requirements.txt
+EASY_INSTALL_PTH=$HOME/env/lib/python2.7/site-packages/easy-install.pth
+
+add_for_dev() {
+	# $1 is path to folder
+	# $2 is .egg-link path
+	echo "$1" > "$2"
+	echo "." >> "$2"
+	grep "$1" "$EASY_INSTALL_PTH" > /dev/null || echo "$1" >> "$EASY_INSTALL_PTH"
+}
+
+# make the development copy of python-lib available within the virtualenv
+add_for_dev "$SRC_ROOT/python-lib" $HOME/env/lib/python2.7/site-packages/aerofs-py-lib.egg-link
+
+# make the development copy of web available within the virtualenv
+add_for_dev "$SRC_ROOT/web" $HOME/env/lib/python2.7/site-packages/web.egg-link
