f8420b559ec0f4bf0669a497d0b4b3156dd335a6
Weihan Wang <weihan@aerofs.com>
2013-11-19 13:01:26 -0800

    cache version number in memory for the pyramid server
    Change-Id: Idd6321f375b5e6455650868ed44a2254befc7a3b

--- src/web/web/version.py
+++ src/web/web/version.py
@@ -1,11 +1,18 @@
 import commands
 
+_version = None
+
 def get_current_version():
     """
     Return the current AeroFS version as a string (Note that enterprise-greeter
     duplicates this function)
     """
-    # The command finds the line "Version=0.1.2" and return the string after '='
-    status, output = commands.getstatusoutput(
-        'grep Version /opt/repackaging/installers/original/current.ver | cut -d = -f 2')
-    return output if status == 0 else '(unknown)'
+    global _version
+    # cache the version to avoid frequent shell-outs
+    if not _version:
+        # The command finds the line "Version=0.1.2" and return the string after '='
+        status, output = commands.getstatusoutput(
+            'grep Version /opt/repackaging/installers/original/current.ver | cut -d = -f 2')
+        _version = output if status == 0 else '(unknown)'
+
+    return _version
