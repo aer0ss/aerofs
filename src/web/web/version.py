9bf6fa84fa67536d54f837467adf2104c37eb55b
Drew Fisher <drew@aerofs.com>
2013-11-19 15:07:30 -0800

    python: don't shell out to determine version
    Just parse it in python, dude.  Then you don't have to worry about the
    overhead so much.
    
    Change-Id: Ib9f050ec03629dae1c8d38626da4fc77d2c1cebc

--- src/web/web/version.py
+++ src/web/web/version.py
@@ -1,18 +1,23 @@
-import commands
-
+#!/usr/bin/env python
 _version = None
 
 def get_current_version():
     """
-    Return the current AeroFS version as a string (Note that enterprise-greeter
-    duplicates this function)
+    Return the current AeroFS version as a string.
+
+    The canonical source of the release version is the current.ver file
+    provided with the client installers.
+
+    (Note that enterprise-greeter duplicates this function.)
     """
     global _version
-    # cache the version to avoid frequent shell-outs
+    # cache the version to avoid frequent reads
     if not _version:
-        # The command finds the line "Version=0.1.2" and return the string after '='
-        status, output = commands.getstatusoutput(
-            'grep Version /opt/repackaging/installers/original/current.ver | cut -d = -f 2')
-        _version = output if status == 0 else '(unknown)'
+        try:
+            with open("/opt/repackaging/installers/original/current.ver") as f:
+                version_line = f.readline().rstrip()
+                _, _version = version_line.split("=", 1)
+        except:
+            _version = "(unknown)"
 
     return _version
