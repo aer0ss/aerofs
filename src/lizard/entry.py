71e269f397dd246f859daf52f627d9c6d495d636
Drew Fisher <drew@aerofs.com>
2013-12-11 22:22:30 -0800

    Make external URLs correctly when proxied
    Change-Id: I81f8a3f57aa1e76e0214f3f6a37d45104966f69b

--- src/lizard/entry.py
+++ src/lizard/entry.py
@@ -2,8 +2,15 @@
 # server hand over fist and is pure-python.
 # We could always run under uwsgi if we wanted to
 from cherrypy import wsgiserver
+from werkzeug.contrib.fixers import ProxyFix
 
 from lizard import app, migrate_database
+
+# We run behind an nginx proxy, so we need to fix up the WSGI environ
+# to use assorted header data to perform external URL construction.
+# Note that it is DANGEROUS to set this if you're NOT running behind a proxy
+# because the site will blindly trust the incoming headers.
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 d = wsgiserver.WSGIPathInfoDispatcher({'/': app})
 # TODO: make this localhost if not testing
