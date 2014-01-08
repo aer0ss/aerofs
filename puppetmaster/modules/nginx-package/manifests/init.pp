cf63551df0695077b71fe8b43163aab3f625a6f1
Drew Fisher <drew@aerofs.com>
2014-01-08 02:05:27 -0800

    Update nginx package to nginx 1.5.6
    The backported package has been added to all of our repos on
    apt.aerofs.com, and can be built with the scripts at
    http://github.arrowfs.org/aerofs/buildbox
    
    Since "nginx" is just a metapackage and doesn't specify explicit deps,
    we have to specify that all three packages (nginx, nginx-common, and
    nginx-full) should all be kept at the latest version.
    
    Change-Id: Idce1a35dead97ef3246412c4ed1ac3b802730602

--- puppetmaster/modules/nginx-package/manifests/init.pp
+++ puppetmaster/modules/nginx-package/manifests/init.pp
@@ -1,6 +1,24 @@
 class nginx-package {
+    # note that nginx is just a meta package which depends on nginx-full or
+    # nginx-light.  we want to make sure that we use the most up-to-date copy
+    # of all three packages.
     package{"nginx":
-        ensure => present,
+        ensure => latest,
+        require => [
+            Apt::Source["aerofs"],
+        ]
+    }
+    package{"nginx-common":
+        ensure => latest,
+        require => [
+            Apt::Source["aerofs"],
+        ]
+    }
+    package{"nginx-full":
+        ensure => latest,
+        require => [
+            Apt::Source["aerofs"],
+        ]
     }
 
     file {"/etc/nginx/sites-enabled/default":
