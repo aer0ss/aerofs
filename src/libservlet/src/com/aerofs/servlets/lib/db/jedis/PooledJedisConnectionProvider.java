02cbb2fc837fd08fd080e51d0a70b225863c4c82
Sergey Smirnov <sergey@aerofs.com>
2014-01-26 01:14:23 -0800

    configure jedis connection pool
    Change-Id: I9220dfea76ac2ba122b98ec7cc4dcf27fca489b9

--- src/libservlet/src/com/aerofs/servlets/lib/db/jedis/PooledJedisConnectionProvider.java
+++ src/libservlet/src/com/aerofs/servlets/lib/db/jedis/PooledJedisConnectionProvider.java
@@ -5,6 +5,7 @@
 package com.aerofs.servlets.lib.db.jedis;
 
 import com.aerofs.servlets.lib.db.IDatabaseConnectionProvider;
+import org.apache.commons.pool.impl.GenericObjectPool;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.JedisPooledConnection;
@@ -35,10 +36,38 @@ public class PooledJedisConnectionProvider
 
         JedisPoolConfig config = new JedisPoolConfig();
 
+        //TODO Move these settings to config file
+
+        // There is a nice post about configuring jedis pool:
+        // http://biasedbit.com/redis-jedispool-configuration
+        // We don't need to hold a lot of idle connections - we want to dynamically adjust according
+        // to load.
+        // It is also good idea to test connections while idle so we can invalidate
+        // stale connections before they are requested
+
+        // Fail-fast behaviour, we don't like to keep the kids waiting
+        config.setWhenExhaustedAction(GenericObjectPool.WHEN_EXHAUSTED_FAIL);
+        // Tests whether connection is dead when connection retrieval method is called
         // So that failures do not reach the user as often.
         config.setTestOnBorrow(true);
         // About the same as the max number of tomcat threads.
         config.setMaxActive(64);
+        // Number of connections to Redis that just sit there and do nothing
+        config.setMaxIdle(32); // half of active
+        // Minimum number of idle connections to Redis - these can be seen as always open
+        // and ready to serve
+        config.setMinIdle(4);
+        // Tests whether connection is dead when returning a connection to the pool
+        config.setTestOnReturn(true);
+        // Tests whether connections are dead during idle periods
+        config.setTestWhileIdle(true);
+        // Maximum number of connections to test in each idle check
+        config.setNumTestsPerEvictionRun(32);
+        // Idle connection checking period
+        config.setTimeBetweenEvictionRunsMillis(60000);
+        // Maximum time, in milliseconds, to wait for a resource
+        // when exausted action is set to WHEN_EXAUSTED_BLOCK
+        config.setMaxWait(3000);
 
         _jedisPool = new JedisPool(config, host, port);
     }
