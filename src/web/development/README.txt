2afda9788080a5eac53f09d12ff4268e9033b5b1
Weihan Wang <weihan@aerofs.com>
2014-02-06 12:18:24 -0800

    fix a regression due to bunker refactoring + update README
    Change-Id: I57451df503f81f203d7bb07c91065f61d910595f

--- src/web/development/README.txt
+++ src/web/development/README.txt
@@ -1,1 +1,28 @@
-see https://github.arrowfs.org/aerofs/aerofs/wiki/Running-the-website-locally on how to use the scripts in this folder to run the Pyramid server on the host computer and point it to your local production VM.
+Running the Web site locally
+
+## Public Mode
+
+Simply set up your system as per the developer setup doc and run the following:
+
+    cd ~/repos/aerofs/src/web
+    ./development/setup.sh
+    ./development/run.sh public
+
+## Private Mode
+
+TODO (MP) make this easier!
+
+On your local system you will have to point "localhost" to your local prod system. This is required because that is the place the web code looks for configuration. Add this to /etc/hosts:
+
+    192.168.51.150 localhost
+
+Then on your local system run the following:
+
+    sudo touch /var/aerofs/configuration-initialized-flag
+    cd ~/repos/aerofs/src/web
+    ./development/setup.sh
+    ./development/run.sh private
+
+## Viewing the website
+
+The website will be available at http://0.0.0.0:6543/
--- src/web/web/views/error/error_view.py
+++ src/web/web/views/error/error_view.py
@@ -6,6 +6,8 @@ from pyramid.security import NO_PERMISSION_REQUIRED
 from pyramid.view import view_config, forbidden_view_config
 from web.views.error.error_view_util import force_login
 from web.views import maintenance
+from web.license import *
+from web.login_util import remember_license_based_login
 
 log = logging.getLogger(__name__)
 
@@ -27,7 +29,32 @@ def not_found_view(request):
 )
 def forbidden_view(request):
     log.error("forbidden view for " + request.path)
-    return force_login(request, _get_login_route(request))
+
+    response = _attempt_license_shasum_login(request)
+    return response if response else force_login(request, _get_login_route(request))
+
+
+def _attempt_license_shasum_login(request):
+    """
+    If a request is made with a license shasum in the query string, attempt
+    to login with the shasum and retry the request. This is necessary for
+    access to the appliance management API through command lines.
+
+    @return a response if login successful, None otherwise
+    """
+    shasum = get_license_shasum_from_query(request)
+    # Skip license-based login if:
+    # 1. the query has no license shasum, or
+    # 2. the shasum stored in the session is the same as the shasum
+    #    in the query. This is to prevent infinite loops if the user got
+    #    permission denied after he logs in with the license.
+    if not shasum or shasum == get_license_shasum_from_session(request):
+        log.info('skip license-based login. shasum: {}'.format(shasum))
+    elif verify_license_shasum_and_attach_to_session(request, shasum):
+        log.info('license shasum in query string verified. login w/ license')
+        remember_license_based_login(request)
+        return request.invoke_subrequest(request, use_tweens=True)
+
 
 @view_config(
     context=ExceptionReply,
