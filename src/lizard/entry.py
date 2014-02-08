f5fc7017e1e9e854b1faba1d0cdfc0725ed0844d
Drew Fisher <drew@aerofs.com>
2014-02-07 16:32:09 -0800

    lizard: log all requests in production
    Change-Id: I72a272aab7f35487a077e65c9aa528148cefdadf

--- src/lizard/entry.py
+++ src/lizard/entry.py
@@ -2,9 +2,11 @@
 # server hand over fist and is pure-python.
 # We could always run under uwsgi if we wanted to
 import sys
+import logging
 
 from cherrypy import wsgiserver
 from werkzeug.contrib.fixers import ProxyFix
+from flask import request
 
 from lizard import create_app, migrate_database
 
@@ -12,6 +14,23 @@ internal = len(sys.argv) > 1 and sys.argv[1] == "internal"
 port = 5001 if internal else 8588
 
 app = create_app(internal)
+
+# Set up logging.
+LOG_FILE = '/var/log/lizard/{}.log'.format("lizard-admin" if internal else "lizard")
+LOG_LEVEL = logging.DEBUG
+file_handler = logging.FileHandler(LOG_FILE)
+file_handler.setLevel(LOG_LEVEL)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s %(levelname)s] %(message)s',
+        datefmt="%Y-%m-%d %H:%M:%S %z")
+file_handler.setFormatter(formatter)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(LOG_LEVEL)
+
+# Log all requests.
+@app.after_request
+def log_request(resp):
+    app.logger.info("{} {} {} {}".format(request.remote_addr, resp.status_code, request.method, request.path))
+    return resp
 
 # We run behind an nginx proxy, so we need to fix up the WSGI environ
 # to use assorted header data to perform external URL construction.
