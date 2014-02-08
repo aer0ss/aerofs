dae88025afbe04e46fd2358ebc59a3e5c7a34a35
Weihan Wang <weihan@aerofs.com>
2014-02-07 21:21:04 -0800

    fix test_web
    Change-Id: I5179e98a03a52f830cb0f2a6ea1f4da18daaa23c

--- src/bunker/unittests/__init__.py
+++ src/bunker/unittests/__init__.py
@@ -4,10 +4,12 @@ import unittest
 def test_suite():
     # import all test modules
     import unittests.maintenance
+    import unittests.setup
 
     suite = unittest.TestSuite()
 
     # add all unit tests
     suite.addTest(unittests.maintenance.test_suite())
+    suite.addTest(unittests.setup.test_suite())
 
     return suite
--- src/web/unittests/__init__.py
+++ src/web/unittests/__init__.py
@@ -7,14 +7,12 @@ def test_suite():
     import unittests.login
     import unittests.misc
     import unittests.payment
-    import unittests.setup
     import unittests.shared_folders
     import unittests.org_users
     import unittests.org_settings
     import unittests.password_reset
     import unittests.settings
     import unittests.oauth
-
 
     suite = unittest.TestSuite()
 
@@ -24,7 +22,6 @@ def test_suite():
     suite.addTest(unittests.misc.test_suite())
     suite.addTest(unittests.password_reset.test_suite())
     suite.addTest(unittests.payment.test_suite())
-    suite.addTest(unittests.setup.test_suite())
     suite.addTest(unittests.shared_folders.test_suite())
     suite.addTest(unittests.org_users.test_suite())
     suite.addTest(unittests.org_settings.test_suite())
