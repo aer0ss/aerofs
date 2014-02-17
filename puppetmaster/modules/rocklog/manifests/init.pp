2f7ef246ba19c84cf4219453b347071acca7ab39
Allen George <allen@aerofs.com>
2014-02-17 12:30:06 -0800

    Update elasticsearch to the latest version (1.0.0)
    Change-Id: Ia9bb969a5a568b32a130ed97e9f1cb61f51abdee

--- puppetmaster/modules/rocklog/manifests/init.pp
+++ puppetmaster/modules/rocklog/manifests/init.pp
@@ -30,16 +30,16 @@ class rocklog {
     }
 
     exec{"get elasticsearch deb":
-        command => "/usr/bin/wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.10.deb",
+        command => "/usr/bin/wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.0.deb",
         cwd => "/root/",
-        creates => "/root/elasticsearch-0.90.10.deb",
+        creates => "/root/elasticsearch-1.0.0.deb",
         notify => Package["elasticsearch"]
     }
 
     package{ "elasticsearch" :
         provider => dpkg,
         ensure => latest,
-        source => "/root/elasticsearch-0.90.10.deb",
+        source => "/root/elasticsearch-1.0.0.deb",
         require => Exec["get elasticsearch deb"]
     }
 
