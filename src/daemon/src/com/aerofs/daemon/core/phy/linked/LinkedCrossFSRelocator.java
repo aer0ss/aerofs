4778313ac7c7b5d6f9dc18a2f4e7c117858d27b4
Hugues Bruant <hugues@aerofs.com>
2014-03-11 09:27:13 -0700

    Handle NROs in LinkedCrossFSRelocator
    Use RepresentabilityHelper to build the correct physical path
    instead of assuming all objects reside in the root anchor
    
    Change-Id: I666a185cd4709d89b055cfdc05a63ac87bece20c

--- src/daemon/src/com/aerofs/daemon/core/phy/linked/LinkedCrossFSRelocator.java
+++ src/daemon/src/com/aerofs/daemon/core/phy/linked/LinkedCrossFSRelocator.java
@@ -4,10 +4,17 @@
 
 package com.aerofs.daemon.core.phy.linked;
 
+import com.aerofs.base.BaseLogUtil;
+import com.aerofs.base.BaseUtil;
+import com.aerofs.base.Loggers;
+import com.aerofs.base.id.SID;
+import com.aerofs.base.id.UniqueID;
 import com.aerofs.daemon.core.admin.HdRelocateRootAnchor.CrossFSRelocator;
 import com.aerofs.daemon.core.ds.DirectoryService;
 import com.aerofs.daemon.core.ds.DirectoryService.IObjectWalker;
 import com.aerofs.daemon.core.ds.OA;
+import com.aerofs.daemon.core.ds.ResolvedPath;
+import com.aerofs.daemon.core.phy.linked.RepresentabilityHelper.PathType;
 import com.aerofs.daemon.core.phy.linked.linker.LinkerRootMap;
 import com.aerofs.daemon.lib.db.trans.Trans;
 import com.aerofs.lib.Path;
@@ -16,6 +23,7 @@ import com.aerofs.lib.id.SOID;
 import com.aerofs.lib.injectable.InjectableDriver;
 import com.aerofs.lib.injectable.InjectableFile;
 import com.google.inject.Inject;
+import org.slf4j.Logger;
 
 import javax.annotation.Nullable;
 import java.io.File;
@@ -29,68 +37,85 @@ import java.util.Arrays;
  */
 class LinkedCrossFSRelocator extends CrossFSRelocator
 {
+    private final static Logger l = Loggers.getLogger(LinkedCrossFSRelocator.class);
+
     private final LinkerRootMap _lrm;
+    private final RepresentabilityHelper _rh;
 
     @Inject
     public LinkedCrossFSRelocator(InjectableFile.Factory factFile,
-            DirectoryService ds, InjectableDriver dr, LinkerRootMap lrm)
+            DirectoryService ds, InjectableDriver dr, LinkerRootMap lrm, RepresentabilityHelper rh)
     {
         super(factFile, ds, dr);
         _lrm = lrm;
+        _rh = rh;
     }
 
     @Override
     public void afterRootRelocation(Trans t) throws Exception
     {
-        updateFID(_ds.resolveThrows_(Path.root(_sid)),
-                _newRoot.getAbsolutePath(), t);
-
+        // update linker root first, to be able to use RepresentabilityHelper in updateFID
         _lrm.move_(_sid, _oldRoot.getAbsolutePath(), _newRoot.getAbsolutePath(), t);
+
+        updateFID(_sid, t);
     }
 
-    private void updateFID(SOID newRootSOID, String absNewRoot, final Trans t) throws Exception
+    private void updateFID(SID sid, final Trans t) throws Exception
     {
         // Initial walk inserts extra byte to each new FID to avoid duplicate key conflicts in
         // the database in case some files in the new location happen to have identical FIDs
         // with original files -- although it should be very rare. Second walk removes this
         // extra byte.
 
-        _ds.walk_(newRootSOID, absNewRoot, new IObjectWalker<String>() {
+        ResolvedPath p = ResolvedPath.root(sid);
+        SOID soid =_ds.resolveThrows_(p);
+        _ds.walk_(soid, p, new IObjectWalker<ResolvedPath>() {
             @Override
-            public String prefixWalk_(String oldParent, OA oa)
+            public ResolvedPath prefixWalk_(ResolvedPath oldParent, OA oa)
             {
                 return prefixWalk(oldParent, oa);
             }
 
             @Override
-            public void postfixWalk_(String oldParent, OA oa)
+            public void postfixWalk_(ResolvedPath oldParent, OA oa)
                     throws IOException, SQLException
             {
                 // Root objects have null FIDs
                 if (!oa.soid().oid().isRoot() && oa.fid() != null) {
-                    FID newFID = _dr.getFID(oldParent + oa.name());
+                    ResolvedPath path = oldParent.join(oa);
+                    LinkedPath lp = _rh.physicalPath(path, PathType.Source);
+                    FID newFID = null;
+                    try {
+                        newFID = _dr.getFID(lp.physical);
+                    } catch (Exception e) {
+                        l.warn("could not get fid for {}", lp.physical, BaseLogUtil.suppress(e));
+                    }
 
-                    // newFID can be null if it is removed by filesystem during relocation
                     if (newFID == null) {
-                        newFID = oa.fid();
-                        assert newFID != null : oa;
+                        // could not get FID for any reason
+                        // the actual relocation went fine so aborting and rolling back now
+                        // would be a pretty terrible thing to do. Assign a random FID that
+                        // is guaranteed not to conflict with any real FID and let the scanner
+                        // resolve things
+                        newFID = new FID(UniqueID.generate().getBytes());
                     }
 
                     byte[] bytesFID = Arrays.copyOf(newFID.getBytes(), _dr.getFIDLength() + 1);
+                    l.info("update fid {} {} {}", oa.soid(), oa.fid(), newFID);
                     _ds.setFID_(oa.soid(), new FID(bytesFID), t);
                 }
             }
         });
 
-        _ds.walk_(newRootSOID, absNewRoot, new IObjectWalker<String>() {
+        _ds.walk_(soid, p, new IObjectWalker<ResolvedPath>() {
             @Override
-            public String prefixWalk_(String oldParent, OA oa)
+            public ResolvedPath prefixWalk_(ResolvedPath oldParent, OA oa)
             {
                 return prefixWalk(oldParent, oa);
             }
 
             @Override
-            public void postfixWalk_(String oldParent, OA oa)
+            public void postfixWalk_(ResolvedPath oldParent, OA oa)
                     throws IOException, SQLException {
                 if (!oa.soid().oid().isRoot() && oa.fid() != null) {
                     assert oa.fid().getBytes().length == _dr.getFIDLength() + 1;
@@ -105,7 +130,7 @@ class LinkedCrossFSRelocator extends CrossFSRelocator
     /**
      * oldParent has the File.separator affixed to the end of the path (except root).
      */
-    private @Nullable String prefixWalk(String oldParent, OA oa)
+    private @Nullable ResolvedPath prefixWalk(ResolvedPath oldParent, OA oa)
     {
         if (oa.isExpelled()) {
             assert oa.fid() == null;
@@ -113,9 +138,8 @@ class LinkedCrossFSRelocator extends CrossFSRelocator
         }
 
         switch (oa.type()) {
-        case ANCHOR: return oldParent + oa.name();
-        case DIR:    return (oa.soid().oid().isRoot()) ? oldParent + File.separator
-                : oldParent + oa.name() + File.separator;
+        case ANCHOR: return oldParent.join(oa);
+        case DIR:    return (oa.soid().oid().isRoot()) ? oldParent: oldParent.join(oa);
         case FILE:   return null;
         default:     assert false; return null;
         }
