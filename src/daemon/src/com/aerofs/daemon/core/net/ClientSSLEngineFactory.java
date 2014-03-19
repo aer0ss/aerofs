40bdf2c2ee7a5f364943c5fe97ce18e97dd328db
Allen George <allen@aerofs.com>
2014-03-18 18:22:48 -0700

    Set SSLSessionContext cache size and timeout for daemon
    Change-Id: I0dfb5f0df01d45e3180231f6230b9eb6245237a1

--- src/daemon/src/com/aerofs/daemon/core/net/ClientSSLEngineFactory.java
+++ src/daemon/src/com/aerofs/daemon/core/net/ClientSSLEngineFactory.java
@@ -9,11 +9,25 @@ import com.aerofs.lib.cfg.CfgCACertificateProvider;
 import com.aerofs.lib.cfg.CfgKeyManagersProvider;
 import com.google.inject.Inject;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSessionContext;
+
 public class ClientSSLEngineFactory extends SSLEngineFactory
 {
     @Inject
     public ClientSSLEngineFactory(CfgKeyManagersProvider keyProvider, CfgCACertificateProvider trustedCA)
     {
         super(Mode.Client, Platform.Desktop, keyProvider, trustedCA, null);
+    }
+
+    @Override
+    protected void onSSLContextCreated(SSLContext context)
+    {
+        SSLSessionContext sessionContext = context.getClientSessionContext();
+        // FIXME (AG): it's not clear what the appropriate cache size and session timeout is
+        // it has to be long enough that a transient disconnect will allow for a fast handshake
+        // and not too long for them to sit around for ever
+        sessionContext.setSessionCacheSize(30); // 30 sessions
+        sessionContext.setSessionTimeout(1800); // 30 minutes
     }
 }
--- src/daemon/src/com/aerofs/daemon/core/net/ServerSSLEngineFactory.java
+++ src/daemon/src/com/aerofs/daemon/core/net/ServerSSLEngineFactory.java
@@ -9,11 +9,23 @@ import com.aerofs.lib.cfg.CfgCACertificateProvider;
 import com.aerofs.lib.cfg.CfgKeyManagersProvider;
 import com.google.inject.Inject;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSessionContext;
+
 public class ServerSSLEngineFactory extends SSLEngineFactory
 {
     @Inject
     public ServerSSLEngineFactory(CfgKeyManagersProvider keyProvider, CfgCACertificateProvider trustedCA)
     {
         super(Mode.Server, Platform.Desktop, keyProvider, trustedCA, null);
+    }
+
+    @Override
+    protected void onSSLContextCreated(SSLContext context)
+    {
+        // see comments in ClientSSLEngineFactory about constants
+        SSLSessionContext sessionContext = context.getServerSessionContext();
+        sessionContext.setSessionCacheSize(30); // only 30 sessions
+        sessionContext.setSessionTimeout(1800); // 30 minutes
     }
 }
