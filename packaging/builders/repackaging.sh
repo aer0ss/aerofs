6a14f0ccd702d81bfdcf336c5dc8598caa10f517
Drew Fisher <drew@aerofs.com>
2014-04-03 16:25:17 -0700

    Clean up repackaging, remove obsolete scripts
    - move all repackaging-related things from tools/enterprise to
      packaging/ (where static shipped-as-is packages generally reside)
    - fix up builder/installer scripts accordingly
    - burn out-of-date enterprise-updater related stuff; we do single-use
      VM images only
    - remove enterprise-updater package
    
    Change-Id: I22ad76ca0773aa3d8a5bea8bde30ea49a1a246f5

--- packaging/builders/repackaging.sh
+++ packaging/builders/repackaging.sh
@@ -3,13 +3,9 @@ set -e -u
 
 OUTPUT_DIR=build/repackaging
 OPT=$OUTPUT_DIR/opt/repackaging
-TOOLS=$OPT/tools
-ENTERPRISE=../tools/enterprise
-
-mkdir -p $TOOLS
 
 # Fetch python dependency packages to cache
-REQUIREMENTS=$ENTERPRISE/repackaging/requirements.txt
+REQUIREMENTS=repackaging/opt/repackaging/requirements.txt
 SDIST_CACHE=$HOME/.aerofs-cache/pip/repackaging
 tools/pip-prefetch.sh $REQUIREMENTS $SDIST_CACHE
 
@@ -17,14 +13,3 @@ tools/pip-prefetch.sh $REQUIREMENTS $SDIST_CACHE
 SDIST=$OPT/sdist
 mkdir -p $SDIST
 cp -a $SDIST_CACHE/* $SDIST/
-cp $REQUIREMENTS $SDIST/
-
-# Include repackaging tools
-for os in linux osx win
-do
-    # cp -r is BAD, prefer cp -a or cp -R for OSX compatibility; man 1 cp
-    cp -a $ENTERPRISE/repackaging/$os $TOOLS
-done
-
-cp $ENTERPRISE/repackaging/pull-installers.sh $TOOLS
-cp $ENTERPRISE/repackaging/repackage-installers.sh $TOOLS
