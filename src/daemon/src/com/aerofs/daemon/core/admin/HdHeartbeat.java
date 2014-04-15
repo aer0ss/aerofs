fd4d70a15b6375c37c5b32abd4954bebb80bc1b3
Hugues Bruant <hugues@aerofs.com>
2014-04-15 15:40:49 -0700

    Fix flaky API test
    Add debug log line for ritual heartbeat
    
    Change-Id: I3849626685ef15268dfd698996fb630146a613ba

--- src/daemon/src/com/aerofs/daemon/core/admin/HdHeartbeat.java
+++ src/daemon/src/com/aerofs/daemon/core/admin/HdHeartbeat.java
@@ -10,11 +10,10 @@ import com.aerofs.lib.event.Prio;
 
 public class HdHeartbeat extends AbstractHdIMC<EIHeartbeat>
 {
-
     @Override
     // This method simply contacts the core and does nothing
     protected void handleThrows_(EIHeartbeat ev, Prio prio) throws Exception
     {
-
+        l.debug("ritual heartbeat");
     }
 }
