be46a7f3959eb04a09eb5e2b0688f3ef8e1393f8
Jonathan Gray <jonathan@aerofs.com>
2014-04-17 10:58:31 -0700

    shelob: fix unit test
    Change-Id: I258b39cfd52b08ae1b8ebf4608ac370b2ce5db44

--- src/web/jstest/shelob/e2e/scenarios.js
+++ src/web/jstest/shelob/e2e/scenarios.js
@@ -15,7 +15,7 @@ describe('Shelob App', function() {
 
     it('should navigate to subfolder when subfolder link is clicked', function() {
         browser().navigateTo('/index.html');
-        element('tbody tr.object:eq(0) a').click();  // empty_folder
+        element('tbody tr.object:eq(0) td.name a').click();  // empty_folder
         expect(browser().location().url()).toBe('/01a01a01a');
         expect(repeater('tbody tr.object').count()).toBe(0);
     });
@@ -48,16 +48,16 @@ describe('Shelob App', function() {
     it('should add folders to breadcrumb trail as subfolder links are clicked', function() {
         browser().navigateTo('/index.html');
         expect(element('ul.breadcrumb li').count()).toEqual(1);
-        element('tbody tr.object:eq(1) a').click();  // other_folder
+        element('tbody tr.object:eq(1) td.name a').click();  // other_folder
         expect(element('ul.breadcrumb li').count()).toEqual(2);
-        element('tbody tr.object:eq(0) a').click();  // deeper_folder
+        element('tbody tr.object:eq(0) td.name a').click();  // deeper_folder
         expect(element('ul.breadcrumb li').count()).toEqual(3);
     });
 
     it('should should remove folders from breadcrumb trail if a breadcrumb link is clicked ', function() {
         browser().navigateTo('/index.html');
-        element('tbody tr.object:eq(1) a').click();  // other_folder
-        element('tbody tr.object:eq(0) a').click();  // deeper_folder
+        element('tbody tr.object:eq(1) td.name a').click();  // other_folder
+        element('tbody tr.object:eq(0) td.name a').click();  // deeper_folder
         expect(element('ul.breadcrumb li').count()).toEqual(3);
         element('ul.breadcrumb li:eq(1) a').click();  // other_folder
         expect(browser().location().url()).toBe('/9f89f89f8');
