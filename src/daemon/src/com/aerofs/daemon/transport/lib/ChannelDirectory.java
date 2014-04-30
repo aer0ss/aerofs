e2a9f34ef57f025e8f2c75d9b879457e8ed27a7e
Jon Pile <jon@aerofs.com>
2014-04-29 17:06:23 -0700

    Add monitor coordination to uses of Multimap
    Any use of the collections views from Multimap requires manual
    coordination by locking the monitor on the containing Multimap.
    
    There are still some potential state boggles here but we can avoid
    google's promised "non-deterministic behavior" anyway.
    
    AMENDED: changed code ordering. Set channel close futures before
    adding to the map. Rename private members.
    
    Change-Id: I30a12c179df0a411ad6ced3cd8a1189468081af6

--- src/daemon/src/com/aerofs/daemon/transport/lib/ChannelDirectory.java
+++ src/daemon/src/com/aerofs/daemon/transport/lib/ChannelDirectory.java
@@ -17,8 +17,6 @@ import org.jboss.netty.channel.ChannelFuture;
 import org.jboss.netty.channel.ChannelFutureListener;
 import org.slf4j.Logger;
 
-import java.util.Set;
-
 /**
  * Presence: The New Hotness.
  *
@@ -29,16 +27,25 @@ import java.util.Set;
  * of a particular set). Also the ability to select members of this set in different ways would
  * be super super great.
  *
- * Pass 1: consolidate users of MultiMap in Unicast.
- * Pass 2: One instance of ChannelDirectory to serve all instances of Unicast.
- * Pass 3: make best-channel-available decisions from here.
+ * Next Pass: One instance of ChannelDirectory to serve all instances of Unicast.
+ *
+ * Next next pass: make best-channel-available decisions from here.
  */
 public class ChannelDirectory
 {
     private static final Logger l = Loggers.getLogger(ChannelDirectory.class);
 
-    /* No public constructor */
-    ChannelDirectory(ITransport tp) { _tp = tp; }
+    private final ITransport tp;
+    private IUnicastListener unicastListener;
+
+    // I'd like to know, based on a remove, whether the result set is now empty.
+    // Multimap does not do this. Ditto the inverse (did adding this key require creating a set?)
+    // Asking separately sucks. And if it's uncoordinated, so you can miss or duplicate edges.
+    private final SortedSetMultimap<DID, Channel> channels
+            = Multimaps.synchronizedSortedSetMultimap(TreeMultimap.<DID, Channel>create());
+
+    // No public constructor
+    ChannelDirectory(ITransport tp) { this.tp = tp; }
 
     /**
      * Register a channel instance for the given remote peer.
@@ -46,32 +53,10 @@ public class ChannelDirectory
     public void register(Channel channel, DID remotePeer)
     {
         // FIXME(jP): enhancement: better atomicity required here
-        _channels.put(remotePeer, channel);
         addChannelCloseFuture(remotePeer, channel);
+        channels.put(remotePeer, channel);
 
-        l.info("register t:d:c {}:{}:{}", _tp.id(), remotePeer, channel.getId() );
-    }
-
-    /**
-     * Remove the channel (which was previously registered for the given remote peer).
-     */
-    void remove(Channel channel, DID remotePeer)
-    {
-        l.info("unregister t:d:c {}:{}:{}", _tp.id(), remotePeer, channel);
-
-        // Remove only if its still the same client in the map.
-        // Hmm, when is that not true? and more importantly, when did someone pre-remove me?
-        // I don't like this logic.
-        // FIXME(jP) BUG? remove and containsKey on the same key, no coordination.
-        if (_channels.remove(remotePeer, channel)) {
-            l.info("{} remove connection", remotePeer);
-
-            // "If this device now has zero unicast channels for this transport..."
-            if (!_channels.containsKey(remotePeer)) {
-                // NOTE: this _may_ cause an EIPresence event to be sent up to the core!
-                _unicastListener.onDeviceDisconnected(remotePeer);
-            }
-        }
+        l.info("register t:d:c {}:{}:{}", tp.id(), remotePeer, channel.getId() );
     }
 
     /**
@@ -79,8 +64,9 @@ public class ChannelDirectory
      */
     public ImmutableSet<DID> getActiveDevices()
     {
-        // FIXME: BUG? can't use the view returned by _channels.get() without synchronizing on _channels. See Google apidoc.
-        return ImmutableSet.copyOf(_channels.keySet());
+        synchronized (channels) {
+            return ImmutableSet.copyOf(channels.keySet());
+        }
     }
 
     /**
@@ -88,8 +74,9 @@ public class ChannelDirectory
      */
     public ImmutableSet<Channel> getSnapshot(DID did)
     {
-        // FIXME: BUG? can't use the view returned by _channels.get() without synchronizing on _channels. See Google apidoc.
-        return ImmutableSet.copyOf(_channels.get(did));
+        synchronized (channels) {
+            return ImmutableSet.copyOf(channels.get(did));
+        }
     }
 
     /**
@@ -98,7 +85,33 @@ public class ChannelDirectory
      */
     public void setUnicastListener(IUnicastListener unicastListener)
     {
-        _unicastListener = unicastListener;
+        this.unicastListener = unicastListener;
+    }
+
+    /**
+     * Remove the channel (which was previously registered for the given remote peer).
+     */
+    void remove(Channel channel, DID remotePeer)
+    {
+        boolean deviceDisconnected = false;
+        l.info("unregister t:d:c {}:{}:{}", tp.id(), remotePeer, channel);
+
+        // The monitor is used only to coordinate the remove() and containskey() so we have
+        // a consistent view. Calling any affected listeners is done outside this lock, which
+        // means you could generate an EIPresence Down event _after_ a new channel has been added
+        // to the directory. Which feels alarming. Previously this was fully uncoordinated.
+
+        synchronized (channels) {
+            if (channels.remove(remotePeer, channel)) {
+                l.info("{} remove connection", remotePeer);
+
+                // "If this device now has zero unicast channels for this transport..."
+                if (!channels.containsKey(remotePeer)) {
+                    deviceDisconnected = true;
+                }
+            }
+        }
+        if (deviceDisconnected) { unicastListener.onDeviceDisconnected(remotePeer); }
     }
 
     /**
@@ -106,28 +119,14 @@ public class ChannelDirectory
      */
     private void addChannelCloseFuture(final DID did, final Channel channel)
     {
-        Preconditions.checkNotNull(_unicastListener);
+        Preconditions.checkNotNull(unicastListener);
 
         channel.getCloseFuture().addListener(new ChannelFutureListener() {
             @Override
             public void operationComplete(ChannelFuture channelFuture) throws Exception
             {
-                remove(channel, did);
+                ChannelDirectory.this.remove(channel, did);
             }
         });
     }
-
-    /**
-     * Right now the Transport instance is purely for logging/debugging.
-     * In the future this param will go away, and the Transport will be a parameter of the
-     * register / unregister calls. You know, for multi-indexing.
-     */
-    private final ITransport _tp;
-    private IUnicastListener _unicastListener;
-
-    // I'd like to know, based on a remove, whether the result set is now empty.
-    // Multimap does not do this. Ditto the inverse (did adding this key require creating a set?)
-    // Asking separately sucks. And if it's uncoordinated, so you can miss or duplicate edges.
-    private final SortedSetMultimap<DID, Channel> _channels
-            = Multimaps.synchronizedSortedSetMultimap(TreeMultimap.<DID, Channel>create());
 }
