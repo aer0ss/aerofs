9505f8bf6a2bb4dc13f3a64621b64bc31970cf01
Matt Pillar <matt@aerofs.com>
2014-05-02 12:19:26 -0700

    Dryad: rotate log data.
    N.B. clean-dryad-logs script is coupled to the implementation of dryad
    persistence.
    
    Change-Id: I57f57d6e818295e7d8545cca6ff32092f6c9d961

--- puppetmaster/manifests/nodes/dryad.pp
+++ puppetmaster/manifests/nodes/dryad.pp
@@ -6,4 +6,11 @@ node /^dryad\.aerofs\.com$/ inherits default {
     users::add_user { "pagerduty" : }
 
     common::service{"dryad": }
+
+    # We keep 90 days worth of logs. Clean once a day, at noon.
+    cron{"remove old dryad logs":
+        command => '/usr/bin/clean-dryad-logs 90',
+        minute  => "0",
+        hour    => "12",
+    }
 }
