45878f13e543ff1080be564da0b9249fc2971d88
Karen Rustad <karen@aerofs.com>
2014-06-13 10:28:24 -0700

    Fixed phantom tests to work with new non-table layout. Sorry about that.
    Change-Id: I223e699eb5b2032016fd0d2d3ba0e6b04af33e4b

--- src/web/jstest/shelob/e2e/scenarios.js
+++ src/web/jstest/shelob/e2e/scenarios.js
@@ -5,36 +5,36 @@
 describe('Shelob App', function() {
     it('should show contents of root when first loaded', function() {
         browser().navigateTo('/index.html');
-        expect(repeater('tbody tr.object').count()).toBe(3);
+        expect(repeater('.file-list-body .object').count()).toBe(3);
     });
 
     it('should show contents of subfolder when navigated to #/:oid', function() {
         browser().navigateTo('/index.html#/9f89f89f8');
-        expect(repeater('tbody tr.object').count()).toBe(4);
+        expect(repeater('.file-list-body .object').count()).toBe(4);
     });
 
     it('should navigate to subfolder when subfolder link is clicked', function() {
         browser().navigateTo('/index.html');
-        element('tbody tr.object:eq(0) td.name a').click();  // empty_folder
+        element('.file-list-body .object:eq(0) .name a').click();  // empty_folder
         expect(browser().location().url()).toBe('/01a01a01a');
-        expect(repeater('tbody tr.object').count()).toBe(0);
+        expect(repeater('.file-list-body .object').count()).toBe(0);
     });
 
     it('should show folder icon for folder', function() {
         browser().navigateTo('/index.html');
-        expect(element('tbody tr.object:eq(1) td.icon img').attr('src'))
+        expect(element('.file-list-body .object:eq(1) .icon img').attr('src'))
             .toEqual('/static/shelob/img/icons/40x40/filetype_folder.png');
     });
 
     it('should show text file icon for mimetype text/plain', function() {
         browser().navigateTo('/index.html');
-        expect(element('tbody tr.object:eq(2) td.icon img').attr('src')) // website.html
+        expect(element('.file-list-body .object:eq(2) .icon img').attr('src')) // website.html
             .toEqual('/static/shelob/img/icons/40x40/filetype_text.png');
     });
 
     it('should show generic file icon for unknown mimetype', function() {
         browser().navigateTo('/index.html#/9f89f89f8');
-        expect(element('tbody tr.object:eq(1) td img').attr('src'))  // otherfile
+        expect(element('.file-list-body .object:eq(1) img').attr('src'))  // otherfile
             .toEqual('/static/shelob/img/icons/40x40/filetype_generic.png');
     });
 
@@ -48,16 +48,16 @@ describe('Shelob App', function() {
     it('should add folders to breadcrumb trail as subfolder links are clicked', function() {
         browser().navigateTo('/index.html');
         expect(element('ul.breadcrumb li').count()).toEqual(1);
-        element('tbody tr.object:eq(1) td.name a').click();  // other_folder
+        element('.file-list-body .object:eq(1) .name a').click();  // other_folder
         expect(element('ul.breadcrumb li').count()).toEqual(2);
-        element('tbody tr.object:eq(0) td.name a').click();  // deeper_folder
+        element('.file-list-body .object:eq(0) .name a').click();  // deeper_folder
         expect(element('ul.breadcrumb li').count()).toEqual(3);
     });
 
     it('should should remove folders from breadcrumb trail if a breadcrumb link is clicked ', function() {
         browser().navigateTo('/index.html');
-        element('tbody tr.object:eq(1) td.name a').click();  // other_folder
-        element('tbody tr.object:eq(0) td.name a').click();  // deeper_folder
+        element('.file-list-body .object:eq(1) .name a').click();  // other_folder
+        element('.file-list-body .object:eq(0) .name a').click();  // deeper_folder
         expect(element('ul.breadcrumb li').count()).toEqual(3);
         element('ul.breadcrumb li:eq(1) a').click();  // other_folder
         expect(browser().location().url()).toBe('/9f89f89f8');
