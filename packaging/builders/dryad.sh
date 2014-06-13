349f79ff9cb581b42962df645f11abec27dd1fa4
Alex Tsay <alex@aerofs.com>
2014-06-13 16:26:31 -0700

    Updated Dryad server package to expose dependency.
    Change-Id: I472aed100f3a861b9a76483d940b2108232cf0ea

--- packaging/builders/dryad.sh
+++ packaging/builders/dryad.sh
@@ -9,3 +9,9 @@ JAVA_ARGS="-Xmx1536m"
 SERVICE_ARGS="dryad.properties"
 
 "$SCRIPT_DIR"/generators/generate_service_deb_template.sh dryad "$CONFIG" "$JAVA_ARGS" "$SERVICE_ARGS"
+
+# replace the following files because of manual edits
+for file in control postinst
+do
+    cp dryad/DEBIAN/"$file" build/dryad/DEBIAN
+done
