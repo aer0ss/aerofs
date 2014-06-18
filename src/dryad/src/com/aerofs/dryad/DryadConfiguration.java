69fbb0600f2981383d54f930106808f55f8ebd04
Alex Tsay <alex@aerofs.com>
2014-06-18 14:42:05 -0700

    Updated build.xml for compile_dryad.
    Also removed dependency on rest-api.
    
    Change-Id: I67590c477cdad2c56f019ab5db1bc8636a2f9919

--- src/dryad/src/com/aerofs/dryad/DryadConfiguration.java
+++ src/dryad/src/com/aerofs/dryad/DryadConfiguration.java
@@ -4,15 +4,33 @@
 
 package com.aerofs.dryad;
 
+import com.aerofs.base.Loggers;
 import com.aerofs.base.Version;
-import com.aerofs.rest.AbstractRestConfiguration;
+import com.aerofs.restless.Configuration;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponse;
 
-public class DryadConfiguration extends AbstractRestConfiguration
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+public class DryadConfiguration implements Configuration
 {
+    @Override
+    public void addGlobalHeaders(HttpRequest request, HttpResponse response)
+    {
+
+    }
+
+    @Override
+    public Response resourceNotFound(String path)
+    {
+        Loggers.getLogger(DryadConfiguration.class).warn("resource not found: {}", path);
+        return Response.status(Status.NOT_FOUND).build();
+    }
+
     @Override
     public boolean isSupportedVersion(Version version)
     {
-        // Internal service, don't care about versions (for now).
         return true;
     }
 }
--- src/dryad/src/com/aerofs/dryad/providers/ExFormatErrorExceptionMapper.java
+++ src/dryad/src/com/aerofs/dryad/providers/ExFormatErrorExceptionMapper.java
@@ -6,18 +6,20 @@ package com.aerofs.dryad.providers;
 
 import com.aerofs.base.Loggers;
 import com.aerofs.base.ex.ExFormatError;
-import com.aerofs.rest.providers.RuntimeExceptionMapper;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 
-public class ExFormatErrorExceptionMapper implements  ExceptionMapper<ExFormatError>
+/**
+ * Used to map ExFormatError when parsing DIDs
+ */
+public class ExFormatErrorExceptionMapper implements ExceptionMapper<ExFormatError>
 {
     @Override
     public Response toResponse(ExFormatError e)
     {
-        Loggers.getLogger(RuntimeExceptionMapper.class).warn("ex ", e);
+        Loggers.getLogger(ExFormatErrorExceptionMapper.class).warn("format error ", e);
         return Response.status(Status.BAD_REQUEST).build();
     }
 }
