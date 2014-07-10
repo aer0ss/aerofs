75fce0f09fd735951c5a665090702db18f0c088e
Karen Rustad <karen@aerofs.com>
2014-07-10 12:06:08 -0700

    Fixed shelob file size filter to provide accurate sizes
    The divisor needs to change based on the size of the file. Oops.
    
    Change-Id: If18fbed738bf057726ac9e2172caa7dbdd09f135

--- src/web/web/static/shelob/js/filters.js
+++ src/web/web/static/shelob/js/filters.js
@@ -1,17 +1,17 @@
 var shelobFilters = angular.module('shelobFilters', []).filter('humanBytes', function(){
-    var rounded = function(n) {
-        return Math.round(n*10/1024)/10;
+    var rounded = function(n, divisor) {
+        return Math.round(n*10/divisor)/10;
     };
     return function(input) {
-        if (input > 0) {
+        if (input >= 0) {
             if (input < 1024) {
                 return input.toString() + ' B';
             } else if (input < 1048576) {
-                return rounded(input).toString() + ' KB';
+                return rounded(input, 1024).toString() + ' KB';
             } else if (input < 1073741824) {
-                return rounded(input).toString() + ' MB';
+                return rounded(input, 1048576).toString() + ' MB';
             } else {
-                return rounded(input).toString() + ' GB';
+                return rounded(input, 1073741824).toString() + ' GB';
             }
         } else {
             return '--';
