914938fc697f0fc2d999d06db591440c7f7ed404
Alex Tsay <alex@aerofs.com>
2014-09-03 13:00:02 -0700

    Updated LogArchiver's period to 2 hours.
    Change-Id: I08dec38199dfe07fd1a3915999278d8b81df9b5e

--- src/gui/src/com/aerofs/ui/UIParam.java
+++ src/gui/src/com/aerofs/ui/UIParam.java
@@ -12,7 +12,11 @@ public class UIParam
     public static final long DM_RESTART_INTERVAL        = 5 * C.SEC;
     public static final int  DM_LAUNCH_PING_RETRIES     = 120;
     public static final long DM_LOG_ARCHIVE_STARTUP_DELAY = 15 * C.MIN;
-    public static final long DM_LOG_ARCHIVE_INTERVAL    = 24 * C.HOUR;
+    // upload archived logs every 2 hours so we can retry in a timely manner.
+    // Since the upload is tuned for high latency, the most likely case of failure is the client
+    // is offline. Thus we don't want to retry too soon nor do we want to wait too long to retry.
+    // Hence the period of 2 hours is chosen.
+    public static final long DM_LOG_ARCHIVE_INTERVAL    = 2 * C.HOUR;
     public static final long DM_RESTART_MONITORING_INTERVAL = 5 * C.SEC;
 
     public static final int DEFAULT_REFRESH_DELAY = 100;
