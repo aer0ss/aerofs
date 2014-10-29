a858013da1c651e0e93c13919af81c3b4ae46d7b
Drew Fisher <drew@aerofs.com>
2014-10-29 15:39:48 -0700

    Remove SV (the service) and packaging
    The server shall remain in good standing until we move email off of it.
    
    Change-Id: Icec51f8e3a93f1ed29f23cdbca9b023ac62ccef7

--- puppetmaster/manifests/nodes/sv.pp
+++ puppetmaster/manifests/nodes/sv.pp
@@ -6,33 +6,9 @@ node "sv.aerofs.com" inherits default {
 
     include public-email-creds
 
-    # install sv servlet
-    include servlet
-    class{"servlet::config::sv":
-        mysql_password => hiera("mysql_password"),
-        mysql_endpoint => hiera("mysql_endpoint")
-    }
-
     include mailserver
 
     class{"analytics":}
-
-    cron{"remove old defects":
-        command => '/usr/bin/clean_defects && symlinks -dr /var/svlogs_prod/defect > /dev/null 2>&1',
-        minute  => "0",
-        # runs 4 times a day and at 23h UTC which is 1-2h before the pagerduty probe fires
-        hour    => ["5","11","17","23"],
-    }
-    cron{"remove empty defects":
-        command => 'find /var/svlogs_prod/defect/ -iname log.defect\* -size 0 | xargs rm',
-        hour    => "0",
-        minute  => "0"
-    }
-    cron{"remove old logs":
-        command => 'find /var/svlogs_prod/archived/ -iname \*.gz -mtime +60 | xargs rm',
-        hour    => "0",
-        minute  => "0"
-    }
 
     include proguard
 
@@ -44,10 +20,6 @@ node "sv.aerofs.com" inherits default {
     }
 
     package { "gnuplot":
-        ensure => installed
-    }
-    # Used to clean up dangling symlinks left by clean_defects
-    package { "symlinks":
         ensure => installed
     }
 }
