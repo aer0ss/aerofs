2f405f044a3a474ca91dbcd655cb3958b24f972d
Matt Pillar <matt@aerofs.com>
2014-11-06 08:35:47 -0800

    Jingle: disable for kiwi.ki users.
    We need a better way to disable jingle at the org level. For now this is
    a stop gap for one of our larger paid HC deployments.
    
    Change-Id: Ic9449dbcb9507988225a093c9d3e3983771591e3

--- src/lib/src/com/aerofs/lib/cfg/CfgEnabledTransports.java
+++ src/lib/src/com/aerofs/lib/cfg/CfgEnabledTransports.java
@@ -15,7 +15,13 @@ public class CfgEnabledTransports
 
     public boolean isJingleEnabled()
     {
-        return Cfg.useJingle() && PrivateDeploymentConfig.isHybridDeployment();
+        // Conditions for jingle to be enabled:
+        //  1. No "nostun" flag.
+        //  2. Must be HC deployment.
+        //  3. Must not be a kiwi.ki user (see https://aerofs.atlassian.net/browse/ENG-2190).
+        return Cfg.useJingle() &&
+                PrivateDeploymentConfig.isHybridDeployment() &&
+                !Cfg.user().getString().endsWith("@kiwi.ki");
     }
 
     public boolean isZephyrEnabled()
