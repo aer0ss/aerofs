6b210909ab6a41f6f7eab6db767b2a975ae6d569
Drew Fisher <drew@aerofs.com>
2015-01-22 15:27:36 -0800

    safetynet backup step: don't trip on sockets
    Change-Id: Iccf127c2bf17d2b2d5aca1c47d815a2bd6c21223

--- tools/build/safetynet/syncdet_deploy/safetynet/manage/backup_installation.py
+++ tools/build/safetynet/syncdet_deploy/safetynet/manage/backup_installation.py
@@ -6,6 +6,19 @@ from lib.app.cfg import get_cfg
 from lib import aero_shutil as shutil
 from safetynet import param
 
+def ignore_special(folder, children):
+    """
+    An exclusion filter for shutil.copytree that ignores anything that isn't a
+    standard file, folder, or symlink.  This is needed to avoid trying to copy
+    socket files in the rtroot on Linux and OSX.
+    """
+    to_ignore = []
+    for child in children:
+        path = os.path.join(folder, child)
+        if not os.path.isfile(path) and not os.path.isdir(path) and not os.path.islink(path):
+            to_ignore.append(child)
+    return to_ignore
+
 def backup_installation():
     user_data_path = case.user_data_folder_path()
     cfg = get_cfg()
@@ -22,8 +35,8 @@ def backup_installation():
 
     # Copy the contents of app_root and rt_root to the backup
     # destination folders
-    shutil.copytree(app_root, dst_app_root)
-    shutil.copytree(rt_root, dst_rt_root)
+    shutil.copytree(app_root, dst_app_root, ignore=ignore_special)
+    shutil.copytree(rt_root, dst_rt_root, ignore=ignore_special)
 
 # The timeout is large (5 minutes) because the SafetyNet clients are running in VMs
 # hosted on the same VM host as the CI VMs. Disk I/O becomes unbearably slow
