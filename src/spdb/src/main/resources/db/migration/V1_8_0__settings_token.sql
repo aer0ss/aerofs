0316c948a901dd11af607d03b0676843385b72d6
Rudy Dai <rudy@aerofs.com>
2015-01-27 11:27:02 -0800

    use default charset latin-1
    mysql can't handle indices on columns longer than 767 bytes :(
    
    Change-Id: Ie5f763876584f8ca105007588a2dddaf35cd49b0

--- src/spdb/src/main/resources/db/migration/V1_8_0__settings_token.sql
+++ src/spdb/src/main/resources/db/migration/V1_8_0__settings_token.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS sp_settings_token (
     `st_uid` VARCHAR(320) NOT NULL PRIMARY KEY,
     `st_token` VARCHAR(255) NOT NULL /* must be same as bifrost.accesstoken.token */
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
