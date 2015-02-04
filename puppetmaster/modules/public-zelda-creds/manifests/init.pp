51424a72c027b10d324d13076d2fe8d35cc01deb
Abhishek Sharma <abhishek@aerofs.com>
2015-02-04 10:50:57 -0800

    fix for broken public server deploy on CI
    Change-Id: I8a02663058990c891d9c654c76d9085a528d8dea

--- puppetmaster/modules/public-zelda-creds/manifests/init.pp
+++ puppetmaster/modules/public-zelda-creds/manifests/init.pp
@@ -1,10 +1,4 @@
 class public-zelda-creds {
-    file {"/etc/aerofs":
-        ensure => directory,
-        owner  => "root",
-        group  => "root",
-        mode   => "644",
-    }
     file {"/etc/aerofs/zelda.properties":
         require => File["/etc/aerofs"],
         source => "puppet:///modules/public-zelda-creds/zelda.properties",
