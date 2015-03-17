8677c57e2908074295f2f6ac5e0fd3a5769e0e84
Matt Pillar <matt@aerofs.com>
2015-03-17 11:03:08 -0700

    Docker: improvements to upgrade-tools script for linux compatibility.
    Change-Id: Ibcd3f2a6a6f3d46b261c030d577933c74f0965fe

--- docker/dev/upgrade-tools.sh
+++ docker/dev/upgrade-tools.sh
@@ -1,47 +1,80 @@
 #!/bin/bash
 set -ex
 
+function yellow_print()
+{
+    (set +x
+        YELLOW='1;33'
+        cecho() { echo -e "\033[$1m$2\033[0m"; }
+        echo
+        cecho ${YELLOW} "$@"
+        echo
+    )
+}
+
 if [ -z "$(which vboxmanage)" ]; then
     echo 'Please install VirtualBox first.'
     exit 11
 fi
 
-# Install docker cli
-brew update && brew install --upgrade docker graphviz
+# Mac.
+if [ $(uname -s) = "Darwin" ]
+then
+    # Install docker cli
+    brew update && brew install --upgrade docker graphviz
 
-# Install crane
-bash -c "$(curl -sL https://raw.githubusercontent.com/michaelsauter/crane/master/download.sh)"
-mv crane /usr/local/bin/crane
+    # Install crane
+    bash -c "$(curl -sL https://raw.githubusercontent.com/michaelsauter/crane/master/download.sh)"
+    mv crane /usr/local/bin/crane
 
-# Install docker-machine
-wget https://github.com/docker/machine/releases/download/v0.1.0-rc4/docker-machine_darwin-amd64 \
-    -O /usr/local/bin/docker-machine
-chmod +x /usr/local/bin/docker-machine
+    # Install docker-machine
+    wget https://github.com/docker/machine/releases/download/v0.1.0-rc4/docker-machine_darwin-amd64 \
+        -O /usr/local/bin/docker-machine
+    chmod +x /usr/local/bin/docker-machine
+
+    # Do not use sudo on mac.
+    SUDO=
+fi
+
+# Linux.
+if [ $(uname -s) = "Linux" ]
+then
+    # Dev must install crane, docker-machine manually.
+    if [ -z "$(which crane)" ] || [ -z "$(which docker-machine)" ]
+    then
+        echo 'Please install crane and docker-machine manually.'
+        exit 22
+    fi
+
+    # Sudo is required on linux for certain commands.
+    SUDO=sudo
+fi
 
 VM=dev
 
 # Install dk-ip
-cat > /usr/local/bin/dk-ip <<END
+cat > /tmp/dk-ip <<END
 #!/bin/sh
 docker-machine ip ${VM}
 END
-chmod +x /usr/local/bin/dk-ip
+${SUDO} mv /tmp/dk-ip /usr/local/bin/dk-ip
+${SUDO} chmod +x /usr/local/bin/dk-ip
 
 # Create a docker machine 'dev'
-if [ -z "$(vboxmanage list vms | grep "^\"${VM}\"")" ]; then
+#if [ -z "$(vboxmanage list vms | grep "^\"${VM}\"")" ]
+echo "$(vboxmanage list vms)"
+if [ -z "$(vboxmanage list vms | grep "${VM}")" ]
+then
     docker-machine create -d virtualbox --virtualbox-disk-size 100000 --virtualbox-memory 3072 dev
 
     # Set environment variables for current and futher bash processes
     $(docker-machine env dev)
-    if [ -z "$(grep 'docker-machine env ${VM}' ${HOME}/.bash_profile)" ]; then
+    if [ -z "$(grep 'docker-machine env ${VM}' ${HOME}/.bash_profile)" ]
+    then
         echo '$(docker-machine env ${VM})' >> "${HOME}/.bash_profile"
     fi
 
-    (set +x
-        YELLOW='1;33'
-        cecho() { echo -e "\033[$1m$2\033[0m"; }
-        echo
-        cecho ${YELLOW} 'Please restart other bash terminals for settings to take effects'
-        echo
-    )
+    yellow_print 'Please restart other bash terminals for settings to take effect.'
+else
+    yellow_print 'Docker-machine vm already created; nothing to do.'
 fi
