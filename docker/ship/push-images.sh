939dc863f89071ca14f1250facf42c259cfcc40e
Weihan Wang <weihan@aerofs.com>
2015-04-01 14:12:07 -0700

    Docker: fix & improve backup procedure
    Change-Id: I4ba86519830ba11cf2597da13f5de66d986d4691

--- docker/ship/push-images.sh
+++ docker/ship/push-images.sh
@@ -16,12 +16,26 @@ yml() {
 LOADER_IMAGE=$(yml 'loader')
 TAG=$(docker run --rm ${LOADER_IMAGE} tag)
 
-# read -p "Are you sure to release a new version ${TAG}? CR to continue; Ctl-C to cancel. "
-
 PUSH_REPO=$(yml 'push-repo')
 if [ -z "${PUSH_REPO}" ]; then
     PUSH_REPO=$(yml 'repo')
 fi
+
+push() {
+    (set +e
+        RETRY=0
+        for i in 1 2 3; do
+            RETRY=$[RETRY + 1]
+            docker push $1
+            if [ $? = 0 ]; then
+                break
+            elif [ ${RETRY} = 5 ]; then
+                echo "ERROR: Retried too many times. I gave up."
+                exit 22
+            fi
+        done
+    )
+}
 
 for i in $(docker run --rm ${LOADER_IMAGE} images); do
     echo "============================================================"
@@ -29,7 +43,7 @@ for i in $(docker run --rm ${LOADER_IMAGE} images); do
     echo "============================================================"
     PUSH_IMAGE="${PUSH_REPO}/${i}:${TAG}"
     docker tag -f "${i}" "${PUSH_IMAGE}"
-    docker push "${PUSH_IMAGE}"
+    push "${PUSH_IMAGE}"
     docker rmi "${PUSH_IMAGE}"
 done
 
@@ -38,7 +52,7 @@ echo " Pushing ${LOADER_IMAGE}:latest to ${PUSH_REPO}..."
 echo "============================================================"
 LOADER_PUSH_IMAGE=${PUSH_REPO}/${LOADER_IMAGE}
 docker tag -f ${LOADER_IMAGE} ${LOADER_PUSH_IMAGE}
-docker push ${LOADER_PUSH_IMAGE}
+push ${LOADER_PUSH_IMAGE}
 docker rmi ${LOADER_PUSH_IMAGE}
 
 
