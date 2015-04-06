f8bc28013edc4adea0b3436e377506c2a914dd7c
Jon Pile <jon@aerofs.com>
2015-04-06 14:08:45 -0700

    Fix unit test breakage
    @Inject considered necessary :)
    
    Change-Id: I663adede903e57ac831831db3775929793d596be

--- src/daemon/src/com/aerofs/daemon/core/update/DPUTMigrateStorageConfig.java
+++ src/daemon/src/com/aerofs/daemon/core/update/DPUTMigrateStorageConfig.java
@@ -4,6 +4,8 @@ import com.aerofs.base.Loggers;
 import com.aerofs.lib.cfg.CfgDatabase;
 import org.slf4j.Logger;
 
+import javax.inject.Inject;
+
 /**
  * Rename the `s3_encryption_password` configuration value to `remote_storage_encryption_password`
  * (because this password is now used by both S3 and Swift backends)
@@ -13,6 +15,7 @@ public class DPUTMigrateStorageConfig implements IDaemonPostUpdateTask
     private final static Logger l = Loggers.getLogger(DPUTMigrateStorageConfig.class);
     private final CfgDatabase _db;
 
+    @Inject
     public DPUTMigrateStorageConfig(CfgDatabase db)
     {
         _db = db;
