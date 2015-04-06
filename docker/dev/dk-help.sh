896388bdaf92f96bc2135ee2ce3e507e690780fc
Weihan Wang <weihan@aerofs.com>
2015-04-06 16:13:31 -0700

    Docker: add dk-help
    Change-Id: Idc2aa4f817f1999dfd77584d9421d1e7a1ae84ee

---
+++ docker/dev/dk-help.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Use quotes to avoid escaping
+cat <<'END'
+
+The dk-* command family supports local development of the AeroFS appliance. Whereas the production system runs Docker
+containers in an VM, local development runs them directly on the host. It allows you to control and interact with the
+containers using local docker commands. "DK" stands for "Development Kit", "DocKer", "DrunK DonKey", or "Denmark",
+whichever you prefer.
+
+Prerequisites:
+    - understand common docker and crane commands such as 'start', 'kill', 'rm'
+    - run upgrade-tools.sh and restart bash before using dk-* commands.
+
+Commands:
+    dk-create   build, launch, and configure appliance containers. Previous appliance containers will be destroyed.
+    dk-reconfig identical to dk-create but it skips image building
+    dk-halt     stop all appliance containers
+    dk-start    start halted appliance containers
+    dk-restart  restart all appliance containers
+    dk-destroy  stop and remove all appliance containers
+    dk-reload   remove and restart given containers and all the containers that depend on them
+    dk-exec     execute commands in a given running container
+    dk-crane    run `crane` commands using the appliance's crane file at docker/crane.yml
+
+Example:
+
+Follow these steps to start developing and testing an AeroFS service 'foo' from scratch:
+
+    $ docker/upgrade-tools.sh   # install required tools and create a docker-machine VM. it may take a while.
+    $ bash -l                   # start a new bash
+    $ dk-create                 # build and launch the entire appliance. it may take a while.
+    $ make -C src/foo           # rebuild foo's Docker image after some code change
+    $ dk-reload foo             # reload the container 'foo' using the newly built image (see docker/crane.yml for container names)
+    $ dk-exec foo ps aux        # list all running processes in the container 'foo'
+    $ dk-crane status           # list container status
+    $ dk-halt                   # stop the entire appliance. Use dk-start to restart it
+
+END
--- tools/bashrc/docker-aliases.sh
+++ tools/bashrc/docker-aliases.sh
@@ -2,22 +2,14 @@
 
 DEV_DIR="$(dirname "${BASH_SOURCE[0]:-$0}")/../../docker/dev"
 
-# Run crane from any folder
+# Type dk-help to see information about these commands
+
+alias dk-help="${DEV_DIR}/dk-help.sh"
 alias dk-crane="${DEV_DIR}/dk-crane.sh"
-
-# Rebuild, recreate, and configure containers
 alias dk-create="${DEV_DIR}/dk-create.sh"
-
-# Recreate and configure containers but do not rebuild them
 alias dk-reconfig="${DEV_DIR}/dk-create.sh nobuild"
-
-# Recreate specified container and all the containers that depend on it.
-# Do not recreate Data container. Otherwise you need to run dk-reconfig to reconfig the system
 alias dk-reload="${DEV_DIR}/dk-reload.sh"
-
 alias dk-exec="docker exec -it"
-
-# Remove, start, stop, and restart all containers
 alias dk-destroy="${DEV_DIR}/dk-destroy.sh"
 alias dk-start="${DEV_DIR}/dk-crane.sh run -dall"
 alias dk-halt="${DEV_DIR}/dk-crane.sh kill -dall"
