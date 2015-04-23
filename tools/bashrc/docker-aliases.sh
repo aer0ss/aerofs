79e67ec34496a42e4441c62623444412119b2adc
Mickaël Bergem <mickael@aerofs.com>
2015-04-23 11:46:20 -0700

    Autostart docker-machine if needed on dk-start or dk-create
    Change-Id: Ia3d69198bb9fcb48939689e5cca105e9353bf6ac

--- tools/bashrc/docker-aliases.sh
+++ tools/bashrc/docker-aliases.sh
@@ -6,8 +6,9 @@ DEV_DIR="$(dirname "${BASH_SOURCE[0]:-$0}")/../../docker/dev"
 
 VM=docker-dev
 
+alias dk-startvm="([ -n \"\$(docker-machine ls | grep '${VM}.*Stopped')\" ] && echo 'docker-machine was not running, starting it...' && docker-machine start) && dk-env"
 alias dk-crane="${DEV_DIR}/dk-crane.sh"
-alias dk-create="${DEV_DIR}/../build-images.sh --unsigned && ${DEV_DIR}/dk-reconfig.sh"
+alias dk-create="dk-startvm; ${DEV_DIR}/../build-images.sh --unsigned && ${DEV_DIR}/dk-reconfig.sh"
 alias dk-createvm="docker-machine create -d virtualbox --virtualbox-disk-size 50000 --virtualbox-memory 3072 ${VM} && dk-env"
 alias dk-destroy="${DEV_DIR}/dk-destroy.sh"
 alias dk-destroyvm="docker-machine rm ${VM}"
@@ -19,7 +20,7 @@ alias dk-ip="docker-machine ip ${VM}"
 alias dk-reconfig="${DEV_DIR}/dk-reconfig.sh"
 alias dk-reload="${DEV_DIR}/dk-reload.sh"
 alias dk-restart="${DEV_DIR}/dk-crane.sh kill -dall && ${DEV_DIR}/dk-crane.sh kill -dall maintenance && ${DEV_DIR}/dk-crane.sh run -dall"
-alias dk-start="${DEV_DIR}/dk-crane.sh run -dall"
+alias dk-start="dk-startvm; ${DEV_DIR}/dk-crane.sh run -dall"
 
 # Autocomplete
 command -v autoload &>/dev/null
