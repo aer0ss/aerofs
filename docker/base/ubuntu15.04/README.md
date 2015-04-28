0ff71adda66b57aded5e24ca037f471bc31701da
Weihan Wang <weihan@aerofs.com>
2015-04-27 22:10:46 -0700

    Docker: replace Debian; use Ubuntu 15.04 as base image
    Change-Id: Id4a44d5831bd07f946e66bbc3f47a8949e8ceddd

--- docker/base/ubuntu15.04/README.md
+++
@@ -1,2 +0,0 @@
-We have some legacy services require Ubuntu and hence this base image. New services should use
-the default Debian base image.
--- docker/build-images.sh
+++ docker/build-images.sh
@@ -8,7 +8,6 @@ set -e
 DIRS="
     base/base
     base/ubuntu14.04
-    base/ubuntu15.04
     base/jre8
     base/jre8-and-mysql-client
     base/python2.7
