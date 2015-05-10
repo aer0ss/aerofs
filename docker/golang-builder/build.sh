23cbc2c30fb3cebe18b9d92d79997fd87c93c13f
Hugues Bruant <hugues@aerofs.com>
2015-05-09 20:59:37 -0700

    Fix golang-builder to work in ci
    Change-Id: I558292cbfb4e19df77972d26b9750a86ee093781

--- docker/golang-builder/build.sh
+++ docker/golang-builder/build.sh
@@ -1,8 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Usage: build.sh image service [source [mapping]]
+#
+#   image  : name of the docker image to be produced
+#   service: fully qualified name of the go package to build
+#   source : source folder
+#            default: $PWD/src
+#   mapping: where to mount the above folder in $GOPATH/src
+#            default: <service>
+#            useful if the source folder contains multiple top-level packages
 
-IMAGE_NAME=$1
-SERVICE_NAME=$2
+IMAGE=$1
+SERVICE=$2
+SOURCE_MAPPING=${3:-$(pwd)/src}:/gopath/src/${4:-$SERVICE}
+SOCKET_MAPPING=/var/run/docker.sock:/var/run/docker.sock
+DOCKER_MAPPING=$(which docker):/usr/local/bin/docker
 
-CGO_ENABLED=0 go get -a -x -installsuffix cgo -ldflags '-s -w' $SERVICE_NAME
-docker build -t $IMAGE_NAME -f $GOPATH/src/$SERVICE_NAME/Dockerfile $GOPATH
-
+echo "Building $SERVICE into $IMAGE from $SOURCE_MAPPING"
+docker run --rm -v $SOCKET_MAPPING -v $DOCKER_MAPPING -v $SOURCE_MAPPING \
+    aerofs/golang-builder $IMAGE $SERVICE
