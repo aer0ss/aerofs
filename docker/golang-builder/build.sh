3f147d127085af2f70e5e0df5e08bd1a64cd59a7
Hugues Bruant <hugues@aerofs.com>
2015-05-29 08:50:22 -0700

    Revert "Update and optimize golang-builder:"
    This reverts commit 106a539a40a7273bb4e4ad2dc3aaf56bfb8ab3ec.
    
    Change-Id: I1cf141f7e8f194300c19dd097e891c2d0b151747

--- docker/golang-builder/build.sh
+++ docker/golang-builder/build.sh
@@ -1,52 +1,24 @@
 #!/bin/bash
-set -e
 
-[[ $# -ge 2 ]] || {
-    echo "Usage: $0 <image> <service> [<local-import> ...]"
+[[ $# = 2 ]] || {
+    echo "Usage: $0 <image> <service>"
     echo "      <image>   name of the docker image to be produced"
-    echo "      <service> fully qualified name of the go package to build. e.g. 'aerofs.com/ca-server'"
-    echo "      <local-import> fully qualified name of the local go package that the service depends. "
-                            "e.g. 'aerofs.com/service'. The user must specify _all_ the local packages that the "
-                            "service imports otherwise building would fail."
+    echo "      <service> fully qualified name of the go package to build"
     exit 11
 }
 IMAGE=$1
 SERVICE=$2
-shift; shift
-LOCAL_IMPORTS=$@
 
-render_dockerfile() {
-    for i in ${SERVICE} ${LOCAL_IMPORTS}; do
-        # $'\n' is to insert newlines to env variable
-        # \\ is for sed to work with newlines
-        COPY_SRC="${COPY_SRC}\\"$'\n'"COPY ${i} /gopath/src/${i}"
-    done
-
-    sed -e "s@{{ copy_src }}@${COPY_SRC}@" \
-        -e "s@{{ service }}@${SERVICE}@" \
-        "${THIS_DIR}/Dockerfile.jinja" > "${DOCKERFILE}"
-
-    # See color code at http://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
-    echo ">>> Content of ${DOCKERFILE}:"
-    echo -e "\033[0;36m"
-    cat "${DOCKERFILE}"
-    echo -e "\033[0m"
-}
-
-BUILDER=aerofs/golang-builder__$(sed -e 's./.__.' <<< ${SERVICE})
-echo "Building ${BUILDER} ..."
-
-# Have to place Dockerfile to the src folder otherwise `docker build` would complain.
 THIS_DIR="$(dirname "$0")"
-SRC_DIR="${THIS_DIR}/../../golang/src"
-DOCKERFILE="${SRC_DIR}/Dockerfile.gen"
-render_dockerfile
 
-docker build -t ${BUILDER} -f "${DOCKERFILE}" "${SRC_DIR}"
-rm "${DOCKERFILE}"
+BUILDER_IMAGE=aerofs/golang-builder
+echo "Building ${BUILDER_IMAGE} ..."
+docker build -t ${BUILDER_IMAGE} "${THIS_DIR}"
 
-echo "Building container image ${IMAGE} ..."
-CMD="docker build -t ${IMAGE} -f /gopath/src/${SERVICE}/Dockerfile /gopath"
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock ${BUILDER} ${CMD}
-
-
+echo "Building ${SERVICE} into ${IMAGE} ..."
+SOURCE_MAPPING=$(git rev-parse --show-toplevel)/golang/src/aerofs.com:/gopath/src/aerofs.com
+SOCKET_MAPPING=/var/run/docker.sock:/var/run/docker.sock
+docker run --rm \
+    -v ${SOURCE_MAPPING} \
+    -v ${SOCKET_MAPPING} \
+    ${BUILDER_IMAGE} ${IMAGE} ${SERVICE}
