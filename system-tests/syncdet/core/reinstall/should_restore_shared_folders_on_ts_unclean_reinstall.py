18702a9921da6e5b544898611b03a67888bba42a
Hugues Bruant <hugues@aerofs.com>
2015-06-03 22:50:31 -0700

    Fix TS unclean reinstall test
    Change-Id: Ie8c381ee2cc2bc2fc05e771e6b6bfaaf300a715a

--- system-tests/syncdet/core/reinstall/should_restore_shared_folders_on_ts_unclean_reinstall.py
+++ system-tests/syncdet/core/reinstall/should_restore_shared_folders_on_ts_unclean_reinstall.py
@@ -14,8 +14,8 @@ import should_restore_shared_folders_on_ts_reinstall
 
 def ts_unclean_uninstall():
     aerofs_proc.stop_all()
-    # remove conf db to force reinstall
-    os.remove(os.path.join(get_cfg().get_rtroot(), "conf"))
+    # remove private key to force reinstall
+    os.remove(os.path.join(get_cfg().get_rtroot(), "key.pem"))
 
 
 def ts_unclean_validate(r0, r1):
