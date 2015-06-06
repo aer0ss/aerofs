f920bd13445840a4bb63a0f9b4d80aac263ee9a2
Weihan Wang <weihan@aerofs.com>
2015-06-05 17:14:14 -0700

    docker build generates QCOW2 images
    Change-Id: I82c554fe906b3e161f6e6b0578d8995d84449a36

--- docker/ship-aerofs/build-vm.sh
+++ docker/ship-aerofs/build-vm.sh
@@ -4,3 +4,13 @@ set -e
 THIS_DIR="$(dirname $0)"
 
 "${THIS_DIR}/build.sh" cloudinit,preloaded $@
+
+# Convert to absolute path for user friendly display
+OUTPUT_DIR="$(cd "${THIS_DIR}/../../out.ship/appliance/preloaded"; pwd)"
+
+BASENAME=$(basename "${OUTPUT_DIR}"/*.ova | sed -e 's/\.ova//')
+QCOW2="${OUTPUT_DIR}/${BASENAME}.qcow2"
+
+echo "Creating QCOW2 image ${QCOW2} ..."
+
+qemu-img convert -O qcow2 "${OUTPUT_DIR}/${BASENAME}.ova" "${QCOW2}"
