62da9dd7e91ef678df50308b4f08ce32c160b5e1
Hugues Bruant <hugues@aerofs.com>
2015-06-09 19:47:04 -0400

    Fix dns forwarding
    Use VPN forwarder for VPN hostnames
    Use host and VPN forwarder for appliance hostname
    
    Change-Id: I23235ee6ad07ae987990d1b7e854fde99ff94a45

--- tools/cache/rawdns/stop.sh
+++ tools/cache/rawdns/stop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker stop rawdns
+docker rm --force rawdns
+
 # remove dns config inserted by start.sh
 # FIXME: boot2docker is very minimalist so we have to use sed, which is pretty fragile...
 docker-machine ssh docker-dev "sudo cat /var/lib/boot2docker/profile | sed \"s/EXTRA_ARGS='--dns 172.17.42.1 /EXTRA_ARGS='/\" | sudo tee cat /var/lib/boot2docker/profile"
