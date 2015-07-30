ccc039e341a3a30b71ad3768d4117935089dcb4a
Matt Pillar <matt@aerofs.com>
2015-07-30 10:42:21 -0700

    Docker: Fix qcow2 build. Need to extract the OVA first.
    Change-Id: I1103ebeebee50ca6944b6675666b039eb7527f2f

--- docker/ship-aerofs/build-vm.sh
+++ docker/ship-aerofs/build-vm.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -eu
 
+# Build OVA, remove build artifacts, change to output directory.
 THIS_DIR="$(dirname $0)"
-
 "${THIS_DIR}/build.sh" cloudinit,preloaded $@
+cd "${THIS_DIR}/../../out.ship/appliance/preloaded"
+rm -f "disk.vdi"
+BASENAME=$(basename *.ova | sed -e 's/\.ova//')
 
-# Convert to absolute path for user friendly display
-OUTPUT_DIR="$(cd "${THIS_DIR}/../../out.ship/appliance/preloaded"; pwd)"
-
-BASENAME=$(basename "${OUTPUT_DIR}"/*.ova | sed -e 's/\.ova//')
-
-QCOW2="${OUTPUT_DIR}/${BASENAME}.qcow2"
-echo "Creating QCOW2 image ${QCOW2} ..."
-qemu-img convert -O qcow2 "${OUTPUT_DIR}/${BASENAME}.ova" "${QCOW2}"
-
-VMDK="${OUTPUT_DIR}/${BASENAME}.vmdk"
+# Build VMDK.
+VMDK="${BASENAME}.vmdk"
 echo "Creating VMDK image ${VMDK} ..."
-qemu-img convert -O vmdk "${OUTPUT_DIR}/${BASENAME}.ova" "${VMDK}"
+tar -xvf ${BASENAME}.ova
+mv ${BASENAME}-disk1.vmdk ${BASENAME}.vmdk
+rm -r ${BASENAME}.mf ${BASENAME}.ovf
 
-# Remove irrelevant build artifacts.
-rm -f "${OUTPUT_DIR}/disk.vdi"
+# Build QCow2.
+QCOW2="${BASENAME}.qcow2"
+echo "Creating QCOW2 image ${QCOW2} ..."
+qemu-img convert -f vmdk -O qcow2 "${VMDK}" "${QCOW2}"
+
+# Success!
+echo "Images successfully built."
