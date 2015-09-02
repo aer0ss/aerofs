4e3a6f03429d7fff8812a3d5222a46aedddd2500
Hugues Bruant <hugues@aerofs.com>
2015-09-02 13:49:59 -0400

    More windows actor tweaks
    Change-Id: I7a88e0db2691f6e0235a75cf32b8fb363b7fd8b6

--- tools/ci/create_and_register_actors.sh
+++ tools/ci/create_and_register_actors.sh
@@ -13,6 +13,10 @@ VAGRANT_BASE_DIR="$(dirname $(dirname $0))/vagrant"
 # Clear the pool
 mysql -uroot --password='temp123' < "$ACTOR_POOL_DIR"/actorpool.sql  # clear the db
 
+# get bridge interface base ip
+# TODO: take the netmask into account instead of assuming it to be 255.255.255.0
+bridge_ip=$(ifconfig $BRIDGE_IFACE | grep 'inet addr:' | cut -d':' -f 2 | cut -d'.' -f 1-3)
+
 # Linux actors
 pushd "$VAGRANT_BASE_DIR"/syncdet_linux
 vagrant destroy --force
@@ -32,7 +36,18 @@ bottom=0
 top=$(($WCLIENT_COUNT - 1))
 popd
 if [[ $top -ge $bottom ]]; then
-    printf "aerofsbuild-win7-vagrant-%i " $(seq $bottom $top) | "$ACTOR_POOL_DIR"/register.py --os w --vm y --isolated n
+    # windows actors do not play well with dnsmasq or whatever piece of magic maps hostname to ips
+    # and their bridged ips have an annoying tendency to change when they are recreated, despite
+    # our best efforts to give them static ips
+    # for robustness, ssh into the VMs to figure out the actual bridged IP
+    # TODO: a future iteration of actor provisioning should instead move the registration to the actor
+    # pool inside the VM itself, allowing the actor pool to determine the public IP of the actor...
+    for i in $(seq $bottom $top) ; do
+        # NB: use the hostonly ip to ssh as this one is guaranteed to be static
+        ip=$(ssh aerofstest@192.168.50.$(( 100 + i)) ipconfig | grep -F "IPv4 Address" | grep -F " : ${bridge_ip}." | cut -d':' -f2)
+        register="${register:+$register }$ip"
+    done
+    echo $register | "$ACTOR_POOL_DIR"/register.py --os w --vm y --isolated n
 fi
 
 # OSX actors
