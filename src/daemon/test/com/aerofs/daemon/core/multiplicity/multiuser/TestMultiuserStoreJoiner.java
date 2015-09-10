5202c45c683210c028f832f4e7aa080ce9b52a07
Abhishek Sharma <abhishek@aerofs.com>
2015-09-10 12:23:50 -0700

    Fix for failing SAStoreJoiner test
    Change-Id: I24cf9b0a3dda6b0bcc438c28b478f9b57d328225

--- src/daemon/test/com/aerofs/daemon/core/multiplicity/multiuser/TestMultiuserStoreJoiner.java
+++ src/daemon/test/com/aerofs/daemon/core/multiplicity/multiuser/TestMultiuserStoreJoiner.java
@@ -5,31 +5,13 @@
 package com.aerofs.daemon.core.multiplicity.multiuser;
 
 import com.aerofs.daemon.core.lib.BaseStoreJoinerTest;
-import com.aerofs.daemon.core.phy.PhysicalOp;
-import com.aerofs.daemon.core.store.StoreDeleter;
-import com.aerofs.daemon.core.store.StoreHierarchy;
-import com.aerofs.ids.SID;
-import com.aerofs.ids.UserID;
-import com.aerofs.lib.cfg.CfgRootSID;
-import com.aerofs.lib.id.SIndex;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-
-import static org.mockito.Mockito.*;
 
 public class TestMultiuserStoreJoiner extends BaseStoreJoinerTest
 {
-    @Mock CfgRootSID cfgRootSID;
-    @Mock StoreHierarchy stores;
-    @Mock StoreDeleter sd;
-
     @InjectMocks MultiuserStoreJoiner msj;
-
-    SIndex sidx = new SIndex(123);
-
-    UserID userID = UserID.fromInternal("test@gmail");
 
     @Before
     public void setUp() throws Exception
@@ -52,45 +34,24 @@ public class TestMultiuserStoreJoiner extends BaseStoreJoinerTest
     @Test
     public void joinStore_shouldNotJoinOwnRootStore() throws Exception
     {
-        SID rootSID = SID.rootSID(userID);
-        when(sidx2sid.getNullable_(sidx)).thenReturn(rootSID);
-        when(cfgRootSID.get()).thenReturn(rootSID);
-
-        msj.joinStore_(sidx, rootSID, sf("test", false), t);
-
-        verifyZeroInteractions(sc);
+        super.joinStore_shouldNotJoinOwnRootStore();
     }
 
     @Test
     public void leaveStore_shouldLeaveRootStore() throws Exception
     {
-        SID sid = SID.generate();
-        when(sidx2sid.getNullable_(sidx)).thenReturn(sid);
-        when(stores.isRoot_(sidx)).thenReturn(true);
-        msj.leaveStore_(sidx, sid, t);
-
-        verify(sd).deleteRootStore_(sidx, PhysicalOp.APPLY, t);
+        super.leaveStore_shouldLeaveRootStore();
     }
 
     @Test
     public void leaveStore_shouldNotLeaveNonRootStore() throws Exception
     {
-        SID sid = SID.generate();
-        when(sidx2sid.getNullable_(sidx)).thenReturn(sid);
-        when(stores.isRoot_(sidx)).thenReturn(false);
-        msj.leaveStore_(sidx, sid, t);
-
-        verifyZeroInteractions(sd);
+        super.leaveStore_shouldNotLeaveNonRootStore();
     }
 
     @Test
     public void leaveStore_shouldNotLeaveAbsentStore() throws Exception
     {
-        SID sid = SID.generate();
-        when(sidx2sid.getNullable_(sidx)).thenReturn(null);
-        when(stores.isRoot_(sidx)).thenReturn(true);
-        msj.leaveStore_(sidx, sid, t);
-
-        verifyZeroInteractions(sd);
+        super.leaveStore_shouldNotLeaveAbsentStore();
     }
 }
--- src/sync/src/test/java/com/aerofs/daemon/core/lib/BaseStoreJoinerTest.java
+++ src/sync/src/test/java/com/aerofs/daemon/core/lib/BaseStoreJoinerTest.java
@@ -1,27 +1,31 @@
 package com.aerofs.daemon.core.lib;
 
 import com.aerofs.base.acl.Permissions;
-import com.aerofs.daemon.core.store.IMapSIndex2SID;
-import com.aerofs.daemon.core.store.IStoreJoiner;
-import com.aerofs.daemon.core.store.StoreCreator;
+import com.aerofs.daemon.core.phy.PhysicalOp;
+import com.aerofs.daemon.core.store.*;
 import com.aerofs.daemon.lib.db.trans.Trans;
 import com.aerofs.ids.SID;
 import com.aerofs.ids.UserID;
+import com.aerofs.lib.cfg.CfgRootSID;
 import com.aerofs.lib.id.SIndex;
 import com.aerofs.testlib.AbstractTest;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import org.junit.Test;
 import org.mockito.Mock;
 
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BaseStoreJoinerTest extends AbstractTest
 {
-    protected @Mock IMapSIndex2SID sidx2sid;
-    protected @Mock StoreCreator sc;
-    protected @Mock Trans t;
+    private @Mock IMapSIndex2SID sidx2sid;
+    private @Mock StoreCreator sc;
+    private @Mock Trans t;
+    private @Mock CfgRootSID cfgRootSID;
+    private @Mock StoreHierarchy stores;
+
+    private @Mock StoreDeleter sd;
     protected IStoreJoiner isj;
 
     UserID userID = UserID.fromInternal("test@gmail");
@@ -51,5 +55,48 @@ public class BaseStoreJoinerTest extends AbstractTest
         isj.joinStore_(sidx, sid, sf("test", false), t);
 
         verify(sc).createRootStore_(eq(sid), eq("test"), eq(t));
+    }
+
+    public void joinStore_shouldNotJoinOwnRootStore() throws Exception
+    {
+        SID rootSID = SID.rootSID(userID);
+        when(sidx2sid.getNullable_(sidx)).thenReturn(rootSID);
+        when(cfgRootSID.get()).thenReturn(rootSID);
+        isj.joinStore_(sidx, rootSID, sf("test", false), t);
+
+        verifyZeroInteractions(sc);
+    }
+
+    @Test
+    public void leaveStore_shouldLeaveRootStore() throws Exception
+    {
+        SID sid = SID.generate();
+        when(sidx2sid.getNullable_(sidx)).thenReturn(sid);
+        when(stores.isRoot_(sidx)).thenReturn(true);
+        isj.leaveStore_(sidx, sid, t);
+
+        verify(sd).deleteRootStore_(sidx, PhysicalOp.APPLY, t);
+    }
+
+    @Test
+    public void leaveStore_shouldNotLeaveNonRootStore() throws Exception
+    {
+        SID sid = SID.generate();
+        when(sidx2sid.getNullable_(sidx)).thenReturn(sid);
+        when(stores.isRoot_(sidx)).thenReturn(false);
+        isj.leaveStore_(sidx, sid, t);
+
+        verifyZeroInteractions(sd);
+    }
+
+    @Test
+    public void leaveStore_shouldNotLeaveAbsentStore() throws Exception
+    {
+        SID sid = SID.generate();
+        when(sidx2sid.getNullable_(sidx)).thenReturn(null);
+        when(stores.isRoot_(sidx)).thenReturn(true);
+        isj.leaveStore_(sidx, sid, t);
+
+        verifyZeroInteractions(sd);
     }
 }
