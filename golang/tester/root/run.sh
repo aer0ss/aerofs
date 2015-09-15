3c19f74f0682cb7f655fc0204cad8b07978c57b5
Jon Pile <jon@aerofs.com>
2015-09-15 10:26:53 -0700

    Add mysql to golang-tester image
    Took a little work to figure out the right incantation for unattended
    setup and start, but the golang tests will have access to a passwordless
    mysql image.
    
    Change-Id: I4dd37d7129405741c09de6cde42538621d5f5942

--- golang/tester/root/run.sh
+++ golang/tester/root/run.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Hey, databases! Let's start this one for tests. Annoying, right?
+service mysql start
+
 for d in $(ls "/gopath/src/$1") ; do
     go get -t "$1/$d"
     go test -v "$1/$d"
