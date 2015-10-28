2b8a2fea47e5be50dfff8e948bce4885c7338259
Matt Pillar <matt@aerofs.com>
2015-10-28 15:16:21 -0700

    Docker: do not regerate deployment secret if already exists. ENG-3175.
    Change-Id: I04f725f18bd8c7f81077c2ce26a084ca8838eb65

--- docker/data-container/root/run.sh
+++ docker/data-container/root/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Creating deployment secret..."
-python -c "import os; print os.urandom(16).encode('hex')" > /data/deployment_secret
+if [ ! -f /data/deployment_secret ]
+then
+    echo "Creating deployment secret..."
+    python -c "import os; print os.urandom(16).encode('hex')" > /data/deployment_secret
+else
+    echo "Deployment secret already created. No-op."
+fi
 
 echo "Exiting..."
