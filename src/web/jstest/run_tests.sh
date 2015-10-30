92a42e69d093d717631c2684b468dd0cb08103be
Michelle Becker <michelle@aerofs.com>
2015-10-30 12:41:21 -0700

    ENG-3186 fix run tests script
    Change-Id: Iadda57aaa50bbe9478d0373bf48eaae885c1dfae

--- src/web/jstest/run_tests.sh
+++ src/web/jstest/run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+set -e
 cd $(dirname $0)
 
 # TODO: Streamline this since it will get annoying really quickly (MB)
-karma start shelob/karma.conf.unit.js
-karma start shelob/karma.conf.e2e.js
-karma start strider/karma.conf.unit.js
-karma start ng-modules/karma.conf.unit.js
+./node_modules/.bin/karma start shelob/karma.conf.unit.js
+./node_modules/.bin/karma start shelob/karma.conf.e2e.js
+./node_modules/.bin/karma start strider/karma.conf.unit.js
+./node_modules/.bin/karma start ng-modules/karma.conf.unit.js
