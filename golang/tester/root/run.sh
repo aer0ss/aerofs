db41b054cb8c0ffa01afe3b8aaeaf9f86da00d3a
Hugues Bruant <hugues@aerofs.com>
2015-12-11 17:04:10 -0500

    Enable vendoring in test container
    Change-Id: I4d1b9334e1b6dfcacfaad2ef4952a198cd861481

--- golang/tester/root/run.sh
+++ golang/tester/root/run.sh
@@ -6,7 +6,7 @@ set -e
 service mysql start
 
 for d in $(ls "/gopath/src/$1") ; do
-    go get -t "$1/$d"
-    go test -v "$1/$d"
+    GO15VENDOREXPERIMENT=1 go get -t "$1/$d"
+    GO15VENDOREXPERIMENT=1 go test -v "$1/$d"
 done
 
