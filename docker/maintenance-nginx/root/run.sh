1a60ba732c03882162f1b0e14a9bb61458800046
Matt Pillar <matt@aerofs.com>
2015-12-15 16:05:26 -0800

    ENG-3501 Config service should be available in maintenance mode.
    This allows us to do the fancy DNS trick described in the following
    support article.
    
    https://support.aerofs.com/hc/en-us/articles/204985954
    
    Change-Id: I1553de80c6534f66969dba5850b7f5b73bb115a4

--- docker/maintenance-nginx/root/run.sh
+++ docker/maintenance-nginx/root/run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 set -e
+
+mkdir -p /etc/nginx/certs
+
+/container-scripts/copy-ca-cert /etc/nginx/certs
+
+echo "Creating AeroFS internal cert..."
+/container-scripts/certify $(/container-scripts/get-config-property base.host.unified) /etc/nginx/certs/aerofs
+# Append issuer cert onto the end of the cert chain
+cat /etc/nginx/certs/cacert.pem >> /etc/nginx/certs/aerofs.crt
+
 /run-common.sh
