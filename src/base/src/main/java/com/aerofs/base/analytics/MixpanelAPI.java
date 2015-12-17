ed4c31d89332812329d25c672bd5a1ae77251791
Rudy Dai <rudy@aerofs.com>
2015-12-17 14:59:52 -0800

    fast return on analytics not configured
    Change-Id: I05e477cbd8d469cf16c31780ef0dc2607ff16311

--- src/base/src/main/java/com/aerofs/base/analytics/MixpanelAPI.java
+++ src/base/src/main/java/com/aerofs/base/analytics/MixpanelAPI.java
@@ -70,38 +70,9 @@ public class MixpanelAPI
      *          additional custom properties in a name-value map
      */
     public void track(final String event, @Nullable final String distinctId,
-            @Nullable final Map<String, String> additionalProperties)
-                throws IOException
+                      @Nullable final Map<String, String> additionalProperties)
+            throws IOException
     {
-
-            JsonObject json = new JsonObject();
-            json.addProperty("event", checkNotNull(event));
-
-            JsonObject properties = new JsonObject();
-            json.add("properties", properties);
-
-            // Add the properties
-            properties.addProperty("token", _token);
-            if (distinctId != null) properties.addProperty("distinct_id", distinctId);
-            if (additionalProperties != null) {
-                for (Entry<String, String> entry : additionalProperties.entrySet()) {
-                    properties.addProperty(entry.getKey(), entry.getValue());
-                }
-            }
-
-            final String message = json.toString();
-            final String url = _apiEndpoint + Base64.encodeBytes(message.getBytes());
-
-            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-            conn.setConnectTimeout(SOCKET_TIMEOUT);
-            conn.setReadTimeout(SOCKET_TIMEOUT);
-
-            String reply = BaseUtil.httpRequest(conn, null);
-            if (!reply.equals("1")) {
-                throw new IOException("mixpanel failed. Reply: " + reply + " for url: " + url);
-            }
-
-            l.debug("mixpanel send ok: {} ", url);
-
+        // do nothing, we don't use mixpanel anymore
     }
 }
