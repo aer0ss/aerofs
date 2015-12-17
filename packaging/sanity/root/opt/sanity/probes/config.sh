687fcd155d4ed5ccfaacd88015cff35ae7dc2720
Gregory Schlomoff <greg@aerofs.com>
2015-12-17 14:49:04 -0800

    status: make the Zephyr probe use the config service to find Zephyr
    This is needed for HPC since zephyr is running on a separate box there
    
    Also: add a probe for the config service
    
    Change-Id: I28c639ec0ca53ed53ea53e04c6852ae09d9eabd6

---
+++ packaging/sanity/root/opt/sanity/probes/config.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -e
+/opt/sanity/probes/tools/url.sh http://config.service:5434/client
--- packaging/sanity/root/opt/sanity/probes/zephyr.sh
+++ packaging/sanity/root/opt/sanity/probes/zephyr.sh
@@ -1,2 +1,20 @@
-#!/bin/bash -e
-/opt/sanity/probes/tools/port.sh zephyr.service 8888
+#!/bin/bash
+set -e
+
+# Note: we can't use Docker links to resolve Zephyr's address like we do for other probes because on
+# Hosted Private Cloud Zephyr will be running on a separate machine so Docker links won't work.
+# Querying the config server is the only way to get Zephyr's address.
+
+ZEPHYR_ADDRESS=$(/container-scripts/get-config-property base.zephyr.address)
+
+if [ -z "$ZEPHYR_ADDRESS" ]; then
+    echo "Zephyr address not found. Is the config service down?"
+    exit 1
+fi
+
+# Split the address in host and port
+IFS=':' read -a TMP <<< $ZEPHYR_ADDRESS
+ZEPHYR_HOST=${TMP[0]}
+ZEPHYR_PORT=${TMP[1]}
+
+/opt/sanity/probes/tools/port.sh $ZEPHYR_HOST $ZEPHYR_PORT
