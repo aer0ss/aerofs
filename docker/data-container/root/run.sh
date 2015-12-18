efb093cb2623c9c3fc6f47289c799c3e57437a91
Hugues Bruant <hugues@aerofs.com>
2015-12-18 10:59:11 -0800

    Leaner data container
    Change-Id: Ibce0060b01d3775667b9b8fb3d2d3d036ee0186a

--- docker/data-container/root/run.sh
+++ docker/data-container/root/run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
+mkdir -p \
+    /data/bunker \
+    /data/web \
+    /data/redis
+
 if [ ! -f /data/deployment_secret ]
 then
     echo "Creating deployment secret..."
-    python -c "import os; print os.urandom(16).encode('hex')" > /data/deployment_secret
+    dd if=/dev/urandom bs=16 count=1 | hexdump -v -e '8/2 "%04x"' > /data/deployment_secret
 else
     echo "Deployment secret already created. No-op."
 fi
