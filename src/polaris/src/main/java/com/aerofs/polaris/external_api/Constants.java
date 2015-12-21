ed694f532f0d07a8fa7ed38a0777f34b76a196c4
Alex Tsay <alex@aerofs.com>
2015-12-20 22:48:16 -0800

    ENG-3548 Updated Polaris API version to 1.4.
    Change-Id: Iacef0834b615b493475d9cc70eae02e20b713688

--- src/polaris/src/main/java/com/aerofs/polaris/external_api/Constants.java
+++ src/polaris/src/main/java/com/aerofs/polaris/external_api/Constants.java
@@ -5,7 +5,7 @@ import com.aerofs.polaris.external_api.rest.util.Version;
 public class Constants
 {
     public static final String EXTERNAL_API_VERSION = "/v{version: [0-9]+\\.[0-9]+}";
-    public static final Version HIGHEST_SUPPORTED_VERSION = new Version(1, 3);
+    public static final Version HIGHEST_SUPPORTED_VERSION = new Version(1, 4);
     public final static String REQUEST_VERSION = "api-request-version";
     public final static String EXTERNAL_API_LOCATION = "http://polaris.service:8086/api";
 
