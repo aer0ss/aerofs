10260d54a585c68f3e2b63c35a2039fc4d13af2e
Gregory Schlomoff <greg@aerofs.com>
2015-12-28 11:37:57 -0800

    Disabling Zephyr probe
    Change-Id: I94250bd4266a43b127529f8cd55c71faa6baa980

--- packaging/sanity/root/opt/sanity/probes_disabled/zephyr.sh
+++ packaging/sanity/root/opt/sanity/probes_disabled/zephyr.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+###################################################
+# FIXME (GS): This probe has been disabled because some of our customers have weird networking
+# configurations where the box can't reach itself via its external IP address. This isn't a
+# problem for other services because we use Docker links instead of the external IP to reach
+# them - see comment below.
+###################################################
+
 # Note: we can't use Docker links to resolve Zephyr's address like we do for other probes because on
 # Hosted Private Cloud Zephyr will be running on a separate machine so Docker links won't work.
 # Querying the config server is the only way to get Zephyr's address.
