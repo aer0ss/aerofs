53d6c0c0ea0547041e3a104fa29b7618583ce094
Hugues Bruant <hugues@aerofs.com>
2016-01-24 23:53:27 -0500

    Pin boot2docker ISO version
    Until 1.10.0 is out
    
    Change-Id: I6e904de9077e8e6fd91ab7a02772153f37ffc924

--- docker/dev/dk-create-vm.sh
+++ docker/dev/dk-create-vm.sh
@@ -11,7 +11,12 @@ VM=$1
 
 if [ -z "$(docker-machine ls | grep ${VM})" ]
 then
+    # NB: explicit boot2docker URL is required for now because
+    #  1. lastest b2d release (1.9.1) has a kernel/AUFS bug which causes some Java containers to hang
+    #  2. recent docker-machine versions automatically upgrade the cached boot2docker iso
+    # TODO: remove explicit URL once 1.10.0 is out
     docker-machine create -d virtualbox --virtualbox-disk-size 50000 \
+        --virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v1.9.0/boot2docker.iso \
         --virtualbox-memory 3072 --virtualbox-cpu-count 2 ${VM}
 
     # XXX
