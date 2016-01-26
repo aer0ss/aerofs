751d2aae02af0ecee4ea46658999ead5b642c0df
Hugues Bruant <hugues@aerofs.com>
2016-01-25 21:39:36 -0500

    One does not simply shell out to Docker CLI
    signup-decoder conencts directly to mysql through a python API
    like a decent human being instead of shelling out to docker
    and again to mysql client (I kid you not).
    
    bunker uses a python wrapper to the docker API instead of shelling
    out to the docker command line.
    
    Change-Id: I33b6d86cf41e50d0f416555f86d2f3ab175b3885

--- docker/dev/signup-decoder/root/decoder.py
+++ docker/dev/signup-decoder/root/decoder.py
@@ -1,60 +1,51 @@
+
+import MySQLdb
 from flask import Flask, request, jsonify
-from subprocess import check_output, Popen, PIPE
+
 app = Flask(__name__)
-IMAGE = 'aerofs/mysql'
 
 @app.route("/get_code", methods=["GET"])
 def get():
     user = request.args.get('userid')
     if not user: return "Must specify user id in the form of 'userid=email@address'", 400
 
-    # Find mysql container
-    container = None
-    for line in check_output(['docker', 'ps']).split('\n'):
-        if IMAGE in line:
-            # last word is the container name
-            container = line.strip().rsplit(' ', 1)[1]
-            break
-    if not container:
-        return 'found no container running image {}'.format(IMAGE), 400
-
-    # Run SQL in mysql container to print the last signup code of the given user
-    query = "select t_code from sp_signup_code where t_to=\'{}\' order by t_ts desc limit 1".format(user)
-    cmd = ['docker', 'exec', container, 'mysql', '-N', 'aerofs_sp', '-e', query]
-    print cmd
-    p = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    out, err = p.communicate()
-    if err:
-        return err, 400
-    else:
-        return jsonify(signup_code=out.strip())
+    try:
+        db = MySQLdb.connect("mysql.docker", db="aerofs_sp")
+        try:
+            c = db.cursor()
+            c.execute("select t_code from sp_signup_code where t_to=%s order by t_ts desc limit 1", [user])
+            r = c.fetchone()
+            if r is None:
+                return "no such user: " + user, 404
+            return jsonify(signup_code=r[0])
+        except MySQLdb.Error as e:
+            print e
+            return e.str(), 400
+        finally:
+            db.close()
+    except Exception as e:
+        print e
+        raise
 
 @app.route("/get_all_codes", methods=["GET"])
 def get_all():
-    # Find mysql container
-    container = None
-    for line in check_output(['docker', 'ps']).split('\n'):
-        if IMAGE in line:
-            # last word is the container name
-            container = line.strip().rsplit(' ', 1)[1]
-            break
-    if not container:
-        return 'found no container running image {}'.format(IMAGE), 400
-
-    # Run SQL in mysql container to print the last signup code of the given user
-    query = "select t_to, t_code from sp_signup_code where t_ts in ( select max(t_ts) from sp_signup_code group by t_to )"
-    cmd = ['docker', 'exec', container, 'mysql', '-N', 'aerofs_sp', '-e', query]
-    print cmd
-    p = Popen(cmd, stdout=PIPE, stderr=PIPE)
-    out, err = p.communicate()
-    if err:
-        return err, 400
-    else:
-        entries = []
-        for line in out.strip().split("\n"):
-            fields = line.split("\t")
-            entries.append({"user": fields[0], "signup_code": fields[1]})
-
-        return jsonify(signup_codes=entries)
+    try:
+        db = MySQLdb.connect("mysql.docker", db="aerofs_sp")
+        try:
+            c = db.cursor()
+            c.execute("select t_to, t_code from sp_signup_code where t_ts in ( select max(t_ts) from sp_signup_code group by t_to )")
+            rows = c.fetchall()
+            entries = []
+            for row in rows:
+                entries.append({"user": row[0], "signup_code": row[1]})
+            return jsonify(signup_codes=entries)
+        except MySQLdb.Error as e:
+            print e
+            return e.str(), 400
+        finally:
+            db.close()
+    except Exception as e:
+        print e
+        raise
 
 app.run('0.0.0.0', 21337)
--- src/bunker/root/opt/bunker/web/views/maintenance/container_logs_view.py
+++ src/bunker/root/opt/bunker/web/views/maintenance/container_logs_view.py
@@ -4,9 +4,9 @@ from tempfile import NamedTemporaryFile
 from os.path import dirname, exists, basename, splitext
 from pyramid.view import view_config
 from os import makedirs, unlink
-from subprocess import check_output
 from zipfile import ZipFile
 from logs_view import LOG_ARCHIVE_PATH
+from docker import Client
 
 @view_config(
     route_name='json-archive-container-logs',
@@ -22,6 +22,8 @@ def archive_container_logs(request):
     if exists(LOG_ARCHIVE_PATH):
         unlink(LOG_ARCHIVE_PATH)
 
+    cli = Client(base_url='unix://var/run/docker.sock')
+
     # Move it to a separate, background process if archiving takes too long.
     with ZipFile(LOG_ARCHIVE_PATH, 'w') as z:
         logfiles = glob.glob('/var/lib/docker/containers/*/*.log*')
@@ -29,7 +31,7 @@ def archive_container_logs(request):
             with open(path) as logfile, NamedTemporaryFile() as formatted_logs:
                 print 'Archiving log at {}...'.format(path)
                 container_id = basename(dirname(path))
-                container_name = check_output(['docker', 'inspect', '--format="{{ .Name }}"', container_id]).strip().replace("/", "")
+                container_name = cli.inspect_container(container_id)['Name'].strip().replace("/", "")
 
                 for line in logfile.readlines():
                     # some docker log files have null bytes left over
