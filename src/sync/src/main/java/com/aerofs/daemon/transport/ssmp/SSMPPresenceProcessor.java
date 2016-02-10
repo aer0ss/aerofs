aer0ss/aerofs

commit 5e598b4e0b77f63ed2a3a0b2275c52fa70d61352

Check SSMP notification provenance
Command, Polaris, and ACL notifications should only be accepted by
desktop clients if they come from an internal appliance service.

This is enforced by checking for anonymous provenance, which is
only allowed when authenticating to lipwig with the appliance
shared secret.

Change-Id: Ia2da3dfc7090d656a038a520b4df653b0db54202

src/sync/src/main/java/com/aerofs/daemon/transport/ssmp/SSMPPresenceProcessor.java
@@ -28,27 +28,16 @@ public class SSMPPresenceProcessor implements ConnectionListener, EventHandler {
 
     @Override
     public void eventReceived(SSMPEvent ev) {
-        switch (ev.type) {
-            case SUBSCRIBE:
-                try {
-                    DID did = new DID(ev.from.toString());
-                    SID sid = new SID(ev.to.toString());
-                    updateStores(true, did, sid);
-                } catch (ExInvalidID ex) {
-                    l.warn("unexpected presence {} {}", ev.from, ev.to);
-                }
-                break;
-            case UNSUBSCRIBE:
-                try {
-                    DID did = new DID(ev.from.toString());
-                    SID sid = new SID(ev.to.toString());
-                    updateStores(false, did, sid);
-                } catch (ExInvalidID ex) {
-                    l.warn("unexpected presence {} {}", ev.from, ev.to);
-                }
-                break;
-            default:
-                break;
+        try {
+            DID did = new DID(ev.from.toString());
+            SID sid = new SID(ev.to.toString());
+            switch (ev.type) {
+                case SUBSCRIBE:   updateStores(true, did, sid);  break;
+                case UNSUBSCRIBE: updateStores(false, did, sid); break;
+                default: break;
+            }
+        } catch (ExInvalidID ex) {
+            l.warn("unexpected presence {} {}", ev.from, ev.to);
         }
     }
 

src/gui/src/com/aerofs/controller/CommandNotificationSubscriber.java
@@ -131,7 +131,10 @@ public final class CommandNotificationSubscriber implements EventHandler
     public void eventReceived(SSMPEvent ev)
     {
         l.debug("cmd: notification received");
-        if (ev.type != Type.UCAST) return;
+        if (ev.type != Type.UCAST || !ev.from.isAnonymous()) {
+            l.warn("ignore cm msg {} {}", ev.from, ev.to);
+            return;
+        }
 
         Command command;
         try {
@@ -145,7 +148,7 @@ public final class CommandNotificationSubscriber implements EventHandler
         // stored in the db. After all operations have been completed, store the new max
         // command ID for next time and send an ack.
 
-        l.debug("cmd notification: epoch=" + command.getEpoch() + " type=" + command.getType());
+        l.debug("cmd notification: epoch={} type={}", command.getEpoch(), command.getType());
         scheduleSingleCommandExecution(command);
     }
 

src/sync/src/main/java/com/aerofs/daemon/core/acl/ACLNotificationSubscriber.java
@@ -103,6 +103,10 @@ public final class ACLNotificationSubscriber implements ConnectionListener, Even
 
     @Override
     public void eventReceived(SSMPEvent e) {
+        if (!e.from.isAnonymous()) {
+            l.warn("ignore acl msg {} {}", e.from, e.to);
+            return;
+        }
         checkState(_topic.equals(e.to), "topic: act:%s exp:%s", e.to, _topic);
 
         l.info("recv t:{}", e.to);

src/sync/src/main/java/com/aerofs/daemon/core/polaris/fetch/ChangeNotificationSubscriber.java
@@ -102,7 +102,11 @@ public class ChangeNotificationSubscriber implements ConnectionListener, EventHa
 
     @Override
     public void eventReceived(SSMPEvent ev) {
-        if (ev.type != Type.MCAST || !ev.to.toString().startsWith("pol/")) return;
+        if (ev.type != Type.MCAST || !ev.from.isAnonymous()
+                || !ev.to.toString().startsWith("pol/")) {
+            l.warn("ignore pol msg {} {}", ev.from, ev.to);
+            return;
+        }
         try {
             scheduleFetch(new SID(ev.to.toString().substring(4)));
         } catch (Exception e) {

commit 5552e617275192ee0e1e89470e77a6b0292950e7

Fix regression
Introduced by 5e598b4e0b77f63ed2a3a0b2275c52fa70d61352

Change-Id: I3dd6b6b607df2c33b3817e75d4bc222f63bac3f0

src/sync/src/main/java/com/aerofs/daemon/transport/ssmp/SSMPPresenceProcessor.java
@@ -9,6 +9,7 @@ import com.aerofs.ids.SID;
 import com.aerofs.ssmp.SSMPClient.ConnectionListener;
 import com.aerofs.ssmp.SSMPEvent;
 import com.aerofs.ssmp.EventHandler;
+import com.aerofs.ssmp.SSMPEvent.Type;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.TreeMultimap;
@@ -28,14 +29,11 @@ public class SSMPPresenceProcessor implements ConnectionListener, EventHandler {
 
     @Override
     public void eventReceived(SSMPEvent ev) {
+        if (ev.type != Type.SUBSCRIBE && ev.type != Type.UNSUBSCRIBE) return;
         try {
             DID did = new DID(ev.from.toString());
             SID sid = new SID(ev.to.toString());
-            switch (ev.type) {
-                case SUBSCRIBE:   updateStores(true, did, sid);  break;
-                case UNSUBSCRIBE: updateStores(false, did, sid); break;
-                default: break;
-            }
+            updateStores(ev.type == Type.SUBSCRIBE, did, sid);
         } catch (ExInvalidID ex) {
             l.warn("unexpected presence {} {}", ev.from, ev.to);
         }