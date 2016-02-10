36d16c42bcfc8ce8c7528a997f08c81d3d81a1dd
Matt Pillar <matt@aerofs.com>
2016-02-09 19:25:55 -0800

    ENG-3914 Lizard: add 30 day promo code.
    Change-Id: Ied066093351e40b66da9f724b92f4db08219719b

--- src/lizard/lizard/promotions.py
+++ src/lizard/lizard/promotions.py
@@ -1,7 +1,6 @@
 import flask_login as login
-import markupsafe
 from flask import flash, redirect, url_for, render_template
-from lizard import models, db, forms, analytics_client
+from lizard import models, db, forms
 
 E_INVALID_CODE = "The promotion code you provided is not valid or has expired. " \
                  "Please contact AeroFS Support for assistance."
@@ -18,7 +17,6 @@ def get_promo(code):
 
 def post_promo(code):
     post = [f for (_, c, _, f) in _promotions if c == code]
-
     if post:
         return post[0]()
     else:
@@ -29,52 +27,50 @@ def get_code_for(policy):
     return [c for (p, c, _, _) in _promotions if p == policy][0]
 
 
-def _get_promo_biz90():
-    # note that we can't distinguish between users refreshing the page
-    # vs. users seeing the promotion but choosing not to activate it.
-    #
-    # TODO: verify the following commented out code works and uncomment them
-    # user = login.current_user
-    #
-    # analytics_client.track(user.customer_id, "Viewed Biz90", {
-    #     'email': markupsafe.escape(user.email)
-    # })
+def _get_promo_biz30():
+    return render_template('promo_biz30.html', form=forms.PromoForm(code=get_code_for('biz30')))
 
+
+def _get_promo_biz90():
     return render_template('promo_biz90.html', form=forms.PromoForm(code=get_code_for('biz90')))
 
 
-def _post_promo_biz90():
+def _create_new_biz_license(days):
     """
-    Issue a Business license with all features for 90 days.
+    Issue a Business license with all features for the given number of days.
     """
     user = login.current_user
-
     l = models.License()
     l.customer = user.customer
     l.state = models.License.LicenseState.PENDING
     l.seats = 30
-    l.set_days_until_expiry(90)
+    l.set_days_until_expiry(days)
     l.is_trial = True
     l.allow_audit = True
     l.allow_identity = True
     l.allow_mdm = True
     l.allow_device_restriction = True
-
     db.session.add(l)
     db.session.commit()
 
-    # TODO: verify the following commented out code works and uncomment them
-    # analytics_client.track(user.customer_id, "Activated Biz90", {
-    #     'email': markupsafe.escape(user.email)
-    # })
 
+def _post_promo_biz30():
+    _create_new_biz_license(30)
+    flash(u"Your 30-day trial has started", "success")
+    return redirect(url_for(".dashboard"))
+
+
+def _post_promo_biz90():
+    _create_new_biz_license(90)
     flash(u"Your 90-day trial has started", "success")
     return redirect(url_for(".dashboard"))
 
-# the only reason to map uuid to promotion is to obscure the code and to discourage
+
+# The only reason to map uuid to promotion is to obscure the code and to discourage
 # guessing/probing the backend for promotions.
 _promotions = [
     # policy, code, get_handler, post_handler
-    # the quick and dirty way to expire a policy is to comment out the policy here
+    # The quick and dirty way to expire a policy is to comment out the policy here.
+    ('biz30', '299F7A32-92EE-4E49-86EF-F96CB79924F9', _get_promo_biz30, _post_promo_biz30),
     ('biz90', 'B10E4FF3-7036-426C-8E23-106A81E8745E', _get_promo_biz90, _post_promo_biz90),
 ]
