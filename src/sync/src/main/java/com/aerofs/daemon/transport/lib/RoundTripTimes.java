99a1241355df1f38fdd194cc76407a004c432f32
Hugues Bruant <hugues@aerofs.com>
2016-02-17 21:54:54 -0500

    Fix thread-safety issue in RoundTripTimes
    Change-Id: I5a1737f8ba3201975e425d9ea9a76bcffbf12be0

--- src/sync/src/main/java/com/aerofs/daemon/transport/lib/RoundTripTimes.java
+++ src/sync/src/main/java/com/aerofs/daemon/transport/lib/RoundTripTimes.java
@@ -5,18 +5,18 @@
 package com.aerofs.daemon.transport.lib;
 
 import com.aerofs.base.Loggers;
-import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 
 import javax.annotation.Nullable;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class RoundTripTimes implements IRoundTripTimes
 {
     private static Logger l = Loggers.getLogger(RoundTripTimes.class);
     private static float _weight = 0.7f;  // for use in exponentially weighted average algorithm
 
-    private final Map<Integer, Long> _map = Maps.newTreeMap();
+    private final Map<Integer, Long> _map = new ConcurrentHashMap<>();
 
     /**
      * N.B. this is not synchronized. If this is called concurrently for the same channelId, one
@@ -26,13 +26,9 @@ public class RoundTripTimes implements IRoundTripTimes
     public void putMicros(int channelId, long time)
     {
         l.trace("rtt putMicros {} {}", channelId, time);
-        if (!_map.containsKey(channelId)) {
-            _map.put(channelId, time);
-            return;
-        }
-        long rttOld = _map.get(channelId);
-        long rttNew = (long)((_weight * (float)rttOld) + ((1 - _weight) * (float)time));
-        _map.put(channelId, rttNew);
+        _map.compute(channelId, (k, old) ->
+            old == null ? time : (long)((_weight * (float)old) + ((1 - _weight) * (float)time))
+        );
     }
 
     @Override
