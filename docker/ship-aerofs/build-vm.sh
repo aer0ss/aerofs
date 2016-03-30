c0bdb4deef7c63ffaf107be76e7a0323d36a91a5
Rudy Dai <rudy@aerofs.com>
2016-03-29 18:32:39 -0700

    fix build vm scripts to handle sa loader
    Change-Id: I977696baac8874f6424abad77986c61562953334

--- docker/ship-aerofs/build-vm.sh
+++ docker/ship-aerofs/build-vm.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -eu
 
+LOADER="$1"
+shift
+if [ "$LOADER" == "aerofs/loader" ]; then
+    OUTPUT_DIR="${THIS_DIR}/../../out.ship/appliance"
+else
+    OUTPUT_DIR="${THIS_DIR}/../../out.ship/sa-appliance"
+fi
+
 # Build OVA, remove build artifacts, change to output directory.
 THIS_DIR="$(dirname $0)"
-"${THIS_DIR}/build.sh" cloudinit,preloaded $@
-cd "${THIS_DIR}/../../out.ship/appliance/preloaded"
+"${THIS_DIR}/build.sh" cloudinit,preloaded "$LOADER" "$OUTPUT_DIR" $@
+
+cd "${OUTPUT_DIR}/preloaded"
 rm -f "disk.vdi"
 BASENAME=$(basename *.ova | sed -e 's/\.ova//')
 
--- docker/ship-aerofs/build.sh
+++ docker/ship-aerofs/build.sh
@@ -4,25 +4,20 @@ set -e
 # Build AeroFS appliance VM. See Ship Enterprise's vm/build.sh for optional command-line arguments.
 #
 
-if [ $# != 2 ]; then
-    echo "Usage: $0 <output-format> <loader-image-name> [optional arguments]"
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <output-format> <loader-image-name> <output-dir> [optional arguments]"
     echo "       See Ship Enterprise's vm/build.sh for output formats and optional command-line arguments."
     exit 11
 fi
 OUTPUT="$1"
 LOADER="$2"
+OUTPUT_DIR="$3"
 shift
 shift
 
 THIS_DIR="$(dirname $0)"
 
 SHIP_YML="$("${THIS_DIR}/render-ship-yml.sh" $LOADER)"
-
-if [ "$LOADER" == "aerofs/loader" ]; then
-    OUTPUT_DIR="${THIS_DIR}/../../out.ship/appliance"
-else
-    OUTPUT_DIR="${THIS_DIR}/../../out.ship/sa-appliance"
-fi
 
 "${THIS_DIR}/../ship/vm/build.sh" "${OUTPUT}" "${SHIP_YML}" "" "${OUTPUT_DIR}" $@
 
