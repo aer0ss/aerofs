2465b6d5bf61141185c22ecf16261800fb32cd06
Kevin James <kevin@aerofs.com>
2016-03-30 10:47:45 -0700

    Remove duplicate img_fresh logic
    Change-Id: I4e7cd0a499634bfdc20b788b4fbfceb181a8ef47

--- tools/cache/img_fresh.sh
+++ tools/cache/img_fresh.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-# $1 image
-# $2 source path
-# return true if the image is newer than the newest file in the source tree
+set -eu
 
-# find creation date of container image, if present
-# NB: remove sub-second precision and make it find-friendly
-created=$(
-    docker inspect --format='{{.Created}}' --type=image "$1" |\
-    cut -d. -f1 |\
-    sed 's/T/ /'
-)
+THIS_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# find first file in build context newer than image, if any
-# NB: docker gives UTC timestamps, make sure find does not compare it to local time
-[[ -n "$created" ]] && [[ -e "$2" ]] && [[ -z "$(TZ=UTC find "$2" -type f -newermt "$created" | head -n 1)" ]]
+source "${THIS_DIR}/../../golang/gockerize/img_fresh.sh"
 
+img_fresh "$1" "$2"
