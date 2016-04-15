30ae75bfea5b4111d10c7ceea8e9441fed55d3c4
Jared Kvanvig <jaredk@MacBook-Pro-3.local>
2016-04-15 15:30:06 -0700

    another borked syncdet test
    Change-Id: I98c0d4c42c0286e7b797f6e7d353f2b962ff6760

--- system-tests/syncdet/core/sync_status/should_resync_on_migration_slow.py
+++ system-tests/syncdet/core/sync_status/should_resync_on_migration_slow.py
@@ -41,12 +41,14 @@ def client():
 
     r.share_folder(instance_path('foo'))
 
+    wait_synced(r, instance_path('foo'))
+
     sync('shared')
 
     shutil.move(instance_path('foo', 'bar'), instance_path('bar'))
 
-    wait_not_synced(r, instance_path('bar'))
     wait_synced(r, instance_path('foo'))
+    wait_synced(r, instance_path('bar'))
 
     sync('migrated')
 
@@ -57,11 +59,13 @@ def client():
 
 
 def team_server():
-    wait_file_with_content(os.path.join(ts_user_instance_unique_path(), 'foo', 'bar', 'baz' + str(FILES - 1)), 'qux')
+    for i in range(FILES):
+        wait_file_with_content(os.path.join(ts_user_instance_unique_path(), 'foo', 'bar', 'baz' + str(i)), 'qux')
     sync('created')
     sync('synced')
     sync('shared')
-    wait_file_with_content(os.path.join(ts_user_instance_unique_path(), 'bar', 'bar', 'baz' + str(FILES - 1)), 'qux')
+    for i in range(FILES):
+        wait_file_with_content(os.path.join(ts_user_instance_unique_path(), 'bar', 'bar', 'baz' + str(i)), 'qux')
     sync('migrated')
 
 
