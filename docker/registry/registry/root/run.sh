e557370e61743332cb535c01ecd589a31eabaae3
Abhishek Sharma <abhishek@aerofs.com>
2016-04-15 18:12:28 -0700

    Update registry version to 2.4 + bug fix with registry serve
    Change-Id: I929a537d9e81bc86f5aa8f657dfff863281e43ef

--- docker/registry/registry/root/run.sh
+++ docker/registry/registry/root/run.sh
@@ -8,4 +8,4 @@ sed -i -e "s/{{ accesskey }}/$aws_key/g" /config.yml
 sed -i -e "s/{{ secretkey }}/$secret_key/g" /config.yml
 
 cp /config.yml /etc/docker/registry/config.yml
-registry /etc/docker/registry/config.yml
+registry serve /etc/docker/registry/config.yml
