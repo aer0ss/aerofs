f57f5caf9fd45d98fc63ca04618c966b1f6ffe6d
Daniel Cardoza <daniel.cardoza@aerofs.com>
2016-04-22 12:49:29 -0700

    [sloth] Fix broken migration script
    Change-Id: Ic72183a2d1c9bec855e453b2f84196601f94a22a

--- golang/src/aerofs.com/sloth/migration/sloth__V3_7_0__bot_types.sql
+++ golang/src/aerofs.com/sloth/migration/sloth__V3_7_0__bot_types.sql
@@ -1,8 +1,7 @@
 /* All Bot messages now use a data payload. Update previous
  * bot messages to the new format
  */
-UPDATE messages SET body=concat("{\"body\":\"", body, "\"}"), is_data=1
-WHERE from_id IN (select id from bots);
+UPDATE messages SET body=concat("{\"body\":\"", body, "\"}"), is_data=1 WHERE from_id IN (select id from bots);
 
 /* Distinguish different type of bots so we can handle different
  * incoming paylods.
