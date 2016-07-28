fdb527c9af595839f66fef5c15f048b3ab7f9eae
Hugues Bruant <hugues@aerofs.com>
2016-07-27 22:46:46 -0700

    daemon: bound PreparedStatement cache for per-store tables
    Change-Id: I893cf45906b4ac8544f7c79b77b1c71cc236111b

--- src/sync/src/main/java/com/aerofs/lib/db/ParameterizedStatement.java
+++ src/sync/src/main/java/com/aerofs/lib/db/ParameterizedStatement.java
@@ -4,9 +4,9 @@
 
 package com.aerofs.lib.db;
 
-import com.google.common.collect.Maps;
-
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * Sometimes the SQL query depends on a parameter that cannot simply be bound as a prepared
@@ -23,8 +23,20 @@ public class ParameterizedStatement<T>
 
     private final StatementProvider<T> _provider;
 
-    // TODO: LRU cache?
-    private final Map<T, PreparedStatementWrapper> _s = Maps.newHashMap();
+    private final static int CAPACITY = 20;
+    private final Map<T, PreparedStatementWrapper> _s = new LinkedHashMap<T, PreparedStatementWrapper>(CAPACITY, .75f, true) {
+        private static final long serialVersionUID = 1L;
+        @Override
+        protected boolean removeEldestEntry(Entry<T, PreparedStatementWrapper> en)
+        {
+            if (size() > CAPACITY) {
+                en.getValue().close();
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
 
     public ParameterizedStatement(StatementProvider<T> provider)
     {
