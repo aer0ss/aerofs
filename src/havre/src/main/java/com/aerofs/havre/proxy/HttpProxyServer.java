971dbf6f5ba234d6261f5698ffc6fd456d279707
Hugues Bruant <hugues@aerofs.com>
2016-09-13 09:35:11 -0700

    havre: shorten thread names
    Change-Id: Ib08a8d7a89e36d5f513cf539d3c67fa7b2e0515e

--- src/havre/src/main/java/com/aerofs/havre/proxy/HttpProxyServer.java
+++ src/havre/src/main/java/com/aerofs/havre/proxy/HttpProxyServer.java
@@ -7,11 +7,15 @@ import com.aerofs.havre.EndpointConnector;
 import com.aerofs.havre.RequestRouter;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.ServerSocketChannelFactory;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 import org.jboss.netty.handler.codec.http.HttpServerCodec;
 import org.jboss.netty.util.Timer;
 
 import javax.annotation.Nullable;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.aerofs.base.ssl.SSLEngineFactory.newServerFactory;
 
@@ -29,6 +33,8 @@ public class HttpProxyServer extends AbstractNettyServer
     private final EndpointConnector _connector;
     private final Authenticator _auth;
 
+    private static final AtomicInteger THREAD_ID_COUNTER = new AtomicInteger(0);
+
     public HttpProxyServer(InetSocketAddress addr, @Nullable IPrivateKeyProvider key, Timer timer,
                            Authenticator auth, EndpointConnector connector, RequestRouter router) {
         super("http_proxy", addr, key != null ? newServerFactory(key, null) : null);
@@ -44,5 +50,13 @@ public class HttpProxyServer extends AbstractNettyServer
         return Channels.pipeline(
                 new HttpServerCodec(),
                 new HttpRequestProxyHandler(_timer, _auth, _connector, _router, _allChannels));
+    }
+
+    @Override
+    protected ServerSocketChannelFactory getServerSocketFactory()
+    {
+        return new NioServerSocketChannelFactory(
+                Executors.newCachedThreadPool(r -> new Thread(r, "pb" + THREAD_ID_COUNTER.getAndIncrement())),
+                Executors.newCachedThreadPool(r -> new Thread(r, "pw" + THREAD_ID_COUNTER.getAndIncrement())));
     }
 }
