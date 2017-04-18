91d02b99a1945a9ec41bcd64cfdcd99a163b9310
Hugues Bruant <hugues@aerofs.com>
2017-04-18 14:22:00 -0700

    havre: Fix naming of tunnel threads
    Change-Id: I58838b6870f21b29a9937b46f9d70bf26c552c2f

--- src/tunnel/src/main/java/com/aerofs/tunnel/TunnelServer.java
+++ src/tunnel/src/main/java/com/aerofs/tunnel/TunnelServer.java
@@ -8,11 +8,17 @@ import com.aerofs.base.ssl.ICertificateProvider;
 import com.aerofs.base.ssl.IPrivateKeyProvider;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.ServerSocketChannelFactory;
+import org.jboss.netty.channel.socket.nio.NioServerBossPool;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.channel.socket.nio.NioWorkerPool;
 import org.jboss.netty.handler.stream.ChunkedWriteHandler;
 import org.jboss.netty.util.Timer;
 
 import javax.annotation.Nullable;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.aerofs.base.ssl.SSLEngineFactory.newServerFactory;
 
@@ -26,6 +32,8 @@ public class TunnelServer extends AbstractNettyServer
 
     private final Timer _timer;
     private final ITunnelConnectionListener _listener;
+
+    private static final AtomicInteger THREAD_ID_COUNTER = new AtomicInteger(0);
 
     public TunnelServer(InetSocketAddress addr, @Nullable IPrivateKeyProvider key,
             @Nullable ICertificateProvider cacert, UserID user, DID did, Timer timer,
@@ -50,5 +58,15 @@ public class TunnelServer extends AbstractNettyServer
                 TunnelHandler.newIdleStateHandler(_timer),
                 handler
         );
+    }
+
+    @Override
+    protected ServerSocketChannelFactory getServerSocketFactory()
+    {
+        NioServerBossPool bossPool = new NioServerBossPool(Executors.newCachedThreadPool(), 2,
+                (current, proposed) -> "tb" + THREAD_ID_COUNTER.getAndIncrement());
+        NioWorkerPool workerPool = new NioWorkerPool(Executors.newCachedThreadPool(), 16,
+                (current, proposed) -> "tw" + THREAD_ID_COUNTER.getAndIncrement());
+        return new NioServerSocketChannelFactory(bossPool, workerPool);
     }
 }
