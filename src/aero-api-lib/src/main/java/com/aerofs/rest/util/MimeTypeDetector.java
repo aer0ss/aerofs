375bea53fe8417f82ddc726d7f1aec90800955c6
Hugues Bruant <hugues@aerofs.com>
2017-05-03 11:54:54 -0700

    Fix MIME detection for filenames w/ question marks
    As seen by including a question mark in the name of a public
    upload, which resulted in the unexpected lack of an image
    blurb as SA incorrectly reported the MIME type as octet-stream
    
    Change-Id: I4e656d386e6e60385bb63ea97abfa51d5d6f9d02

--- src/aero-api-lib/src/main/java/com/aerofs/rest/util/MimeTypeDetector.java
+++ src/aero-api-lib/src/main/java/com/aerofs/rest/util/MimeTypeDetector.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
 
 public class MimeTypeDetector extends DefaultDetector
 {
@@ -26,7 +27,12 @@ public class MimeTypeDetector extends DefaultDetector
         String type = APPLICATION_OCTET_STREAM;
         try {
             Metadata m = new Metadata();
-            m.set(TikaMetadataKeys.RESOURCE_NAME_KEY, name);
+            // NB: Tika "helpfully" detects and ignore URL query/fragment
+            // This completely breaks detection for filenames that include question marks
+            // as it results in the entire prefix being stripped. Work around this URL-encoding
+            // the file name if needed.
+            m.set(TikaMetadataKeys.RESOURCE_NAME_KEY,
+                    name != null && name.indexOf('?') != -1 ? URLEncoder.encode(name, "UTF-8") : name);
             // NB: only use name-based detection for now
             try (InputStream s = new ByteArrayInputStream(new byte[0])) {
                 type = detect(s, m).toString();
