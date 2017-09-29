9420129f96037400cc12ba55e2221bbcb0090db5
Hugues Bruant <hugues@aerofs.com>
2017-09-28 18:19:16 -0700

    Pin boot2docker ISO again
    Change-Id: I89c7054692a30a12ab693bb4c16005d84b3570fc

--- docker/dev/dk-create-vm.sh
+++ docker/dev/dk-create-vm.sh
@@ -11,7 +11,10 @@ VM=$1
 
 if [ -z "$(docker-machine ls | grep ${VM})" ]
 then
+    # NB: explicit boot2docker URL is required for now
+    # docker 1.13 removed support for pushing to v1 registries but registry.aerofs.com is still using v1
     docker-machine create -d virtualbox --virtualbox-disk-size 50000 \
+        --virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v1.12.5/boot2docker.iso \
         --virtualbox-memory 3072 --virtualbox-cpu-count 2 ${VM}
 
     # XXX
