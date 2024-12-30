02395247a1a904e5043c9fd43d570e9103e54fc8
Hugues Bruant <hugues.bruant@gmail.com>
2024-12-30 00:11:18 -0800

    build: fix docker builds

--- docker/ship/vm/loader/build.sh
+++ docker/ship/vm/loader/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-docker build -t shipenterprise/vm-loader "$(dirname "$0")"
+docker build --network aero-bridge -t shipenterprise/vm-loader "$(dirname "$0")"
 echo >&2 -e "\033[32mok: \033[0m- build shipenterprise/vm-loader"
